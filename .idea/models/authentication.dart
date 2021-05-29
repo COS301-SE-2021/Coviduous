aa97d266740c8c654334b4cee57d6564444fece4
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-05-29 03:57:36 +0200

    SignUp class ...

--- .idea/models/authentication.dart
+++ .idea/models/authentication.dart
@@ -3,3 +3,18 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'http_exeption.dart';
+
+class Authentication with ChangeNotifier
+{
+
+  Future<void> signUp(String email,String password) async
+  {
+    const  url= 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAYf69-rLqgS4ryuJc1bYCJObTogVuglpo';
+    try {
+      final response = await http.post(url, body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }
+      ));
