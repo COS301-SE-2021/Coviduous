399e0116ec7aa63626223d79a3043bf29e7dbbd2
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-05-29 03:49:30 +0200

    Register Class decla..

--- .idea/screens/signup_screen.dart
+++ .idea/screens/signup_screen.dart
@@ -3,3 +3,9 @@ import '../models/authentication.dart';
 import 'package:provider/provider.dart';
 import 'home_screen.dart';
 import 'login.screen.dart';
+
+class Register extends StatefulWidget {
+  static const routeName = "/register";
+  @override
+  _RegisterState createState() => _RegisterState();
+}
