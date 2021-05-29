1d4033d1e8ca68fd749030284bf40b51b00caaf7
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-05-29 03:42:13 +0200

    Login Screen declaration.

--- .idea/screens/login_screen.dart
+++ .idea/screens/login_screen.dart
@@ -5,3 +5,17 @@ import 'package:provider/provider.dart';
 
 import 'signup_screen.dart';
 import '../models/authentication.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = "/login";
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  Map<String, String> _authData = {
+    'email' : '',
+    'password' : ''
+  };
