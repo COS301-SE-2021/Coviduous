0136f2ef8f3c445a74c5a37bb587b34cd1b8e1bc
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-05-29 03:46:19 +0200

    login_screen.dart impl..

--- .idea/screens/login_screen.dart
+++ .idea/screens/login_screen.dart
@@ -57,3 +57,111 @@ class _LoginScreenState extends State<LoginScreen>{
     }
 
   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        actions: <Widget>[
+          FlatButton(
+            child: Row(
+              children: <Widget>[
+                Text('Register'),
+                Icon(Icons.person_add)
+              ],
+            ),
+            textColor: Colors.white,
+            onPressed: (){
+              Navigator.of(context).pushReplacementNamed(Register.routeName);
+            },
+          )
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.lightGreenAccent,
+                      Colors.blue,
+                    ]
+                )
+            ),
+          ),
+          Center(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Container(
+                height: 260,
+                width: 300,
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          //email
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value)
+                            {
+                              if(value.isEmpty || value.contains('@'))
+                              {
+                                return 'invalid email';
+                              }
+                              return null;
+                            },
+                            onSaved: (value){
+                              _authData['email'] = value;
+                            },
+                          ),
+                          //password
+                          TextFormField(
+                            decoration: InputDecoration(labelText:'Password'),
+                            obscureText: true,
+                            validator: (value)
+                            {
+                              if(value.isEmpty || value.length<=5)
+                              {
+                                return 'invalid password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value)
+                            {
+                              _authData['password'] = value;
+                            },
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          RaisedButton(
+                            child: Text(
+                                'Submit'
+                            ),
+                            onPressed: ()
+                            {
+                              _submit();
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
