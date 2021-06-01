ac69463102d84d67e84c5f03f6809e02989d65cb
Peter Okumbe <u18052640@tuks.co.za>
2021-06-02 01:25:40 +0200

    added previously removed required module

--- test_node_server/app.js
+++ test_node_server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const db = require("./queries");
+const db = require("./queries");
 const host = db.host;
 const port = db.port;
 
