2f80562f1554eb9d7d8626f0d8c65013d01683f0
Peter Okumbe <u18052640@tuks.co.za>
2021-06-02 01:30:49 +0200

    Update http.dart

--- test_node_flutter/lib/modules/http.dart
+++ test_node_flutter/lib/modules/http.dart
@@ -17,12 +17,15 @@ Future<RequestResult> http_get(String route, [dynamic data]) async
   var dataStr = jsonEncode(data);
   var url = Uri.parse("$PROTOCOL://$DOMAIN/$route?data=$dataStr");
   var result = await http.get(url);
+
   return RequestResult(true, jsonDecode(result.body));
 }
+
 Future<RequestResult> http_post(String route, [dynamic data]) async
 {
   var url = Uri.parse("$PROTOCOL://$DOMAIN/$route");
   var dataStr = jsonEncode(data);
   var result = await http.post(url, body: dataStr, headers:{"Content-Type":"application/json"});
+  
   return RequestResult(true, jsonDecode(result.body));
 }
