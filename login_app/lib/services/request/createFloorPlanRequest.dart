3c431b0ac7f90b1ebbc10be131c2c956d8c9d4b8
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 01:09:43 +0200

    floor plan request

--- login_app/lib/services/request/createFloorPlanRequest.dart
+++ login_app/lib/services/request/createFloorPlanRequest.dart
@@ -1,1 +1,23 @@
-class createFloorPlanRequest {}
+class createFloorPlanRequest {
+  String admin;
+  int floorNumber;
+  int totalRooms;
+
+  createFloorPlanRequest(String admin, int floorNumber, int totalRooms) {
+    this.admin = admin;
+    this.floorNumber = floorNumber;
+    this.totalRooms = totalRooms;
+  }
+
+  String getAdmin() {
+    return admin;
+  }
+
+  int getFloorNumber() {
+    return floorNumber;
+  }
+
+  int getTotalRooms() {
+    return totalRooms;
+  }
+}
