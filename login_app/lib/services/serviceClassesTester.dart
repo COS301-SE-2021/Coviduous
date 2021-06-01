7a6f4008d8e353c972693aa5cee451c6404c3674
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 01:07:51 +0200

    class to test services

---
+++ login_app/lib/services/serviceClassesTester.dart
@@ -0,0 +1,18 @@
+import 'request/createFloorPlanRequest.dart';
+import 'response/createFloorPlanResponse.dart';
+import 'services.dart';
+
+void main() {
+  createFloorPlanRequest req = createFloorPlanRequest("Njabulo Skosana", 1, 5);
+  createFloorPlanRequest req2 = createFloorPlanRequest("Njabulo Skosana", 1, 5);
+  services service = new services();
+  services service2 = new services();
+  createFloorPlanResponse resp = service.createFloorPlan(req);
+  resp = service2.createFloorPlan(req2);
+
+  if (resp.getResponse()) {
+    print('successfuly added a floor');
+    int numFloors = service.getNumberOfFloors();
+    print(numFloors.toString());
+  }
+}
