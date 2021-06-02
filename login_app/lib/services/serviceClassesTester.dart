822ba9fca2cdcaf8f19350a0c9d84cc2f315cf05
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 16:13:59 +0200

    syntax fix

--- login_app/lib/services/serviceClassesTester.dart
+++ login_app/lib/services/serviceClassesTester.dart
@@ -3,8 +3,10 @@ import 'response/createFloorPlanResponse.dart';
 import 'services.dart';
 
 void main() {
-  createFloorPlanRequest req = createFloorPlanRequest("Njabulo Skosana", 1, 5);
-  createFloorPlanRequest req2 = createFloorPlanRequest("Njabulo Skosana", 1, 5);
+  createFloorPlanRequest req =
+      createFloorPlanRequest("Njabulo Skosana", "1", 5);
+  createFloorPlanRequest req2 =
+      createFloorPlanRequest("Njabulo Skosana", "1", 5);
   services service = new services();
   services service2 = new services();
   createFloorPlanResponse resp = service.createFloorPlan(req);
