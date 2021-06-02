65562beff0f7e88bbafb849485ecbaa9dd21b4cd
u18202374-Chaks <u18202374@tuks.co.za>
2021-06-02 15:03:51 +0200

    bookofficeSpace setter and getters

--- login_app/lib/services/response/bookOfficeSpaceResponse.dart
+++ login_app/lib/services/response/bookOfficeSpaceResponse.dart
@@ -6,7 +6,39 @@ class bookOfficeSpaceResponse {
   int roomNum;
   int deskNum;
 
+  bookOfficeSpaceResponse(int floorNum, int roomNum, int deskNum)
+  {
+    date = LocalDate.now();
+    time = LocalTime.now();
+    this.floorNum = floorNum;
+    this.roomNum = roomNum;
+    this.deskNum = deskNum;
+  }
 
+  LocalDate getDate()
+  {
+    return date;
+  }
+
+  LocalTime getTime()
+  {
+    return time;
+  }
+
+  int getFloorNumber()
+  {
+    return floorNum;
+  }
+
+  int getRoomNumber()
+  {
+    return roomNum;
+  }
+
+  int getDeskNumber()
+  {
+    return deskNum;
+  }
 
 
 
