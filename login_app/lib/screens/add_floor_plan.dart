df3f7d25f720b16b2399690b970a5a803ca79985
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 09:34:23 +0200

    Added floor_plan file with imports..

---
+++ login_app/lib/screens/add_floor_plan.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FloorPlan extends StatelessWidget {
+  static const routeName = "/floor-plan";
+  const FloorPlan({Key key}) : super(key: key);
