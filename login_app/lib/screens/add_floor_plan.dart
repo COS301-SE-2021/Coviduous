2f125ceb7f89c8bd92e6d388e960202a8021f39f
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 09:40:45 +0200

    Working on sizing and styling..

--- login_app/lib/screens/add_floor_plan.dart
+++ login_app/lib/screens/add_floor_plan.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+//needs to be styled, unfinished..
 class FloorPlan extends StatelessWidget {
   static const routeName = "/floor-plan";
   const FloorPlan({Key key}) : super(key: key);
