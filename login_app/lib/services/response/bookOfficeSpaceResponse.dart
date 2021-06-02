9946f64fb6f9a3733c11c1b1718ea354e2edbb27
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 16:16:13 +0200

    syntax fix

--- login_app/lib/services/response/bookOfficeSpaceResponse.dart
+++ login_app/lib/services/response/bookOfficeSpaceResponse.dart
@@ -1,13 +1,30 @@
 class bookOfficeSpaceResponse {
   //add code for response object
-  LocalDate date;
-  LocalTime time;
-  int floorNum;
-  int roomNum;
-  int deskNum;
+  DateTime dateTime;
+  String floorNum = "";
+  String roomNum = "";
+  int deskNum = 0;
 
+  bookOfficeSpaceResponse(String floorNum, String roomNum, int deskNum) {
+    this.dateTime = DateTime.now();
+    this.floorNum = floorNum;
+    this.roomNum = roomNum;
+    this.deskNum = deskNum;
+  }
 
+  DateTime getDateTime() {
+    return dateTime;
+  }
 
+  String getFloorNumber() {
+    return floorNum;
+  }
 
+  String getRoomNumber() {
+    return roomNum;
+  }
 
+  int getDeskNumber() {
+    return deskNum;
+  }
 }
