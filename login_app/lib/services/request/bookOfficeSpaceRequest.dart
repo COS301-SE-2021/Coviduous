8cfd4dd3a51f9786771904a2f2cd50e97fa194a7
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 14:41:53 +0200

    update

--- login_app/lib/services/request/bookOfficeSpaceRequest.dart
+++ login_app/lib/services/request/bookOfficeSpaceRequest.dart
@@ -1,3 +1,24 @@
 class bookOfficeSpaceRequest {
   //add code for request object
+  int floorNum = 0;
+  int roomNum = 0;
+  int deskNum = 0;
+
+  bookOfficeSpaceRequest(int floorNum, int roomNum, int deskNum) {
+    this.floorNum = floorNum;
+    this.roomNum = roomNum;
+    this.deskNum = deskNum;
+  }
+
+  int getFloorNumber() {
+    return floorNum;
+  }
+
+  int getRoomNumber() {
+    return roomNum;
+  }
+
+  int getDeskNumber() {
+    return deskNum;
+  }
 }
--- login_app/lib/services/request/viewOfficeSpaceRequest.dart
+++ login_app/lib/services/request/viewOfficeSpaceRequest.dart
@@ -1,3 +1,28 @@
+import 'package:login_app/services/floorplan/floor.dart';
+
 class viewOfficeSpaceRequest {
   //add code for request object
+  int floorNumber;
+    List<floor> floor=[];
+
+    public viewOfficeSpaceRequest(List<floor> f, int floorNumber)
+    {
+        this.f = f;
+        this.floorNumber = floorNumber;
+    }
+
+    int getFloorNumber()
+    {
+        return floorNumber;
+    }
+
+    floor getFloor()
+    {
+        for(int i=0;i<floor.length;i++){
+          if(floor[i].getFloorNumber==floorNumber)
+          {
+            return floor[i];
+          }
+        }
+    }
 }
