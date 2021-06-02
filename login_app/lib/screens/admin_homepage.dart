3b5153ed7be4883328c79ca9cddea092a3520d7d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 04:06:32 +0200

    define route and class names.

--- login_app/lib/screens/admin_homepage.dart
+++ login_app/lib/screens/admin_homepage.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:mypages/screens/home_screen.dart';
 import 'package:mypages/screens/add_floor_plan.dart';
 import '../models/globals.dart' as globals;
+class AdminHomePage extends StatefulWidget {
+  static const routeName = "/user";
