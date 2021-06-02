5e828cf530f6e3909030a2aaa90b1469caa75eb4
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 12:52:32 +0200

    calc_floorplan.dart added to git

---
+++ login_app/lib/screens/calc_floorplan.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//class CalcFloorPlan extends StatefulWidget {
+  //static const routeName = "/calc";
+ // @override
+ // _CalcFloorPlanState createState() => _CalcFloorPlanState();
+//}
