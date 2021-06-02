3845439746f7ef0a04136cb3f3785893c69219f3
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-02 16:14:54 +0200

    syntax fix

--- login_app/lib/services/floorplan/floor.dart
+++ login_app/lib/services/floorplan/floor.dart
@@ -1,97 +1,78 @@
-class floor {
-  int floorNum = 0;
-  //add all the attributes in the floor class
-   room rooms;
-   String floorNum;
-   String admin;
-   int numOfRooms;
-   int totalNumRooms;
-   double maxCapacity;
-   int currentCapacity;
+import 'package:login_app/services/floorplan/room.dart';
 
-  floor(int floornum) {
-    this.floorNum = floornum;
-  }
+class floor {
+  //add all the attributes in the floor class
+  List<room> rooms = [];
+  String floorNum = "";
+  String admin = "";
+  int numOfRooms = 0;
+  int totalNumRooms = 0;
+  double maxCapacity = 0.0;
+  int currentCapacity = 0;
 
   floor(String admin, String floorNum, int totalNumOfRoomsInTheFloor) {
     this.floorNum = floorNum;
-    this.numOfRooms = 0; //reprents the rooms that have their capacity determined at this point there are zero rooms that have been initialized within this floor.
-    this.rooms=null;
-    this.admin=admin;
-    this.totalNumRooms=totalNumOfRoomsInTheFloor;
-    this.maxCapacity=0;
-    this.currentCapacity=0;
-    this.rooms=new List(totalNumOfRoomsInTheFloor);
-    for(int i=0;i<totalNumRooms;i++)
-    {
-      rooms[i]=null;
-    }
+    this.numOfRooms =
+        0; //reprents the rooms that have their capacity determined at this point there are zero rooms that have been initialized within this floor.
+    this.admin = admin;
+    this.totalNumRooms = totalNumOfRoomsInTheFloor;
+    this.maxCapacity = 0;
+    this.currentCapacity = 0;
   }
 
-  Boolean addRoom(int counter, String roomNum,double dimentions,double percentage,int numDesks,double deskLength,double deskWidth){
-
-    if(rooms[counter-1]==null)
-    {
-      rooms[counter-1]=new Room(roomNum,dimentions,percentage,numDesks,deskLength,deskWidth);
-      this.maxCapacity=this.maxCapacity+(rooms[counter-1].capacityOfPeopleForSixFtGrid);
-      return true;
-    }
-    numOfRooms++;
-    return false;
+  bool addRoom(String roomNum, double dimentions, double percentage,
+      int numDesks, double deskLength, double deskWidth) {
+    room holder = new room(
+        roomNum, dimentions, percentage, numDesks, deskLength, deskWidth);
+    this.maxCapacity = this.maxCapacity + (holder.capacityOfPeopleForSixFtGrid);
+    return true;
   }
 
-  void viewRoomDetails()
-  {
-    for(int i=0;i<totalNumRooms;i++)
-    {
-      if(rooms[i]!=null)
-      {
-        if((rooms[i].roomNum).equals(roomNum)) //checks if the room number in rooms array is the same as the room number the user specified
-            {
+  void viewRoomDetails(String roomNum) {
+    for (int i = 0; i < totalNumRooms; i++) {
+      if (rooms[i] != null) {
+        if ((rooms[i].roomNum) ==
+            roomNum) //checks if the room number in rooms array is the same as the room number the user specified
+        {
           rooms[i].displayCapacity();
         }
       }
     }
   }
-  Boolean bookDesk()
-  {
 
-    for(int i=0;i<totalNumRooms;i++)
-    {
-      if(rooms[i]!=null)
-      {
-        if((rooms[i].roomNum).equals(roomNum)) //checks if the room number in rooms array is the same as the room number the user specified
-            {
-          if(rooms[i].bookDesk())
-          {
-            System.out.println("Successfully Booked.");
+  bool bookDesk(String roomNum) {
+    for (int i = 0; i < totalNumRooms; i++) {
+      if (rooms[i] != null) {
+        if ((rooms[i].roomNum) ==
+            roomNum) //checks if the room number in rooms array is the same as the room number the user specified
+        {
+          if (rooms[i].bookDesk()) {
+            print("Successfully Booked.");
             return true;
           }
         }
       }
     }
     return false;
-
-
   }
-  void  viewFloorDetails()
-  {
-    System.out.println("***************************************************************************************");
-    System.out.println("Displaying floor Infromation");
-    System.out.println("Total Number Of Rooms : "+totalNumRooms);
-    System.out.println("MaxCapacity : "+maxCapacity);
-    System.out.println("Current Capacity Occupied: "+currentCapacity);
-    System.out.println("***************************************************************************************");
-    for(int i=0;i<totalNumRooms;i++)
-    {
-      if(rooms[i]!=null)
-      {
+
+  void viewFloorDetails() {
+    print(
+        "***************************************************************************************");
+    print("Displaying floor Infromation");
+    print("Total Number Of Rooms : " + totalNumRooms.toString());
+    print("MaxCapacity : " + maxCapacity.toString());
+    print("Current Capacity Occupied: " + currentCapacity.toString());
+    print(
+        "***************************************************************************************");
+    for (int i = 0; i < totalNumRooms; i++) {
+      if (rooms[i] != null) {
         rooms[i].displayCapacity();
       }
     }
-
   }
-  int getFloorNumber() {
+
+  String getFloorNumber() {
     return floorNum;
   }
 }
