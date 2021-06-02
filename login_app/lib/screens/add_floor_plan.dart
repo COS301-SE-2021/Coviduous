529ff105cd78d39330733f7efdaa8c6c8a803ac6
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 12:39:01 +0200

    changes to class name to match file name

--- login_app/lib/screens/add_floor_plan.dart
+++ login_app/lib/screens/add_floor_plan.dart
@@ -6,3 +6,25 @@ class AddFloorPlan extends StatefulWidget {
   @override
   _AddFloorPlanState createState() => _AddFloorPlanState();
 }
+class _AddFloorPlanState extends State<AddFloorPlan> {
+  String _numFloor;
+  String _numRooms;
+
+  Widget _buildAge(){
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Enter number of floors'
+      ),
+      keyboardType: TextInputType.number,
+      validator: (String value) {
+
+        int num = int.tryParse(value);
+        if(num == null || num <= 0){
+          return 'floor number must be greater than zero';
+        }
+      },
+      onSaved: (String value){
+        _numFloor = value;
+      },
+    );
+  }
