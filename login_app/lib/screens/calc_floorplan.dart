956bbef992e0806a479034e7bbeb68bb78824fd5
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 12:54:36 +0200

    Added global variables

--- login_app/lib/screens/calc_floorplan.dart
+++ login_app/lib/screens/calc_floorplan.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-//class CalcFloorPlan extends StatefulWidget {
-  //static const routeName = "/calc";
- // @override
- // _CalcFloorPlanState createState() => _CalcFloorPlanState();
-//}
+class CalcFloorPlan extends StatefulWidget {
+  static const routeName = "/calc";
+  @override
+  _CalcFloorPlanState createState() => _CalcFloorPlanState();
+}
+class _CalcFloorPlanState extends State<CalcFloorPlan> {
+  String _dimensions;
+  String _numdesks;
+  String _length;
+  String _width;
