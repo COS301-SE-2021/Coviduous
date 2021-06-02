5bc50e34095a178741223461f6f6a5faa8087a55
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 12:55:21 +0200

    widget Build Dimensions

--- login_app/lib/screens/calc_floorplan.dart
+++ login_app/lib/screens/calc_floorplan.dart
@@ -10,3 +10,23 @@ class _CalcFloorPlanState extends State<CalcFloorPlan> {
   String _numdesks;
   String _length;
   String _width;
+
+  Widget _buildDimensions(){
+    return TextFormField(
+      maxLength: 15,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Desk Dimensions',
+      ),
+      validator: (String value) {
+        int dimension = int.tryParse(value);
+        if(dimension == null || dimension <= 0){
+          return 'Dimensions are required in order to proceed';
+        }
+      },
+      onSaved: (String value){
+        _dimensions = value;
+      },
+    );
+  }
+
