a36296f6151a3bc357ebebca06048b9f92ec71a3
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 09:36:06 +0200

    A simple textform functionality to accept a digit from user..

--- login_app/lib/screens/add_floor_plan.dart
+++ login_app/lib/screens/add_floor_plan.dart
@@ -6,3 +6,24 @@ import 'package:flutter/services.dart';
 class FloorPlan extends StatelessWidget {
   static const routeName = "/floor-plan";
   const FloorPlan({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Container(
+          padding: const EdgeInsets.all(40.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new TextField(
+                decoration: new InputDecoration(labelText: "Enter the number of floors"),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
