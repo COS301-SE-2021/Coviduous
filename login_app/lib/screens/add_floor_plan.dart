f8b1afe56e8c79adbf5254b559a6bede76492ef3
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-02 12:41:05 +0200

    Input validation number of rooms and floors

--- login_app/lib/screens/add_floor_plan.dart
+++ login_app/lib/screens/add_floor_plan.dart
@@ -10,40 +10,87 @@ class _AddFloorPlanState extends State<AddFloorPlan> {
   String _numFloor;
   String _numRooms;
 
-  Widget _buildAge(){
+  Widget _buildAge() {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Enter number of floors'
       ),
       keyboardType: TextInputType.number,
       validator: (String value) {
-
         int num = int.tryParse(value);
-        if(num == null || num <= 0){
+        if (num == null || num <= 0) {
           return 'floor number must be greater than zero';
         }
       },
-      onSaved: (String value){
+      onSaved: (String value) {
         _numFloor = value;
       },
     );
   }
 
-  Widget _buildRooms(){
+  Widget _buildRooms() {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Enter number of rooms'
       ),
       keyboardType: TextInputType.number,
       validator: (String value) {
-
         int rooms = int.tryParse(value);
-        if(rooms == null || rooms <= 0){
+        if (rooms == null || rooms <= 0) {
           return 'Age must be greater then 0';
         }
       },
-      onSaved: (String value){
+      onSaved: (String value) {
         _numRooms = value;
       },
     );
   }
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Add Floor-plan")
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildAge(),
+                _buildRooms(),
+
+                SizedBox(height: 50),
+
+                RaisedButton(
+                    child: Text('Proceed',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16
+                      ),
+                    ),
+
+                    onPressed: () {
+                      if (!_formKey.currentState.validate()) {
+                        return;
+                      }
+
+                      _formKey.currentState.save();
+                      Navigator.of(context).pushReplacementNamed(
+                          CalcFloorPlan.routeName);
+                    }
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
