58a6ff028f20d16aca61092fa531b689c750f7ec
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-09 12:35:12 +0200

    admin_view_announcements.dart impl

---
+++ login_app/lib/screens/admin_view_announcements.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'admin_homepage.dart';
+import '../services/globals.dart' as globals;
+
+class AdminViewAnnouncements extends StatefulWidget {
+  static const routeName = "/adminannouncements";
+
+  @override
+  _AdminViewAnnouncementsState createState() => _AdminViewAnnouncementsState();
+}
+
+class _AdminViewAnnouncementsState extends State<AdminViewAnnouncements> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
