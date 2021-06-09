964de798ec51532d6d8bba3df01690245549a5c9
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-09 12:37:49 +0200

    admin_view_announcements.dart count number of announcements.

--- login_app/lib/screens/admin_view_announcements.dart
+++ login_app/lib/screens/admin_view_announcements.dart
@@ -13,6 +13,32 @@ class AdminViewAnnouncements extends StatefulWidget {
 class _AdminViewAnnouncementsState extends State<AdminViewAnnouncements> {
   @override
   Widget build(BuildContext context) {
+    Widget getList() {
+      int numberOfAnnouncements = 1;
 
-  }
+      if (numberOfAnnouncements == 0) {
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                height: MediaQuery.of(context).size.height/(24*globals.getWidgetScaling()),
+                color: Theme.of(context).primaryColor,
+                child: Text('No announcements found', style: TextStyle(color: Colors.white, fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+              ),
+              Container(
+                  alignment: Alignment.center,
+                  // width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                  // height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
+                  color: Colors.white,
+                  padding: EdgeInsets.all(12),
+                  child: Text('You have no announcements.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
+              )
+            ]
+        );
+      }
+    }
+
+    }
 }
