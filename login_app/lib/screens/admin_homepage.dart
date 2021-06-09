bd596c4216ee1c6817ad0c99f5847dc611f5ff4b
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-09 21:27:17 +0200

    Update admin_homepage.dart
    Added manage account button.

--- login_app/lib/screens/admin_homepage.dart
+++ login_app/lib/screens/admin_homepage.dart
@@ -227,6 +227,40 @@ class _AdminHomePageState extends State<AdminHomePage> {
                         },
                       )
                   ),
+                ),
+                Container (
+                  alignment: Alignment.bottomLeft,
+                  child: Container (
+                      height: 50,
+                      width: 150,
+                      padding: EdgeInsets.all(10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom (
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text('Manage account'),
+                        onPressed: (){
+                          //Navigator.of(context).pushReplacementNamed(AdminManageAccount.routeName);
+                          showDialog(
+                              context: context,
+                              builder: (ctx) => AlertDialog(
+                                title: Text('Placeholder'),
+                                content: Text('Manage account'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text('Okay'),
+                                    onPressed: (){
+                                      Navigator.of(ctx).pop();
+                                    },
+                                  )
+                                ],
+                              )
+                          );
+                        },
+                      )
+                  ),
                 )
               ]
           )
