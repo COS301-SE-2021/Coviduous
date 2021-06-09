4e51c2e0a7bbe8d22eeb7bdbb8fca72b185794b0
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-09 11:54:08 +0200

    final make announcement page without styling.

--- login_app/lib/screens/admin_make_announcement.dart
+++ login_app/lib/screens/admin_make_announcement.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class DropDown extends StatefulWidget {
+class MakeAnnouncement extends StatefulWidget {
   static const routeName = "/announce";
-  DropDown() : super();
+  MakeAnnouncement() : super();
 
-  final String title = "DropDown Demo";
+  final String title = "Make announcement";
 
   @override
-  DropDownState createState() => DropDownState();
+  MakeAnnouncementState createState() => MakeAnnouncementState();
 }
+
 class Announcement {
   int id;
   String name;
@@ -23,8 +24,8 @@ class Announcement {
   }
 }
 
-class DropDownState extends State<DropDown>{
-
+class MakeAnnouncementState extends State<MakeAnnouncement> {
+  //
   List<Announcement> _announceType = Announcement.getAnnouncementType();
   List<DropdownMenuItem<Announcement>> _dropdownMenuItems;
   Announcement _selectedType;
@@ -36,9 +37,9 @@ class DropDownState extends State<DropDown>{
     super.initState();
   }
 
-  List<DropdownMenuItem<Announcement>> buildDropdownMenuItems(List TypeofAnnounc) {
+  List<DropdownMenuItem<Announcement>> buildDropdownMenuItems(List typeofAnnounc) {
     List<DropdownMenuItem<Announcement>> items = List();
-    for (Announcement _type in TypeofAnnounc) {
+    for (Announcement _type in typeofAnnounc) {
       items.add(
         DropdownMenuItem(
           value: _type,
@@ -48,77 +49,77 @@ class DropDownState extends State<DropDown>{
     }
     return items;
   }
-
-  onChangeDropdownItem(Announcement selectedtype) {
+  onChangeDropdownItem(Announcement selected_type) {
     setState(() {
-      _selectedType = selectedtype;
+      _selectedType = selected_type;
     });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: new Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
         appBar: new AppBar(
-        title: new Text("Make Announcement"),
+          title: new Text("Make Announcement"),
         ),
-          body: new Container(
-            child:Center(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  Text("Select announcement type"),
-              SizedBox(
-                height: 20.0,
-              ),
-                    DropdownButton(
-                      value: _selectedType,
-                      items: _dropdownMenuItems,
-                      onChanged: onChangeDropdownItem,
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Text('Selected: ${_selectedType.name}'),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: "Topic: ",
-                          labelStyle: TextStyle(fontSize: 24, color: Colors.black),
-                          border: InputBorder.none,
-                          fillColor: Colors.black12,
-                          filled: true),
-                      obscureText: false,
-                      maxLength: 20,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Write your announcement",
-                          labelText: "Description",
-                          labelStyle: TextStyle(fontSize: 24, color: Colors.black),
-                          border: UnderlineInputBorder()),
-                      obscureText: false,
-                      maxLines: 3,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    FlatButton(
-                      child: Text("Post"),
-                      color: Colors.blueAccent,
-                      onPressed: () {
-                        // handleSubmit(); (function for backend)
-                      },
-                    )
+        body: new Container(
+
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Select announcement type"),
+                SizedBox(
+                  height: 20.0,
+                ),
+                DropdownButton(
+                  value: _selectedType,
+                  items: _dropdownMenuItems,
+                  onChanged: onChangeDropdownItem,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text('Selected: ${_selectedType.name}'),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Topic: ",
+                      labelStyle: TextStyle(fontSize: 24, color: Colors.black),
+                      border: InputBorder.none,
+                      fillColor: Colors.black12,
+                      filled: true),
+                  obscureText: false,
+                  maxLength: 20,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Write your announcement",
+                      labelText: "Description",
+                      labelStyle: TextStyle(fontSize: 24, color: Colors.black),
+                      border: UnderlineInputBorder()),
+                  obscureText: false,
+                  maxLines: 3,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                FlatButton(
+                  child: Text("Post"),
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    // handleSubmit(); (function for backend)
+                  },
+                )
               ],
-              ),
             ),
           ),
         ),
+      ),
     );
   }
 }
