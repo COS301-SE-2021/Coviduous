a93a4ca18af99551dc2ac1dba89882956ce15065
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-09 11:25:22 +0200

    admin_make_announcement.dart adding class and BuildContext.

---
+++ login_app/lib/screens/admin_make_announcement.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DropDown extends StatefulWidget {
+  static const routeName = "/announce";
+  DropDown() : super();
+
+  final String title = "DropDown Demo";
+
+  @override
+  DropDownState createState() => DropDownState();
+}
+
+class DropDownState extends State<DropDown>{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp();
+
+  }
+}
