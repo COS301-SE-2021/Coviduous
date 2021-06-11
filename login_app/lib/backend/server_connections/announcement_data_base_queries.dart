53c8c35fdccbceab81c2f0c9445b12abb8af5943
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-11 12:11:53 +0200

    announcement database queries

---
+++ login_app/lib/backend/server_connections/announcement_data_base_queries.dart
@@ -0,0 +1,14 @@
+class AnnouncementDatabaseQueries {
+  AnnouncementDatabaseQueries() {}
+
+  bool createAnnouncement() {
+    return false;
+  }
+
+  void viewAnnouncements() //arraylist
+  {}
+
+  bool deleteAnnouncement(String announcementId) {
+    return false;
+  }
+}
