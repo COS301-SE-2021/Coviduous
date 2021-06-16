ec3daf54dce8c20fa438828c1bd257148e3f6bfe
Peter Okumbe <u18052640@tuks.co.za>
2021-06-16 16:33:07 +0200

    Added constructor

--- login_app/lib/requests/announcements_requests/create_announcement_request.dart
+++ login_app/lib/requests/announcements_requests/create_announcement_request.dart
@@ -3,4 +3,12 @@ class CreateAnnouncementRequest {
 	String type;
 	String AdminID;
 	String CompanyID;
+
+	CreateAnnouncementRequest(String type, String message, String AdminID, String CompanyID) 
+	{
+		this.type = type;
+		this.message = message;
+		this.AdminID = AdminID;
+		this.CompanyID = CompanyID;
+	}
 }
