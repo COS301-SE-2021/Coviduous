b1769abd78b47219d16c821e6c6f83f23a005b3e
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-16 17:45:46 +0200

    Create admin_view_announcements_test.dart
    Test class for admin_view_announcements.

---
+++ login_app/test/screens/admin_view_announcements_test.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:login_app/screens/admin_view_announcements.dart';
+
+void main() {
+  setUpAll(() => HttpOverrides.global = null);
+
+  testWidgets('Correct widgets appear', (WidgetTester tester) async {
+    //Create widget for testing
+    Widget createWidgetForTesting({Widget child}) {
+      return MaterialApp(
+          home: child
+      );
+    }
+
+    //Build admin delete announcement screen
+    await tester.pumpWidget(createWidgetForTesting(child: new AdminViewAnnouncements()));
+
+    int numberOfAnnouncements = 1;
+
+    //Verify that the correct widgets appear in the correct order
+    if (numberOfAnnouncements == 0) {
+      expect(find.text('No announcements found'), findsOneWidget); //Find one widget containing 'No announcements found'
+      expect(find.text('You have no announcements.'), findsOneWidget); //Find one widget containing 'You have no announcements.'
+    } else {
+      expect(find.text('Announcement 1'), findsOneWidget); //Find one widget containing 'Announcement 1'
+      expect(find.text('ID: A1'), findsOneWidget); //Find one widget containing 'ID: A1'
+      expect(find.text('Type: General'), findsOneWidget); //Find one widget containing 'Type: General'
+      expect(find.text('Date: test'), findsOneWidget); //Find one widget containing 'Date: test'
+      expect(find.text('Message: Hello World'), findsOneWidget); //Find one widget containing 'Message: Hello World'
+    }
+  });
+}
