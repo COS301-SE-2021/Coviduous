cab003f19f12126cbb7e153f58254a1033ff1471
u18202374-Chaks <u18202374@tuks.co.za>
2021-06-16 23:38:53 +0200

    solve errors viewAdmin

--- login_app/lib/requests/announcements_requests/viewAdmin_announcement_request.dart
+++ login_app/lib/requests/announcements_requests/viewAdmin_announcement_request.dart
@@ -1,6 +1,10 @@
 class ViewAdminAnnouncementRequest {
   String announcement_id;
 
+  ViewAdminAnnouncementRequest(String announcementID) {
+    this.announcement_id = announcementID;
+  }
+
   String getAnnouncement_id() {
     return announcement_id;
   }
