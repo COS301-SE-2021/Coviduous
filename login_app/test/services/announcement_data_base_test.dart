c247069190f0357cca5c8146b252c032f7474460
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-16 15:39:11 +0200

    database connection

---
+++ login_app/test/services/announcement_data_base_test.dart
@@ -0,0 +1,10 @@
+//import '../../lib/backend/server_connections/announcement_data_base_queries.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Correct CreateFloorPlanResponse construction', () {
+    // AnnouncementDatabaseQueries dbQueries = new AnnouncementDatabaseQueries();
+    // var result = dbQueries.deleteAnnouncement("Peter");
+    // expect(result, true);
+  });
+}
