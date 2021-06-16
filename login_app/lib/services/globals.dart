5541836827bc6dc82e86db3e5433f52964546a29
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-16 18:36:20 +0200

    Update globals.dart
    Added a global variable to count the number of announcements that currently exist in the system.

--- login_app/lib/services/globals.dart
+++ login_app/lib/services/globals.dart
@@ -14,6 +14,7 @@ import 'office/booking.dart';
 List<Floor> globalFloors = [];
 List<Booking> globalBookings = [];
 int globalNumFloors = 0;
+int globalNumAnnouncements = 1;
 
 //Frontend global variables
 //==========================
