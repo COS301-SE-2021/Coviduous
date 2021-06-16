b4131b974988afd5b9d35ff5ef1cef5f95bba230
u18202374-Chaks <u18202374@tuks.co.za>
2021-06-17 00:05:25 +0200

    updateviewUserAnnouncement

---
+++ login_app/lib/requests/viewUser_announcement_request.dart
@@ -0,0 +1,11 @@
+class viewUserAnnouncementRequest {
+  String User_id;
+
+  viewUserAnnouncementRequest(String userID) {
+    this.User_id = userID;
+  }
+
+  String getUser_id() {
+    return User_id;
+  }
+}
