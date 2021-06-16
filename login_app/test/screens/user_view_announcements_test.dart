00376170e0558308ad67591ccec009ed54c7b840
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-16 18:36:54 +0200

    Update user_view_announcements_test.dart
    Replaced numberOfAnnouncements hardcoding with global variable.

--- login_app/test/screens/user_view_announcements_test.dart
+++ login_app/test/screens/user_view_announcements_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:login_app/screens/user_view_announcements.dart';
+import 'package:login_app/services/globals.dart' as globals;
 
 void main() {
   setUpAll(() => HttpOverrides.global = null);
@@ -18,7 +19,7 @@ void main() {
     //Build user view announcements screen
     await tester.pumpWidget(createWidgetForTesting(child: new UserViewAnnouncements()));
 
-    int numberOfAnnouncements = 1;
+    int numberOfAnnouncements = globals.globalNumAnnouncements;
 
     //Verify that the correct widgets appear in the correct order
     if (numberOfAnnouncements == 0) {
