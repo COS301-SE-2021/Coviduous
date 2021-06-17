996353d5fbd3fd6d3744eca8c375243753475e43
Peter Okumbe <u18052640@tuks.co.za>
2021-06-18 00:18:27 +0200

    Added testing db attributes

--- login_app/test/services/announcement_data_base_test.dart
+++ login_app/test/services/announcement_data_base_test.dart
@@ -1,10 +1,16 @@
 //import '../../lib/backend/server_connections/announcement_data_base_queries.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:postgres/postgres.dart';
 
 void main() {
-  test('Correct CreateFloorPlanResponse construction', () {
-    // AnnouncementDatabaseQueries dbQueries = new AnnouncementDatabaseQueries();
-    // var result = dbQueries.deleteAnnouncement("Peter");
-    // expect(result, true);
-  });
+  PostgreSQLConnection connection;
+  String host = 'localhost';
+  int port = 5432;
+  String dbName = 'mock_CoviduousDB'; // an existing DB name on your localhost
+  String user = 'postgres';
+  String pass = ' '; // your postgres user password
+
+  String expectedValue;
+  String expectedMessage;
+  
 }
