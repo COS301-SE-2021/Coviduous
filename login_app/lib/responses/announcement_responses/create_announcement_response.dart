0786e31212428d1cc25a70428ff81fcecbb2d5b0
Peter Okumbe <u18052640@tuks.co.za>
2021-06-17 21:08:51 +0200

    Added constructor

--- login_app/lib/responses/announcement_responses/create_announcement_response.dart
+++ login_app/lib/responses/announcement_responses/create_announcement_response.dart
@@ -3,4 +3,12 @@ class CreateAnnouncementResponse {
   String timestamp;
   bool response;
   String responseMessage;
+
+  CreateAnnouncementResponse(String announcementID, String timestamp,
+      bool response, String responseMessage) {
+    this.announcementID = announcementID;
+    this.timestamp = timestamp;
+    this.response = response;
+    this.responseMessage = responseMessage;
+  }
 }
