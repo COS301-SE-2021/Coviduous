54168d051cda18ece0206cbdb6e2674997984938
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:20:25 +0200

    reset.dart google email verification..

--- flutter_pub/lib/Screens/AuthScreen/reset.dart
+++ flutter_pub/lib/Screens/AuthScreen/reset.dart
@@ -7,3 +7,33 @@ class ResetPage extends StatefulWidget {
   @override
   _ResetPageState createState() => _ResetPageState();
 }
+
+class _ResetPageState extends State<ResetPage> {
+  TextEditingController _email = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reset Password"),
+      ),
+      body: isLoading == false ? Padding(
+        padding: const EdgeInsets.all(8.0),
+          child: Column(
+              children: [
+                TextFormField(
+                  controller: _email,
+                  decoration: InputDecoration(
+                      hintText: 'Email'
+                  ),
+                ),
+                const SizedBox(height: 30,),
+                
+              ],
+          ),
+        ) : Center(child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
