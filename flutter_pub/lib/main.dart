cded52a112c57b29ac737dd10d1412210d47bf9d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:39:49 +0200

    main.dart HomeState..

--- flutter_pub/lib/main.dart
+++ flutter_pub/lib/main.dart
@@ -22,3 +22,32 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+class _HomeState extends State<Home> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Splash();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
+
+
