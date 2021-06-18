9aceb7b3fa0c7d50a5f1ebdb1a146c6a3d0fac6e
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-18 10:56:07 +0200

    structure modification

---
+++ login_app/lib/responses/floor_plan_responses/create_floor_plan_response.dart
@@ -0,0 +1,15 @@
+class CreateFloorPlanResponse {
+  bool successful = false;
+
+  createFloorplanResponse(bool success) {
+    this.successful = success;
+  }
+
+  bool getResponse() {
+    return successful;
+  }
+
+  void setResponse(bool success) {
+    this.successful = success;
+  }
+}
