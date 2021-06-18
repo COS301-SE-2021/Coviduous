80bd107ba0f73a8671042e9962f1c24239b4cf6e
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-18 04:31:37 +0200

    announcement globals update

---
+++ login_app/lib/backend/globals/announcements_globals.dart
@@ -0,0 +1,12 @@
+library globals;
+
+import 'package:login_app/announcement_subsystem/announcement.dart';
+
+//Global variables used throughout the program
+//=============================================
+
+//Backend global variables
+//==========================
+
+List<Announcement> announcementDatabaseTable = [];
+int numAnnouncements = 0;
