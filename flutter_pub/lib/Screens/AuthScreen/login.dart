d3b6bf7f63adc222a06787bed05ab66ef6ca9590
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 14:38:36 +0200

    login.dart imports cloud firestore services.

---
+++ flutter_pub/lib/Screens/AuthScreen/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pub/Screens/AuthScreen/register.dart';
+import 'package:flutter_pub/Screens/AuthScreen/reset.dart';
+import 'package:flutter_pub/Screens/home.dart';
+import 'package:flutter_pub/providers/auth_provider.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
