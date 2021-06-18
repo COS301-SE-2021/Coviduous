99a9477b2335359181ad8fe550b13aeba45a252b
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 14:28:21 +0200

    added auth_provider.dart create account

--- flutter_pub/lib/providers/auth_provider.dart
+++ flutter_pub/lib/providers/auth_provider.dart
@@ -1,5 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 class AuthClass{
   FirebaseAuth auth = FirebaseAuth.instance;
-  
+  //create account
+
+  Future<String> createAccount({
+    String email,
+    String password
+  }) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      return "Account created";
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The password provided is too weak.';
+      }
+      else if (e.code == 'email-already-in-use') {
+        return 'The account already exists for that email.';
+      }
+    } catch (e) {
+      return "Error occured";
+    }
+  }
 }
