5cae52a8de8ed8fe12ec5c137e69df6b7cc4b983
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-18 04:27:13 +0200

    announcement db test

--- login_app/test/services/announcement_data_base_test.dart
+++ login_app/test/services/announcement_data_base_test.dart
@@ -1,9 +1,14 @@
 //import '../../lib/backend/server_connections/announcement_data_base_queries.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:login_app/backend/controllers/announcements_controller.dart';
+import 'package:login_app/requests/announcements_requests/create_announcement_request.dart';
+import 'package:login_app/responses/announcement_responses/create_announcement_response.dart';
 import 'package:postgres/postgres.dart';
+import 'package:login_app/backend/globals/announcements_globals.dart'
+    as globals;
 
 void main() {
-  PostgreSQLConnection connection;
+  /*PostgreSQLConnection connection;
   String host = 'localhost';
   int port = 5432;
   String dbName = 'mock_CoviduousDB'; // an existing DB name on your localhost
@@ -60,9 +65,22 @@ void main() {
 
     // expect on use case responses -> expect(expectedResponse, resp.getResponse())
     expect(results.length, isNot(0));
-  });
+  });*/
 
-  test('View announcement', () async {
+/////////////////////////////////////////////////////////////////////////////////
+  test('Create Announcement Mock', () async {
+    var announcementController = AnnouncementsController();
+    CreateAnnouncementRequest req = new CreateAnnouncementRequest(
+        "General", "Please Register For PaySlips", "USRAD-1", "CID-1");
+    CreateAnnouncementResponse resp =
+        announcementController.createAnnouncementMock(req);
+    print("Response : " + resp.getResponseMessage());
+    print("AnnouncementID : " + resp.getAnnouncementID());
+
+    expect(globals.numAnnouncements, isNot(0));
+  });
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /*test('View announcement', () async {
     // to replace with actual viewAnnouncement use case handling function maybe
     var results = await connection.query("SELECT * FROM announcements");
 
@@ -84,6 +102,5 @@ void main() {
 
     // expect on use case responses -> expect(expectedResponse, resp.getResponse())
     expect(results.length, 0);
-  });
-  
+  });*/
 }
