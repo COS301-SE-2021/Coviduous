7cf63ca648c4174cb8063bb0add6377e77715d29
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:15:06 +0200

    imports to reset.dart

---
+++ flutter_pub/lib/Screens/AuthScreen/reset.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pub/Screens/AuthScreen/login.dart';
+import 'package:flutter_pub/Screens/home.dart';
+import 'package:flutter_pub/providers/auth_provider.dart';
+
+class ResetPage extends StatefulWidget {
+  @override
+  _ResetPageState createState() => _ResetPageState();
+}
