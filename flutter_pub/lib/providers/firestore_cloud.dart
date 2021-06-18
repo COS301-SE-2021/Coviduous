66dac51dd4e1cf6e76a71051eae7658b6c4651db
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 14:33:15 +0200

    added firestore_cloud.dart add user functionality impl

--- flutter_pub/lib/providers/firestore_cloud.dart
+++ flutter_pub/lib/providers/firestore_cloud.dart
@@ -3,3 +3,21 @@ import 'package:firebase_auth/firebase_auth.dart';
 //imports for the firebase_core and cloud_firestore plugin
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> userSetup(String Firstname, String lastname, String username, String companyName, String companyLocation)
+async {
+  CollectionReference users = FirebaseFirestore.instance.collection('Users');
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String uid = auth.currentUser.uid.toString();
+  String email = auth.currentUser.email.toString();
+  users.add({
+    'uid': uid,
+    'Firstname': Firstname,
+    'Lastname' : lastname,
+    'Email' : email,
+    'Username' : username,
+    'Company Name' :companyName,
+    'Company Location' : companyLocation
+  });
+  return;
+}
