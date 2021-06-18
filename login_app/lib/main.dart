c767bb0f1f6fc3586d860b2411cc5f00da6b6608
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-18 17:12:15 +0200

    Update main.dart
    For new authentication method.

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/admin_homepage.dart';
@@ -16,49 +17,77 @@ import 'screens/user_manage_account.dart';
 import 'screens/user_view_office_spaces.dart';
 import 'screens/user_view_current_bookings.dart';
 import 'screens/user_view_announcements.dart';
-import 'models/authentication.dart';
 import 'services/globals.dart' as globals;
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget{
-
   @override
   Widget build(BuildContext context){
-    return MultiProvider(
-    providers: [
-        ChangeNotifierProvider.value(
-        value: Authentication(),
-    )
-        ],
-      child: MaterialApp(
-      debugShowCheckedModeBanner: false, //Remove "debug" banner
-      title: 'Coviduous',
-      theme: ThemeData(
-        primaryColor: Color(0xff056676), //AppBar and buttons default color
-        //primaryColor: Color(0xff5E2C25), //AppBar and buttons default color
-        primarySwatch: globals.textFieldSelectedColor, //TextField default color when selected
-        scaffoldBackgroundColor: Color(0xff022C33), //Scaffold background default color
-      ),
-        home: LoginScreen(),
-        routes: {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false, //Remove "debug" banner
+    title: 'Coviduous',
+    theme: ThemeData(
+      primaryColor: Color(0xff056676), //AppBar and buttons default color
+      //primaryColor: Color(0xff5E2C25), //AppBar and buttons default color
+      primarySwatch: globals.textFieldSelectedColor, //TextField default color when selected
+      scaffoldBackgroundColor: Color(0xff022C33), //Scaffold background default color
+    ),
+      home: Home(),
+      routes: {
+        Register.routeName: (ctx)=> Register(),
+        LoginScreen.routeName: (ctx)=> LoginScreen(),
+        AdminHomePage.routeName: (ctx)=> AdminHomePage(),
+        AdminDeleteAnnouncement.routeName: (ctx)=> AdminDeleteAnnouncement(),
+        AdminViewAnnouncements.routeName: (ctx)=> AdminViewAnnouncements(),
+        MakeAnnouncement.routeName: (ctx)=> MakeAnnouncement(),
+        AdminManageAccount.routeName: (ctx)=> AdminManageAccount(),
+        AddFloorPlan.routeName: (ctx)=> AddFloorPlan(),
+        CalcFloorPlan.routeName: (ctx)=> CalcFloorPlan(),
+        UserHomepage.routeName: (ctx)=> UserHomepage(),
+        UserBookOfficeSpace.routeName: (ctx)=> UserBookOfficeSpace(),
+        UserViewOfficeSpaces.routeName: (ctx)=> UserViewOfficeSpaces(),
+        UserViewCurrentBookings.routeName: (ctx)=> UserViewCurrentBookings(),
+        UserViewAnnouncements.routeName: (ctx)=> UserViewAnnouncements(),
+        UserManageAccount.routeName: (ctx)=> UserManageAccount(),
+      },
+    );
+  }
+}
 
-          Register.routeName: (ctx)=> Register(),
-          LoginScreen.routeName: (ctx)=> LoginScreen(),
-          AdminHomePage.routeName: (ctx)=> AdminHomePage(),
-          AdminDeleteAnnouncement.routeName: (ctx)=> AdminDeleteAnnouncement(),
-          AdminViewAnnouncements.routeName: (ctx)=> AdminViewAnnouncements(),
-          MakeAnnouncement.routeName: (ctx)=> MakeAnnouncement(),
-          AdminManageAccount.routeName: (ctx)=> AdminManageAccount(),
-          AddFloorPlan.routeName: (ctx)=> AddFloorPlan(),
-          CalcFloorPlan.routeName: (ctx)=> CalcFloorPlan(),
-          UserHomepage.routeName: (ctx)=> UserHomepage(),
-          UserBookOfficeSpace.routeName: (ctx)=> UserBookOfficeSpace(),
-          UserViewOfficeSpaces.routeName: (ctx)=> UserViewOfficeSpaces(),
-          UserViewCurrentBookings.routeName: (ctx)=> UserViewCurrentBookings(),
-          UserViewAnnouncements.routeName: (ctx)=> UserViewAnnouncements(),
-          UserManageAccount.routeName: (ctx)=> UserManageAccount(),
-        },
-    ));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Splash();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
