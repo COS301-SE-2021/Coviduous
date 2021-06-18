af90b0806fc6f79d757d9b68ee0eb1e4e08d0c04
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-18 17:57:09 +0200

    Update firestore_cloud.dart
    New function to add an admin, specifically.

--- login_app/lib/models/firestore_cloud.dart
+++ login_app/lib/models/firestore_cloud.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> userSetup(String type, String firstName, String lastName, String username, String companyName, String companyLocation)
+Future<void> userSetup(String firstName, String lastName, String username, String companyId)
 async {
   CollectionReference users = FirebaseFirestore.instance.collection('Users');
   FirebaseAuth auth = FirebaseAuth.instance;
@@ -12,11 +12,30 @@ async {
   String email = auth.currentUser.email.toString();
   users.add({
     'uid': uid,
-    'Type': type,
+    'Type': "User",
     'Firstname': firstName,
     'Lastname' : lastName,
     'Email' : email,
     'Username' : username,
+    'Company Id' :companyId
+  });
+  return;
+}
+
+Future<void> adminSetup(String firstName, String lastName, String username, String companyId, String companyName, String companyLocation)
+async {
+  CollectionReference users = FirebaseFirestore.instance.collection('Users');
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String uid = auth.currentUser.uid.toString();
+  String email = auth.currentUser.email.toString();
+  users.add({
+    'uid': uid,
+    'Type': "Admin",
+    'Firstname': firstName,
+    'Lastname' : lastName,
+    'Email' : email,
+    'Username' : username,
+    'Company ID' : companyId,
     'Company Name' :companyName,
     'Company Location' : companyLocation
   });
