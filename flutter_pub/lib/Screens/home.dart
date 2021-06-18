dcec13eb8e027d54975201264794a58a21771ba0
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:30:04 +0200

    home.dart implementation..

--- flutter_pub/lib/Screens/home.dart
+++ flutter_pub/lib/Screens/home.dart
@@ -8,3 +8,25 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
+  String email = FirebaseAuth.instance.currentUser.email;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+        actions: [
+          IconButton(icon: Icon(Icons.exit_to_app), onPressed: (){
+            //sign out user
+            AuthClass().signOut();
+            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
+          })
+        ],
+      ),
+      body: Center(
+          child: Text("Email $email")
+      ),
+    );
+  }
+}
