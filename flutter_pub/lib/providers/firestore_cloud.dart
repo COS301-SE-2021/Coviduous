130e0688aa66f249614fe5fa503a100c9e4ff993
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 14:32:37 +0200

    added firestore_cloud.dart firebase plugins impl

---
+++ flutter_pub/lib/providers/firestore_cloud.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+//imports for the firebase_core and cloud_firestore plugin
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
