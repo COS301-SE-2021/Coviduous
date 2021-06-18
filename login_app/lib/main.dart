cc999ba5a25d57a9fe4dd62e09c05f8ea71ab9d3
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-18 20:27:45 +0200

    Update main.dart
    Renamed reset password screen to forgot password screen.

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -20,7 +20,7 @@ import 'screens/user_manage_account.dart';
 import 'screens/user_view_office_spaces.dart';
 import 'screens/user_view_current_bookings.dart';
 import 'screens/user_view_announcements.dart';
-import 'screens/reset_password_screen.dart';
+import 'screens/forgot_password_screen.dart';
 import 'services/globals.dart' as globals;
 
 void main() {
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget{
         UserViewCurrentBookings.routeName: (ctx)=> UserViewCurrentBookings(),
         UserViewAnnouncements.routeName: (ctx)=> UserViewAnnouncements(),
         UserManageAccount.routeName: (ctx)=> UserManageAccount(),
-        ResetPage.routeName: (ctx)=> ResetPage(),
+        ForgotPassword.routeName: (ctx)=> ForgotPassword(),
       },
     );
   }
