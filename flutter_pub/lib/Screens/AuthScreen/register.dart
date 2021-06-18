5fc8f73c3e64d518bb147c5d0be877f834875e0e
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 14:57:36 +0200

    register.dart added imports

---
+++ flutter_pub/lib/Screens/AuthScreen/register.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pub/Screens/AuthScreen/login.dart';
+import 'package:flutter_pub/providers/firestore_cloud.dart';
+import 'package:flutter_pub/Screens/home.dart';
+import 'package:flutter_pub/providers/auth_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
