94868a05dfca63cc0cead63752e62f0df1ec8297
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-18 16:27:37 +0200

    structure modification

--- login_app/test/screens/signup_screen_test.dart
+++ login_app/test/screens/signup_screen_test.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:login_app/screens/signup_screen.dart';
+import 'package:login_app/frontend/screens/signup_screen.dart';
 
 void main() {
   setUpAll(() => HttpOverrides.global = null);
@@ -10,19 +10,22 @@ void main() {
   testWidgets('Correct widgets appear', (WidgetTester tester) async {
     //Create widget for testing
     Widget createWidgetForTesting({Widget child}) {
-      return MaterialApp(
-          home: child
-      );
+      return MaterialApp(home: child);
     }
 
     //Build signup screen
     await tester.pumpWidget(createWidgetForTesting(child: new Register()));
 
     //Verify that the correct widgets appear in the correct order
-    expect(find.text('Email'), findsOneWidget); //Find one widget containing 'Email'
-    expect(find.text('Password'), findsOneWidget); //Find one widget containing 'Password'
-    expect(find.text('Confirm Password'), findsOneWidget); //Find one widget containing 'Confirm Password'
-    expect(find.text('Select user type'), findsOneWidget); //Find one widget containing 'Select user type'
-    expect(find.text('Submit'), findsOneWidget); //Find one widget containing 'Submit'
+    expect(find.text('Email'),
+        findsOneWidget); //Find one widget containing 'Email'
+    expect(find.text('Password'),
+        findsOneWidget); //Find one widget containing 'Password'
+    expect(find.text('Confirm Password'),
+        findsOneWidget); //Find one widget containing 'Confirm Password'
+    expect(find.text('Select user type'),
+        findsOneWidget); //Find one widget containing 'Select user type'
+    expect(find.text('Submit'),
+        findsOneWidget); //Find one widget containing 'Submit'
   });
 }
