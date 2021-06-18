f132230e0d22ddc8b0a66883f0ab9fd8973c92c2
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-18 11:21:57 +0200

    structure modifications

---
+++ login_app/lib/subsystems/office_subsystem/booking.dart
@@ -0,0 +1,30 @@
+class Booking {
+  DateTime dateTime;
+  String floorNum = "";
+  String roomNum = "";
+  String user = "";
+  int deskNum = 0;
+//Booking constructor
+
+  Booking(String user, String floorNum, String roomNum, int deskNum) {
+    this.dateTime = DateTime.now();
+    this.floorNum = floorNum;
+    this.user = user;
+    this.roomNum = roomNum;
+    this.deskNum = deskNum;
+  }
+//function display booking
+ // displays booking details..
+  void displayBooking() {
+    print(
+        "***************************************************************************************");
+    print("Displaying Booking Information");
+    print("User : " + this.user);
+    print("Date : " + this.dateTime.toString());
+    print("Floor Number : " + this.floorNum.toString());
+    print("Room Number : " + this.roomNum.toString());
+    print("Desk Number : " + this.deskNum.toString());
+    print(
+        "***************************************************************************************");
+  }
+}
