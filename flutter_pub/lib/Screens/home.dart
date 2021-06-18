e13904314742b67caea43b06e4060b932663dfb5
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:29:30 +0200

    home.dart imports and auth link

---
+++ flutter_pub/lib/Screens/home.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pub/Screens/AuthScreen/login.dart';
+import 'package:flutter_pub/providers/auth_provider.dart';
+//import 'package:flutter_pub/Screens/AuthScreen//register.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
