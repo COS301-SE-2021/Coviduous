5ce6c9da71a0686da2196b1a3fb370069c1087d3
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:28:00 +0200

    imports to splash.dart

---
+++ flutter_pub/lib/Screens/splash.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pub/Screens/AuthScreen//register.dart';
+import 'package:flutter_pub/Screens/home.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
