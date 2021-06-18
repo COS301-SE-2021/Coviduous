d9a47bffeafb2cabf33835ec0e91ba5a119a71a6
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:38:30 +0200

    main.dart App class

--- flutter_pub/lib/main.dart
+++ flutter_pub/lib/main.dart
@@ -7,3 +7,14 @@ void main() {
   Firebase.initializeApp();
   runApp(App());
 }
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+
