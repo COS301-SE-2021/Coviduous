7a7cc77626a39eadfd385be4ba8c83cf6567972c
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-18 15:28:37 +0200

    splash.dart impl

--- flutter_pub/lib/Screens/splash.dart
+++ flutter_pub/lib/Screens/splash.dart
@@ -7,3 +7,27 @@ class Splash extends StatefulWidget {
   @override
   _SplashState createState() => _SplashState();
 }
+class _SplashState extends State<Splash> {
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Future.delayed(const Duration(seconds: 2), (){
+      if(auth.currentUser == null){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => RegisterPage()), (route) => false);
+      }
+      else {
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => HomePage()), (route) => false);
+      }
+    });
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(
+          size: 80,
+        ),
+      ),
+    );
+  }
+}
