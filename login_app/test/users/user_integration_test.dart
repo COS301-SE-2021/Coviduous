09ea30a419648b802dcf6580522f6c4667a17a5a
Peter Okumbe <u18052640@tuks.co.za>
2021-06-19 11:02:36 +0200

    Create user_integration_test.dart

---
+++ login_app/test/users/user_integration_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:postgres/postgres.dart';
+
+void main() {
+  PostgreSQLConnection connection;
+  String host = 'localhost';
+  int port = 5432;
+  String dbName = 'mock_CoviduousDB'; // an existing DB name on your localhost
+  String user = 'postgres';
+  String pass = 'TripleHBK2000'; // your postgres user password
+
+  String expectedValue;
+  String expectedMessage;
+
+  setUp(() async {
+    expectedValue = "test-1";
+
+    //connect to db
+    connection = PostgreSQLConnection(host, port, dbName,
+        username: user, password: pass);
+
+    try {
+      await connection.open();
+      expectedMessage = "Connected to postgres database...";
+      print(expectedMessage);
+    } catch (e) {
+      print("error");
+      print(e.toString());
+    }
+  });
+
+  tearDown(() async {
+    await connection.close();
+  });
+
+  test('Database connection', () async {
+    expect(expectedMessage, "Connected to postgres database...");
+    expect(await connection.execute('select 1'), equals(1));
+  });
+
+  test('Correct Register User', () async {
+    await connection
+        .query('''INSERT INTO users (userid, type, firstName, lastName, username, email, password, activationcode, adminid, companyid)
+                                  VALUES (@id, @type, @fName, @lName, @username, @email, @pass, @a_code, @a_id, @c_id)''',
+            substitutionValues: {
+          'id': expectedValue,
+          'type': expectedValue,
+          'fName': expectedValue,
+          'lName': expectedValue,
+          'username': expectedValue,
+          'email': expectedValue,
+          'pass': expectedValue,
+          'a_code': expectedValue,
+          'a_id': expectedValue,
+          'c_id': expectedValue,
+        });
+
+    var results = await connection.query("SELECT * FROM users");
+
+    //print(results);
+
+    expect(results.length, isNot(0));
+  });
+
+  test('Correct Delete User Account', () async {
+    var results = await connection.query("DELETE FROM users WHERE userid = @id",
+        substitutionValues: {'id': expectedValue});
+
+    //print(results);
+
+    expect(results.length, 0);
+  });
+
+  test('Register Company', () async {
+    await connection
+        .query('''INSERT INTO company (companyid, name, address, adminid)
+                                  VALUES (@id, @name, @address, @a_id)''',
+            substitutionValues: {
+          'id': expectedValue,
+          'name': expectedValue,
+          'address': expectedValue,
+          'a_id': expectedValue,
+        });
+
+    var results = await connection.query("SELECT * FROM company");
+
+    //print(results);
+
+    expect(results.length, isNot(0));
+  });
+
+
+
+}
