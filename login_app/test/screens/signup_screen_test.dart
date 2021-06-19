aef67ad90cd594d12a19d8917b38d30800b4d4bf
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-19 13:49:39 +0200

    Fixing tests

--- login_app/test/screens/signup_screen_test.dart
+++ login_app/test/screens/signup_screen_test.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../../lib/frontend/screens/login_screen.dart';
+import '../../lib/frontend/screens/home_signup_screen.dart';
 
 void main() {
   setUpAll(() => HttpOverrides.global = null);
@@ -15,14 +15,14 @@ void main() {
       );
     }
 
-    //Build login screen
-    await tester.pumpWidget(createWidgetForTesting(child: new LoginScreen()));
+    //Build signup screen
+    await tester.pumpWidget(createWidgetForTesting(child: new Register()));
 
     //Verify that the correct widgets appear in the correct order
     expect(find.text('Email'), findsOneWidget); //Find one widget containing 'Email'
     expect(find.text('Password'), findsOneWidget); //Find one widget containing 'Password'
-    expect(find.text('Admin'), findsOneWidget); //Find one widget containing 'Admin'
-    expect(find.text('User'), findsOneWidget); //Find one widget containing 'User'
+    expect(find.text('Confirm Password'), findsOneWidget); //Find one widget containing 'Confirm Password'
+    expect(find.text('Select user type'), findsOneWidget); //Find one widget containing 'Select user type'
     expect(find.text('Submit'), findsOneWidget); //Find one widget containing 'Submit'
   });
 }
