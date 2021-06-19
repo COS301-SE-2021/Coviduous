8fdfcf66d42ed770288e627e182896f145fa039f
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-19 21:10:31 +0200

    Update home_signup_screen.dart
    Made this page prettier.

--- login_app/lib/frontend/screens/home_signup_screen.dart
+++ login_app/lib/frontend/screens/home_signup_screen.dart
@@ -31,71 +31,99 @@ class _RegisterState extends State<Register>{
 
     return WillPopScope(
       onWillPop: () async => false, //Prevent the back button from working
-      child: isLoading == false ? Scaffold(
-        appBar: AppBar(
-          title: Text('Register'),
-          automaticallyImplyLeading: false, //Back button will not show up in app bar
-          actions: <Widget>[
-            TextButton(
-              child: Row(
-                children: <Widget>[
-                  Text('Login '),
-                  Icon(Icons.person)
-                ],
-              ),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-              ),
-              onPressed: (){
-                Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
-              },
-            )
-          ],
-        ),
-        body: Center(
-          child: Container(
-            height: MediaQuery.of(context).size.height/(2*globals.getWidgetScaling()),
-            width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
-            padding: EdgeInsets.all(16),
+      child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/bg.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        child: isLoading == false ? Scaffold(
+          backgroundColor: Colors.transparent, //To show background image
+          appBar: AppBar(
+            title: Text('Register'),
+            automaticallyImplyLeading: false, //Back button will not show up in app bar
+            actions: <Widget>[
+              TextButton(
+                child: Row(
+                  children: <Widget>[
+                    Text('Login '),
+                    Icon(Icons.person)
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: (){
+                  Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+                },
+              )
+            ],
+          ),
+          body: Center(
             child: Column (
-                children: <Widget>[
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom (
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container (
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(20.0),
+                    child: Image(
+                      alignment: Alignment.center,
+                      image: AssetImage('assets/placeholder.com-logo1.png'),
+                      color: Colors.white,
+                      width: double.maxFinite,
+                      height: MediaQuery.of(context).size.height/8,
                     ),
-                    child:(
-                      Text('Admin signup')
-                    ),
-                    onPressed:() {
-                      Navigator.of(context).pushReplacementNamed(
-                          AdminRegister.routeName);
-                    }
                   ),
                   SizedBox (
                     height: MediaQuery.of(context).size.height/48,
                     width: MediaQuery.of(context).size.width,
                   ),
-                  ElevatedButton(
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height/20,
+                    width: MediaQuery.of(context).size.width/1.5,
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom (
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                       child:(
-                          Text('User signup')
+                        Text('Admin signup')
                       ),
                       onPressed:() {
                         Navigator.of(context).pushReplacementNamed(
-                            UserRegister.routeName);
+                            AdminRegister.routeName);
                       }
+                    ),
+                  ),
+                  SizedBox (
+                    height: MediaQuery.of(context).size.height/48,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height/20,
+                    width: MediaQuery.of(context).size.width/1.5,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom (
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child:(
+                            Text('User signup')
+                        ),
+                        onPressed:() {
+                          Navigator.of(context).pushReplacementNamed(
+                              UserRegister.routeName);
+                        }
+                    ),
                   ),
                 ]
-            )
-          ),
-        )
-      ) : Center( child: CircularProgressIndicator())
+            ),
+          )
+        ) : Center( child: CircularProgressIndicator())
+      ),
     );
   }
 }
