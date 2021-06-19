19e1e7c18a40964e1742b64351333530b7617c13
u18202374-Chaks <u18202374@tuks.co.za>
2021-06-19 17:58:19 +0200

    Register Updated

--- login_app/lib/responses/user_responses/RegisterAdminResponse.dart
+++ login_app/lib/responses/user_responses/RegisterAdminResponse.dart
@@ -3,4 +3,28 @@ class RegisterAdminResponse {
   String adminID;
   bool response;
   String resMessage;
+
+  RegisterAdminResponse(
+      String userid, String adminid, bool res, String resmess) {
+    this.userID = userid;
+    this.adminID = adminid;
+    this.response = res;
+    this.resMessage = resmess;
+  }
+
+  String getUserID() {
+    return userID;
+  }
+
+  String getAdminID() {
+    return adminID;
+  }
+
+  bool getResponse() {
+    return response;
+  }
+
+  String getMessage() {
+    return resMessage;
+  }
 }
