c45ac418318ac3a3e65a3a94f9deff30509ae469
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 00:54:52 +0200

    Added register user mock impl.

--- login_app/lib/backend/controllers/user_controller.dart
+++ login_app/lib/backend/controllers/user_controller.dart
@@ -3,13 +3,11 @@ import 'package:login_app/backend/server_connections/user_data_base_queries.dart
 import 'package:login_app/requests/announcements_requests/create_announcement_request.dart';
 import 'package:login_app/requests/announcements_requests/delete_announcement_request.dart';
 import 'package:login_app/requests/announcements_requests/viewAdmin_announcement_request.dart';
-import 'package:login_app/requests/user_requests/DeleteAccountUserRequest.dart';
-import 'package:login_app/requests/user_requests/UpdateAccountInfoRequest.dart';
+import 'package:login_app/requests/user_requests/RegisterUserRequest.dart';
 import 'package:login_app/responses/announcement_responses/create_announcement_response.dart';
 import 'package:login_app/responses/announcement_responses/delete_announcement_response.dart';
 import 'package:login_app/responses/announcement_responses/viewAdmin_announcement_response.dart';
-import 'package:login_app/responses/user_responses/DeleteAccountUserResponse.dart';
-import 'package:login_app/responses/user_responses/UpdateAccountInfoResponse.dart';
+import 'package:login_app/responses/user_responses/RegisterUserResponse.dart';
 
 class UserController {
 //This class provides an interface to all the announcement service contracts of the system. It provides a bridge between the front end screens and backend functionality for announcements.
@@ -22,116 +20,34 @@ class UserController {
     this.userQueries = new UserDatabaseQueries();
   }
 
-  // Future<CreateAnnouncementResponse> createAnnouncement(
-  //     CreateAnnouncementRequest req) async {
-  //   if (req != null) {
-  //     if (await announcementQueries.createAnnouncement(req.getType(),
-  //             req.getMessage(), req.getAdminID(), req.getCompanyID()) ==
-  //         true) {
-  //       return new CreateAnnouncementResponse(
-  //           announcementQueries.getAnnouncementID(),
-  //           announcementQueries.getTimestamp(),
-  //           true,
-  //           "Successfully Created Announcement");
-  //     } else {
-  //       //throw new Exception("Announcement unsuccessfully created");
-  //       return new CreateAnnouncementResponse(
-  //           null, null, false, "Announcement unsuccessfully created");
-  //     }
-  //   } else {
-  //     //throw new Exception("Announcement unsuccessfully created");
-  //     return new CreateAnnouncementResponse(
-  //         null, null, false, "Announcement unsuccessfully created");
-  //   }
-  // }
-
-  // Future<ViewAdminAnnouncementResponse> viewAdminAnnouncement(
-  //     ViewAdminAnnouncementRequest req) async {
-  //   if (req != null) {
-  //     if (await announcementQueries
-  //             .viewAdminAnnouncement(req.getAnnouncement_id()) ==
-  //         true) {
-  //       return new ViewAdminAnnouncementResponse(
-  //           null, null, null, true, "Announcement successfully found");
-  //     } else {
-  //       //throw new Exception("Announcement unsuccessfully found");
-  //       return new ViewAdminAnnouncementResponse(
-  //           null, null, null, false, "Announcement unsuccessfully found");
-  //     }
-  //   } else {
-  //     //throw new Exception("Announcement unsuccessfully found");
-  //     return new ViewAdminAnnouncementResponse(
-  //         null, null, null, false, "Announcement unsuccessfully found");
-  //   }
-  // }
-
-  // Future<DeleteAnnouncementResponse> deleteAnnouncement(
-  //     DeleteAnnouncementRequest req) async {
-  //   if (req != null) {
-  //     if (await announcementQueries
-  //             .deleteAnnouncement(req.getAnnouncementId()) ==
-  //         true) {
-  //       return new DeleteAnnouncementResponse(
-  //           true, "Successfully Deleted Announcement");
-  //     } else {
-  //       //throw new Exception("Announcement unsuccessfully deleted");
-  //       return new DeleteAnnouncementResponse(
-  //           false, "Announcement unsuccessfully deleted");
-  //     }
-  //   } else {
-  //     //throw new Exception("Announcement unsuccessfully deleted");
-  //     return new DeleteAnnouncementResponse(
-  //         false, "Announcement unsuccessfully deleted");
-  //   }
-  // }
-
   // //////////////////////////////////Mocked Implementations/////////////////////////////////////////////////////////////////////
-  // //create Announcement Mock
+  // //register user Mock
   // /**
-  //  * This function is used to test if the logic and implementation of creating an announcement works
+  //  * This function is used to test if the logic and implementation of creating a user works
   //  */
-  // CreateAnnouncementResponse createAnnouncementMock(
-  //     CreateAnnouncementRequest req) {
-  //   if (req != null) {
-  //     if (announcementQueries.createAnnouncementMock(req.getType(),
-  //         req.getMessage(), req.getAdminID(), req.getCompanyID())) {
-  //       return new CreateAnnouncementResponse(
-  //           announcementQueries.getAnnouncementID(),
-  //           announcementQueries.getTimestamp(),
-  //           true,
-  //           "Successfully Created Announcement");
-  //     } else {
-  //       // throw new Exception("Announcement unsuccessfully created");
-  //       return new CreateAnnouncementResponse(
-  //           null, null, false, "Announcement unsuccessfully created");
-  //     }
-  //   } else {
-  //     // throw new Exception("Announcement unsuccessfully created");
-  //     return new CreateAnnouncementResponse(
-  //         null, null, false, "Announcement unsuccessfully created");
-  //   }
-  // }
-  /*DeleteAccountUserResponse deleteUserAccount(DeleteAccountUserRequest request) throws Exception {
-		if(userQueries.deleteUserAccount(request.getUserID()))
-		{
-			return new DeleteAccountUserResponse("Successfully Deleted");
-		}
-		else
-		{
-			throw new Exception("User unsuccessfully deleted ");
-		}
-	}*/
-  /*UpdateAccountInfoResponse updateInfoAccount(UpdateAccountInfoRequest req) throws Exception
-	{
-		if(userQueries.updateAccountInfo(req.getFirstname(),req.getLastname(),req.getEmail(),req.getType(),req.getAdminID(),req.getUserID()))
-		{
-			return new UpdateAccountInfoResponse("Successfully Updated");
-		}
-		else
-		{
-			throw new Exception("Update account unsuccessfully");
-		}
-	}
-
-*/
+  RegisterUserResponse registerUserMock(RegisterUserRequest req) {
+    if (req != null) {
+      if (userQueries.registerUserMock(
+          req.getType(),
+          req.getFirstName(),
+          req.getLastName(),
+          req.getUsername(),
+          req.getEmail(),
+          req.getPassword(),
+          req.getCompanyID())) {
+        return new RegisterUserResponse(
+            userQueries.getUserID(),
+            userQueries.getAdminID(),
+            userQueries.getActivationCode(),
+            true,
+            "Successfully Registered User");
+      } else {
+        return new RegisterUserResponse(
+            null, null, null, false, "Unsuccessfully registered user");
+      }
+    } else {
+      return new RegisterUserResponse(
+          null, null, null, false, "Unsuccessfully registered user");
+    }
+  }
 }
