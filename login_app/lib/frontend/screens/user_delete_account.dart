a4e7c841e99a0f66471ae3b66485a8adf5fe6c00
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-06-19 21:09:27 +0200

    Create user_delete_account.dart
    Splitting delete_user_account into two pages, since it was giving issues as just one page.

---
+++ login_app/lib/frontend/screens/user_delete_account.dart
@@ -0,0 +1,191 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../frontend/models/auth_provider.dart';
+import '../../frontend/screens/user_manage_account.dart';
+import 'login_screen.dart';
+import '../front_end_globals.dart' as globals;
+
+class UserDeleteAccount extends StatefulWidget {
+  static const routeName = "/userDeleteAccount";
+  @override
+  _UserDeleteAccountState createState() => _UserDeleteAccountState();
+}
+
+FirebaseAuth auth = FirebaseAuth.instance;
+User admin = FirebaseAuth.instance.currentUser;
+DocumentSnapshot snap = FirebaseFirestore.instance.collection('Users').doc(admin.uid).get() as DocumentSnapshot;
+String _type = snap.get('Type');
+String _companyId = snap.get('Company ID');
+String _email = snap.get('Email');
+String _password = snap.get('Password');
+
+class _UserDeleteAccountState extends State<UserDeleteAccount>{
+  TextEditingController _userEmail = TextEditingController();
+  TextEditingController _userPassword = TextEditingController();
+  TextEditingController _confirmUserPassword = TextEditingController();
+  TextEditingController _userCompanyId = TextEditingController();
+  bool isLoading = false;
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading == false ? Scaffold(
+      appBar: AppBar(
+        title: Text('Delete user'),
+        leading: BackButton( //Specify back button
+          onPressed: (){
+            Navigator.of(context).pushReplacementNamed(UserManageAccount.routeName);
+          },
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: SingleChildScrollView( //So the element doesn't overflow when you open the keyboard
+              child: Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height/(2.8*globals.getWidgetScaling()),
+                width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          //email
+                          TextFormField(
+                            textInputAction: TextInputAction.next, //The "return" button becomes a "next" button when typing
+                            decoration: InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _userEmail,
+                            validator: (value) {
+                              if(value.isEmpty || !value.contains('@')) {
+                                return 'invalid email';
+                              } else if (value != _email) {
+                                return 'email does not exist in database';
+                              }
+                              return null;
+                            },
+                          ),
+                          //password
+                          TextFormField(
+                            textInputAction: TextInputAction.next, //The "return" button becomes a "next" button when typing
+                            decoration: InputDecoration(labelText:'Password'),
+                            obscureText: true,
+                            controller: _userPassword,
+                            validator: (value) {
+                              if(value.isEmpty) {
+                                return 'please input a password';
+                              } else if (value != _password) {
+                                return 'invalid password';
+                              }
+                              return null;
+                            },
+                          ),
+                          //confirm password
+                          TextFormField(
+                            textInputAction: TextInputAction.next, //The "return" button becomes a "next" button when typing
+                            decoration: InputDecoration(labelText:'Confirm password'),
+                            obscureText: true,
+                            controller: _confirmUserPassword,
+                            validator: (value) {
+                              if(value.isEmpty) {
+                                return 'please input a password';
+                              } else if (value != _password) {
+                                return 'passwords do not match';
+                              }
+                              return null;
+                            },
+                          ),
+                          //company ID
+                          TextFormField(
+                            textInputAction: TextInputAction.next, //The "return" button becomes a "next" button when typing
+                            decoration: InputDecoration(labelText:'Company ID'),
+                            obscureText: true,
+                            controller: _userCompanyId,
+                            validator: (value) {
+                              if (value.isEmpty || value != _companyId) {
+                                return 'incorrect company ID';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox (
+                            height: MediaQuery.of(context).size.height/48,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          ElevatedButton(
+                            child: Text(
+                                'Remove'
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (ctx) => AlertDialog(
+                                    title: Text('Warning'),
+                                    content: Text('Are you sure you want to delete your account? This cannot be undone.'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text('Yes'),
+                                        onPressed: (){
+                                          setState(() {
+                                            isLoading = true;
+                                          });
+                                          String oldEmail = FirebaseAuth.instance.currentUser.email;
+                                          AuthClass().deleteAccount().then((value) {
+                                            if (value == "Success") {
+                                              setState(() {
+                                                isLoading = false;
+                                              });
+
+                                              //If delete was successful, delete from Firestore as well
+                                              FirebaseFirestore.instance.runTransaction((Transaction transaction) async {
+                                                var query = FirebaseFirestore.instance.collection('Users')
+                                                    .where("Email", isEqualTo: oldEmail);
+                                                var querySnapshot = await query.get();
+                                                String id = querySnapshot.docs.first.id;
+                                                FirebaseFirestore.instance.collection('Users').doc(id).delete();
+                                              });
+
+                                              AuthClass().signOut();
+                                              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginScreen()), (route) => false);
+                                            } else {
+                                              setState(() {
+                                                isLoading = false;
+                                              });
+                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                  SnackBar(content: Text(value)));
+                                            }
+                                          });
+                                        },
+                                      ),
+                                      TextButton(
+                                        child: Text('No'),
+                                        onPressed: (){
+                                          Navigator.of(ctx).pop();
+                                        },
+                                      )
+                                    ],
+                                  ));
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    ) : Center( child: CircularProgressIndicator());
+  }
+}
