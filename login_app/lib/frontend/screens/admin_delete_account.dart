548e3450992c9c4dbb6a178301c7db1cac022aae
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-20 15:29:35 +0200

    admin_delete_account.dart background.

--- login_app/lib/frontend/screens/admin_delete_account.dart
+++ login_app/lib/frontend/screens/admin_delete_account.dart
@@ -38,6 +38,12 @@ class _AdminDeleteAccountState extends State<AdminDeleteAccount>{
   @override
   Widget build(BuildContext context) {
     return isLoading == false ? Scaffold(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
       appBar: AppBar(
         title: Text('Delete user'),
         leading: BackButton( //Specify back button
