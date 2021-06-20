114ea02ca4d014bf5d2bd335eb2de08669e356d4
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-20 06:47:57 +0200

    register mock

--- login_app/test/user/user_unit_test.dart
+++ login_app/test/user/user_unit_test.dart
@@ -2,9 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:login_app/backend/controllers/user_controller.dart';
 import 'package:login_app/requests/user_requests/RegisterUserRequest.dart';
 import 'package:login_app/responses/user_responses/RegisterUserResponse.dart';
-import 'package:login_app/subsystems/user_subsystem/user.dart';
-import 'package:login_app/backend/backend_globals/announcements_globals.dart'
-    as globals;
+import 'package:login_app/backend/backend_globals/user_globals.dart' as globals;
 
 void main() {
   ///////////////////////////////////// These Unit Tests Test Mocked Functionality For The User System ///////////
@@ -14,18 +12,22 @@ void main() {
   test('Register User Mock', () {
     var userController = UserController();
     print("/////////////////// Testing Mock Register User ///////////////////");
-    RegisterUserRequest req = new RegisterUserRequest("Admin", "John", "Jacobs",
-        "johnjacobs99", "johnjacobs99@gmail.com", "jj-p678", "CMPNY-1");
+    RegisterUserRequest req = new RegisterUserRequest("Admin", "Njabulo",
+        "Skosana", "Nskosana", "njabuloskosana24@gmail", "Ns01", "CID-1");
     RegisterUserResponse resp = userController.registerUserMock(req);
 
-    print("Response : " + resp.getResponseMessage());
-    print("UserID : " + resp.getUserID());
-    print("AdminID : " + resp.getAdminID());
-    print("Activation code : " + resp.getActivationCode());
+    print("Current Users Registered");
+    for (var i = 0; i < globals.userDatabaseTable.length; i++) {
+      print("First Name: " + globals.userDatabaseTable[i].getFirstName());
+      print("Last Name: " + globals.userDatabaseTable[i].getLastName());
+      print("User ID: " + globals.userDatabaseTable[i].getUserId());
+      print("Company ID: " + globals.userDatabaseTable[i].getCompanyId());
+      print("////////////////////////////");
+    }
 
     print(
-        "/////////////////// Completed Mock Testing For Invalid Register User Request ///////////////////");
-    expect(globals.numUsers, isNot(0));
+        "/////////////////// Completed Mock Testing For valid Register User Request ///////////////////");
+    expect(resp.getResponse(), true);
   });
 
   test('Multiple User Registration Mock', () {
@@ -33,28 +35,27 @@ void main() {
     print(
         "/////////////////// Testing Multiple User Registration ///////////////////");
     RegisterUserRequest req = new RegisterUserRequest("Admin", "John", "Jacobs",
-        "johnjacobs99", "johnjacobs99@gmail.com", "jj-p678", "CMPNY-1");
+        "Jacobs", "johnjacobs99@gmail.com", "Ns01", "CID-1");
     RegisterUserResponse resp = userController.registerUserMock(req);
 
-    print("====USER-1====");
-    print("Response : " + resp.getResponseMessage());
-    print("UserID : " + resp.getUserID());
-    print("AdminID : " + resp.getAdminID());
-    print("Activation code : " + resp.getActivationCode());
+    RegisterUserRequest reqes = new RegisterUserRequest("User", "Adam", "Paul",
+        "adamlaup12", "johnjacobs99@gmail.com", "a_paul@yahoo.com", "CID-2");
+    RegisterUserResponse respn = userController.registerUserMock(reqes);
 
-    req = new RegisterUserRequest("User", "Adam", "Paul", "adamlaup12",
-        "a_paul@yahoo.com", "pauladamisthebest", "CMPNY-2");
-    resp = userController.registerUserMock(req);
-
-    print("====USER-2====");
-    print("Response : " + resp.getResponseMessage());
-    print("UserID : " + resp.getUserID());
-    print("AdminID : " + resp.getAdminID());
-    print("Activation code : " + resp.getActivationCode());
+    print("Current Users Registered");
+    for (var i = 0; i < globals.userDatabaseTable.length; i++) {
+      print("First Name: " + globals.userDatabaseTable[i].getFirstName());
+      print("Last Name: " + globals.userDatabaseTable[i].getLastName());
+      print("User ID: " + globals.userDatabaseTable[i].getUserId());
+      print("Company ID: " + globals.userDatabaseTable[i].getCompanyId());
+      print("////////////////////////////");
+    }
 
     print(
         "/////////////////// Completed Mock Testing For Multiple User Registration ///////////////////");
     expect(globals.numUsers, 2);
+    expect(resp.getResponse(), true);
+    expect(respn.getResponse(), true);
   });
 
   test('Invalid register user request', () {
