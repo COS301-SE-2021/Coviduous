3f92b66393926da16742e9ef400a9cf381954d16
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 11:12:33 +0200

    Added bool response param to view user announcement response class

--- login_app/lib/responses/announcement_responses/viewUser_announcement_response.dart
+++ login_app/lib/responses/announcement_responses/viewUser_announcement_response.dart
@@ -1,19 +1,25 @@
 import 'package:login_app/subsystems/announcement_subsystem/announcement.dart';
 
 class ViewUserAnnouncementResponse {
-  String message;
   List<Announcement> announcementArrayList;
+  bool response;
+  String message;
 
-  ViewUserAnnouncementResponse(List<Announcement> list, String Message) {
-    this.message = Message;
+  ViewUserAnnouncementResponse(List<Announcement> list, bool response, String Message) {
     this.announcementArrayList = list;
-  }
-
-  String getMessage() {
-    return message;
+    this.response = response;
+    this.message = Message;
   }
 
   List<Announcement> getUserAnnouncements() {
     return announcementArrayList;
+  }
+
+  bool getResponse() {
+    return response;
+  }
+
+  String getMessage() {
+    return message;
   }
 }
