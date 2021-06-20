a76a63aea9da2d183d066df02ede8e1c300ebb66
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 11:49:36 +0200

    Update user_integration_test.dart

--- login_app/test/user/user_integration_test.dart
+++ login_app/test/user/user_integration_test.dart
@@ -12,10 +12,16 @@ void main() {
   String expectedValue;
   String expectedUpdate;
   String expectedMessage;
+  String expectedUserID;
+  String expectedAdminID;
+  String expectedCompanyID;
 
   setUp(() async {
     expectedValue = "test-1";
     expectedUpdate = "test-2";
+    expectedUserID = "USR-1";
+    expectedAdminID = "ADMN-1";
+    expectedCompanyID = "CMPNY-1";
 
     //connect to db
     connection = PostgreSQLConnection(host, port, dbName,
@@ -45,7 +51,7 @@ void main() {
         .query('''INSERT INTO users (userid, type, firstName, lastName, username, email, password, activationcode, adminid, companyid)
                                   VALUES (@id, @type, @fName, @lName, @username, @email, @pass, @a_code, @a_id, @c_id)''',
             substitutionValues: {
-          'id': expectedValue,
+          'id': expectedUserID,
           'type': expectedValue,
           'fName': expectedValue,
           'lName': expectedValue,
@@ -53,8 +59,8 @@ void main() {
           'email': expectedValue,
           'pass': expectedValue,
           'a_code': expectedValue,
-          'a_id': expectedValue,
-          'c_id': expectedValue,
+          'a_id': expectedAdminID,
+          'c_id': expectedCompanyID,
         });
 
     var results = await connection.query("SELECT * FROM users");
@@ -64,29 +70,29 @@ void main() {
     expect(results.length, isNot(0));
   });
 
-  // test('Correct Register Company', () async {
-  //   await connection
-  //       .query('''INSERT INTO company (companyid, name, address, adminid)
-  //                                 VALUES (@id, @name, @address, @a_id)''',
-  //           substitutionValues: {
-  //         'id': expectedValue,
-  //         'name': expectedValue,
-  //         'address': expectedValue,
-  //         'a_id': expectedValue,
-  //       });
+  test('Correct Register Company', () async {
+    await connection
+        .query('''INSERT INTO company (companyid, name, address, adminid)
+                                  VALUES (@id, @name, @address, @a_id)''',
+            substitutionValues: {
+          'id': expectedCompanyID,
+          'name': expectedValue,
+          'address': expectedValue,
+          'a_id': expectedAdminID,
+        });
 
-  //   var results = await connection.query("SELECT * FROM company");
+    var results = await connection.query("SELECT * FROM company");
 
-  //   //print(results);
+    //print(results);
 
-  //   expect(results.length, isNot(0));
-  // });
+    expect(results.length, isNot(0));
+  });
 
   test('Correct Update account info', () async {
     var results = await connection.query(
         "UPDATE users SET firstName = @fName, lastName = @lName, email = @email WHERE userid = @id",
         substitutionValues: {
-          'id': expectedValue,
+          'id': expectedUserID,
           'fName': expectedUpdate,
           'lName': expectedUpdate,
           'email': expectedUpdate,
@@ -100,7 +106,7 @@ void main() {
   test('Correct reset password', () async {
     var results = await connection.query(
         "UPDATE users SET password = @pass WHERE userid = @id",
-        substitutionValues: {'id': expectedValue, 'pass': expectedUpdate});
+        substitutionValues: {'id': expectedUserID, 'pass': expectedUpdate});
 
     //print(results);
 
@@ -109,7 +115,7 @@ void main() {
 
   test('Correct Delete User Account', () async {
     var results = await connection.query("DELETE FROM users WHERE userid = @id",
-        substitutionValues: {'id': expectedValue});
+        substitutionValues: {'id': expectedUserID});
 
     //print(results);
 
