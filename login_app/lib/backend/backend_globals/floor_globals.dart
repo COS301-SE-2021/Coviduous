7a356f6619d01c6aaf392a62f2ef07e4ccba3b4e
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-20 12:04:37 +0200

    code cleanup

--- login_app/lib/backend/backend_globals/floor_globals.dart
+++ login_app/lib/backend/backend_globals/floor_globals.dart
@@ -6,5 +6,10 @@ import 'package:login_app/subsystems/floorplan_subsystem/floor.dart';
 
 //Backend global variables
 //==========================
+/**
+ * List<Floor> globalFloors  acts like a database table that holds floors , this is to mock out functionality for testing
+ * globalNumFloors keeps track of number of floors in the mock floors database table
+ * Floors refer to the buildings floors, each floor is a level inside the building.
+ */
 List<Floor> globalFloors = [];
 int globalNumFloors = 0;
