44fdd3cd54dae42384a135c3c4c4302e144b9c23
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-20 05:06:12 +0200

    user subsystem

--- login_app/lib/subsystems/user_subsystem/user.dart
+++ login_app/lib/subsystems/user_subsystem/user.dart
@@ -9,14 +9,13 @@ class User {
   String email;
   String password;
   String activation_code;
-  String adminId;
   String companyId;
 
   User(String type, String FirstName, String LastName, String Username,
-      String Email, String Password, String adminID, String companyID) {
+      String Email, String Password, String companyID) {
     int randomInt = new Random().nextInt((9999 - 100) + 1) + 10;
     int randomInt2 = new Random().nextInt((9999 - 100) + 1) + 10;
-    
+
     this.userId = "USR-" + randomInt.toString();
     this.type = type;
     this.first_name = FirstName;
@@ -24,7 +23,6 @@ class User {
     this.email = Email;
     this.password = Password;
     this.activation_code = "ACTIV-" + randomInt2.toString();
-    this.adminId = adminID;
     this.companyId = companyID;
   }
 
