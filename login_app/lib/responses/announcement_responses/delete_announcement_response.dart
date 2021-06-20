cc84f299a1925b03d99a6676fad03d03ef31c7a1
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 16:52:45 +0200

    Update delete_announcement_response.dart

--- login_app/lib/responses/announcement_responses/delete_announcement_response.dart
+++ login_app/lib/responses/announcement_responses/delete_announcement_response.dart
@@ -1,3 +1,12 @@
+/*
+  File name: delete_announcement_response.dart
+  Purpose: Holds the response class of deleting an announcement
+  Collaborators:
+    - Njabulo Skosana
+    - Peter Okumbe
+    - Chaoane Malakoane
+ */
+
 /**
  * This class holds the response object for deleting an announcement by the admin
  */
