7c6b6651c3e9e943154779770695e00b33b488ab
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 11:09:35 +0200

    Added bool response param to view Admin response class

--- login_app/lib/responses/announcement_responses/viewAdmin_announcement_response.dart
+++ login_app/lib/responses/announcement_responses/viewAdmin_announcement_response.dart
@@ -1,19 +1,26 @@
 import 'package:login_app/subsystems/announcement_subsystem/announcement.dart';
 
 class ViewAdminAnnouncementResponse {
-  String message;
   List<Announcement> announcementArrayList;
+  bool response;
+  String message;
 
-  ViewAdminAnnouncementResponse(List<Announcement> list, String Message) {
-    this.message = Message;
+  ViewAdminAnnouncementResponse(List<Announcement> list, bool response, String Message) {
     this.announcementArrayList = list;
+    this.response = response;
+    this.message = Message;
+  }
+
+  List<Announcement> getAdminAnnouncements() {
+    return announcementArrayList;
+  }
+
+  bool getResponse() {
+    return response;
   }
 
   String getMessage() {
     return message;
   }
 
-  List<Announcement> getAdminAnnouncements() {
-    return announcementArrayList;
-  }
 }
