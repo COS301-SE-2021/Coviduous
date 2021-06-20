adf5fe70de4c79f398bbe92da41be68480e2175f
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-20 12:30:10 +0200

    code cleanup

--- login_app/lib/backend/controllers/floor_plan_controller.dart
+++ login_app/lib/backend/controllers/floor_plan_controller.dart
@@ -5,16 +5,23 @@ import 'package:login_app/subsystems/floorplan_subsystem/floor.dart';
 import 'package:login_app/backend/backend_globals/floor_globals.dart'
     as floorGlobals;
 
+/**
+ * This class is the controller for floorplan, all service contracts for the floorplan subsystem are offered through this class
+ * The class has both mock and concrete implementations of the service contracts.
+ */
 class FloorPlanController {
 //This class provides an interface to all the floorplan service contracts of the system. It provides a bridge between the front end screens and backend functionality for floor plan.
   /** 
-   * announcementQueries attribute holds the class that provides access to the database , the attribute allows you to access functions that will handle database interaction.
+   * floorPlanQueries attribute holds the class that provides access to the database , the attribute allows you to access functions that will handle database interaction.
    */
   FloorPlanDatabaseQueries floorPlanQueries;
   FloorPlanController() {
     this.floorPlanQueries = new FloorPlanDatabaseQueries();
   }
 
+/**
+ * createFloorPlanMock : Mocks out the functionality of creating a floor plan for a building
+ */
   CreateFloorPlanResponse createFloorPlanMock(CreateFloorPlanRequest req) {
     var holder =
         new Floor(req.getAdmin(), req.getFloorNumber(), req.getTotalRooms());
@@ -25,6 +32,10 @@ class FloorPlanController {
     return resp;
   }
 
+////////////////////////////////////////////////////////////
+/**
+ * For every floor, there are rooms assosiated with a floor , this function allows you to populate rooms for each floor
+ */
   bool addRoomMock(String floorNum, String roomNum, double dimensions,
       double percentage, int numDesks, double deskLength, double deskWidth) {
     for (int i = 0; i < floorGlobals.globalFloors.length; i++) {
@@ -39,7 +50,11 @@ class FloorPlanController {
     return false;
   }
 
+/**
+ * This function returns the number of floors in a building
+ */
   int getNumberOfFloors() {
     return floorGlobals.globalNumFloors;
   }
+  ////////////////////////////////////////////////////////////////////////////////
 }
