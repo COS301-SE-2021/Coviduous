a505cc1cd923d496c74bbe9e8272a142ff77f0fa
Peter Okumbe <u18052640@tuks.co.za>
2021-06-20 15:53:36 +0200

    Added coding standards headers

--- login_app/lib/backend/backend_globals/announcements_globals.dart
+++ login_app/lib/backend/backend_globals/announcements_globals.dart
@@ -1,3 +1,11 @@
+/*
+  File name: announcements_globals.dart
+  Purpose: Global variables used for integration with front and backend.
+  Collaborators:
+    - Njabulo Skosana
+    - Peter Okumbe
+    - Chaoane Malakoane
+ */
 library globals;
 
 import 'package:login_app/subsystems/announcement_subsystem/announcement.dart';
