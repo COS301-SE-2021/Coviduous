4adda8bff90af94ff82e93ae63d59bc467e7153d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-29 04:12:50 +0200

    admin_modify_floor-plan.dart create floor list.

--- login_app/lib/frontend/screens/admin_modify_floor-plan.dart
+++ login_app/lib/frontend/screens/admin_modify_floor-plan.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 
 
 class ModifyFloorPlan extends StatefulWidget {
-  static const routeName = "/modifyplan";
+  static const routeName = "/modify_plan";
   @override
   _ModifyFloorPlanState createState() => _ModifyFloorPlanState();
 }
 
 class _ModifyFloorPlanState extends State<ModifyFloorPlan> {
-    String _chosenValue;
+    //String _chosenValue;
 
     @override
     Widget build(BuildContext context) {
@@ -19,7 +19,16 @@ class _ModifyFloorPlanState extends State<ModifyFloorPlan> {
           backgroundColor: Colors.grey,
         ),
         backgroundColor: Colors.grey[850],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
 
+              ),
+            ],
+          ),
+        )
       );
 
   }
