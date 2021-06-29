95e631e5ef013e016aae3b4a8e332e7acceb6620
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-29 04:07:28 +0200

    admin_modify_floor-plan.dart class and State

---
+++ login_app/lib/frontend/screens/admin_modify_floor-plan.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class ModifyFloorPlan extends StatefulWidget {
+  static const routeName = "/modifyplan";
+  @override
+  _ModifyFloorPlanState createState() => _ModifyFloorPlanState();
+}
+
+class _ModifyFloorPlanState extends State<ModifyFloorPlan> {
+    String _chosenValue;
+
+    @override
+    Widget build(BuildContext context) {
+
+  }
+}
