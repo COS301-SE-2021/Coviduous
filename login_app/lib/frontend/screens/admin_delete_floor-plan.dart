802fe83aba8e90a5ae2c3b19ca3d9c9474a57bde
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-29 09:02:33 +0200

    admin_delete_floor-plan.dart

---
+++ login_app/lib/frontend/screens/admin_delete_floor-plan.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+///import 'package:flutter/rendering.dart';
+
+class DeleteFloorPlan extends StatefulWidget {
+  @override
+  DeleteFloorPlanState createState() {
+    return DeleteFloorPlanState();
+  }
+}
+
+class DeleteFloorPlanState extends State<DeleteFloorPlan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
