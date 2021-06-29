4a54dcaa3b76b7a322c793d2593078b4a4b9d714
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-29 09:06:27 +0200

    admin_delete_floor-plan.dart password confirm.

--- login_app/lib/frontend/screens/admin_delete_floor-plan.dart
+++ login_app/lib/frontend/screens/admin_delete_floor-plan.dart
@@ -54,6 +54,20 @@ class DeleteFloorPlanState extends State<DeleteFloorPlan> {
                 ),
               ),
 
+              Padding(
+                padding: EdgeInsets.fromLTRB(350, 1, 350, 2),
+                child: TextFormField(
+                  decoration: InputDecoration(focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    ),
+                    labelText: 'Confirm Password:',
+                  ),
+                ),
+              ),
+
 
 
             ],
