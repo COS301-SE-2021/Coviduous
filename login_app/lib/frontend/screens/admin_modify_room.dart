77afa7eb966caab3d08040730d0f2d63b1ea4a8f
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-06-29 07:02:29 +0200

    admin_modify_room.dart add room number text-field.

--- login_app/lib/frontend/screens/admin_modify_room.dart
+++ login_app/lib/frontend/screens/admin_modify_room.dart
@@ -15,6 +15,32 @@ class EditFloorPlanState extends State<EditFloorPlan> {
         title: Text('Edit Floor-Plan'),
         backgroundColor: Colors.grey,
       ),
+      backgroundColor: Colors.white70,
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(350, 1, 350, 2),
+                child: TextField(
+                  //textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    ),
+
+                    labelText: 'Room Number',
+                  ),
+                ),
+              ),
+
+            ],
+        ),
+      ),
     );
   }
 }
