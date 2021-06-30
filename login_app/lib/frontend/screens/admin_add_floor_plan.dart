62e516cb6a5bb14bea164cbb18ed760dad319df5
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-06-30 05:27:15 +0200

    floor plan update

--- login_app/lib/frontend/screens/admin_add_floor_plan.dart
+++ login_app/lib/frontend/screens/admin_add_floor_plan.dart
@@ -8,7 +8,8 @@ import 'package:login_app/frontend/screens/admin_view_floors.dart';
 import 'package:login_app/subsystems/floorplan_subsystem/floor.dart';
 
 import 'package:login_app/frontend/front_end_globals.dart' as globals;
-import 'package:login_app/backend/backend_globals/floor_globals.dart' as floorGlobals;
+import 'package:login_app/backend/backend_globals/floor_globals.dart'
+    as floorGlobals;
 
 class AddFloorPlan extends StatefulWidget {
   static const routeName = "/admin_add_floor_plan";
@@ -16,35 +17,34 @@ class AddFloorPlan extends StatefulWidget {
   @override
   _AddFloorPlanState createState() => _AddFloorPlanState();
 }
+
 //add floor plan
 class _AddFloorPlanState extends State<AddFloorPlan> {
   String _numFloor;
 
   FloorPlanController service = new FloorPlanController();
 
-  Widget _buildFloors(){
+  Widget _buildFloors() {
     return TextFormField(
-      textInputAction: TextInputAction.done, //The "return" button becomes a "done" button when typing
-      decoration: InputDecoration(
-          labelText: 'Enter number of floors'
-      ),
+      textInputAction: TextInputAction
+          .done, //The "return" button becomes a "done" button when typing
+      decoration: InputDecoration(labelText: 'Enter number of floors'),
       keyboardType: TextInputType.number,
       validator: (String value) {
-
         int num = int.tryParse(value);
-        if(num == null || num <= 0){
+        if (num == null || num <= 0) {
           return 'Number of floors must be greater than zero';
         }
         return null;
       },
-      onSaved: (String value){
+      onSaved: (String value) {
         _numFloor = value;
       },
     );
   }
 
 //global key _formkey.
-  final GlobalKey<FormState> _formKey  = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -58,16 +58,19 @@ class _AddFloorPlanState extends State<AddFloorPlan> {
         backgroundColor: Colors.transparent, //To show background image
         appBar: AppBar(
           title: Text("Add floor plan"),
-          leading: BackButton( //Specify back button
-            onPressed: (){
+          leading: BackButton(
+            //Specify back button
+            onPressed: () {
               Navigator.of(context).pushReplacementNamed(FloorPlan.routeName);
             },
           ),
         ),
         body: Center(
-          child: SingleChildScrollView( //So the element doesn't overflow when you open the keyboard
+          child: SingleChildScrollView(
+            //So the element doesn't overflow when you open the keyboard
             child: Container(
-              width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+              width: MediaQuery.of(context).size.width /
+                  (2 * globals.getWidgetScaling()),
               color: Colors.white,
               margin: EdgeInsets.all(20),
               child: Form(
@@ -79,53 +82,59 @@ class _AddFloorPlanState extends State<AddFloorPlan> {
                     children: <Widget>[
                       Padding(
                           padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                          child: _buildFloors()
-                      ),
+                          child: _buildFloors()),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height/48,
+                        height: MediaQuery.of(context).size.height / 48,
                         width: MediaQuery.of(context).size.width,
                       ),
                       ElevatedButton(
-                          style: ElevatedButton.styleFrom (
+                          style: ElevatedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
                           ),
                           child: Text('Proceed'),
                           onPressed: () {
-                            if(!_formKey.currentState.validate()) {
+                            if (!_formKey.currentState.validate()) {
                               return;
                             }
                             _formKey.currentState.save();
 
-                            CreateFloorPlanResponse response = service.createFloorPlanMock(CreateFloorPlanRequest(globals.email, _numFloor, 0));
+                            CreateFloorPlanResponse response = service
+                                .createFloorPlanMock(CreateFloorPlanRequest(
+                                    globals.email, 2, 0));
 
                             if (response.getResponse()) {
                               floorGlobals.globalFloors.clear();
-                              floorGlobals.globalFloors = new List<Floor>.generate(int.parse(_numFloor), (int index) => new Floor(globals.email, "", 0));
-                              floorGlobals.globalNumFloors = int.parse(_numFloor);
-                              Navigator.of(context).pushReplacementNamed(AdminViewFloors.routeName);
+                              floorGlobals.globalFloors =
+                                  new List<Floor>.generate(
+                                      int.parse(_numFloor),
+                                      (int index) =>
+                                          new Floor(globals.email, "", 0));
+                              floorGlobals.globalNumFloors =
+                                  int.parse(_numFloor);
+                              Navigator.of(context).pushReplacementNamed(
+                                  AdminViewFloors.routeName);
                             } else {
                               showDialog(
                                   context: context,
                                   builder: (ctx) => AlertDialog(
-                                    title: Text('Creation unsuccessful'),
-                                    content: Text('Floor plans must have at least one floor.'),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        child: Text('Okay'),
-                                        onPressed: (){
-                                          Navigator.of(ctx).pop();
-                                        },
-                                      )
-                                    ],
-                                  )
-                              );
+                                        title: Text('Creation unsuccessful'),
+                                        content: Text(
+                                            'Floor plans must have at least one floor.'),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: Text('Okay'),
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                          )
+                                        ],
+                                      ));
                             }
-                          }
-                      ),
+                          }),
                       SizedBox(
-                        height: MediaQuery.of(context).size.height/48,
+                        height: MediaQuery.of(context).size.height / 48,
                         width: MediaQuery.of(context).size.width,
                       ),
                     ],
