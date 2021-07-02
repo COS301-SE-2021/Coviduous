41c4150690571e729d297773c3fe3b0041d8c0f5
Peter Okumbe <u18052640@tuks.co.za>
2021-07-03 01:14:40 +0200

    Update floor_plan_controller.dart

--- login_app/lib/backend/controllers/floor_plan_controller.dart
+++ login_app/lib/backend/controllers/floor_plan_controller.dart
@@ -26,7 +26,11 @@ import 'package:login_app/backend/backend_globals/floor_globals.dart'
     as floorGlobals;
 import 'package:login_app/subsystems/floorplan_subsystem/room.dart';
 
-
+/**
+ * Class name: FloorPlanController
+ * Purpose: This class is the controller for floorplans, all service contracts for the floorplan subsystem are offered through this class
+ * The class has both mock and concrete implementations of the service contracts.
+ */
 class FloorPlanController {
 //This class provides an interface to all the floorplan service contracts of the system. It provides a bridge between the front end screens and backend functionality for floor plan.
   /** 
