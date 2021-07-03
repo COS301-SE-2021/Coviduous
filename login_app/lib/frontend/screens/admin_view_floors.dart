4cf1b641f943e2c3e0df354f8063219a6733c0e6
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-07-03 15:37:21 +0200

    view floors update

--- login_app/lib/frontend/screens/admin_view_floors.dart
+++ login_app/lib/frontend/screens/admin_view_floors.dart
@@ -10,7 +10,8 @@ import 'package:login_app/subsystems/floorplan_subsystem/floor.dart';
 import 'package:login_app/frontend/screens/admin_view_rooms.dart';
 
 import 'package:login_app/frontend/front_end_globals.dart' as globals;
-import 'package:login_app/backend/backend_globals/floor_globals.dart' as floorGlobals;
+import 'package:login_app/backend/backend_globals/floor_globals.dart'
+    as floorGlobals;
 
 class AdminViewFloors extends StatefulWidget {
   static const routeName = "/admin_view_floors";
@@ -18,6 +19,7 @@ class AdminViewFloors extends StatefulWidget {
   @override
   _AdminViewFloorsState createState() => _AdminViewFloorsState();
 }
+
 class _AdminViewFloorsState extends State<AdminViewFloors> {
   @override
   Widget build(BuildContext context) {
@@ -30,105 +32,119 @@ class _AdminViewFloorsState extends State<AdminViewFloors> {
 
       print(numOfFloors);
 
-      if (numOfFloors == 0) { //This should not happen, but checking just in case.
+      if (numOfFloors == 0) {
+        //This should not happen, but checking just in case.
         showDialog(
             context: context,
             builder: (ctx) => AlertDialog(
-              title: Text('Error'),
-              content: Text('No floors have been defined for your company. Please return to the previous page and specify the number of floors.'),
-              actions: <Widget>[
-                TextButton(
-                  child: Text('Okay'),
-                  onPressed: (){
-                    Navigator.of(ctx).pop();
-                  },
-                )
-              ],
-            )
-        );
+                  title: Text('Error'),
+                  content: Text(
+                      'No floors have been defined for your company. Please return to the previous page and specify the number of floors.'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Okay'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                    )
+                  ],
+                ));
         Navigator.pushReplacementNamed(context, FloorPlan.routeName);
         return Container();
-      } else { //Else create and return a list
+      } else {
+        //Else create and return a list
         return ListView.builder(
             physics: NeverScrollableScrollPhysics(),
             shrinkWrap: true,
             padding: const EdgeInsets.all(8),
             itemCount: numOfFloors,
-            itemBuilder: (context, index) { //Display a list tile FOR EACH floor in floors[]
+            itemBuilder: (context, index) {
+              //Display a list tile FOR EACH floor in floors[]
               return ListTile(
-                title: Column(
-                    children:[
-                      Container(
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height/24,
-                        color: Theme.of(context).primaryColor,
-                        child: Text('Floor ' + services.getFloors()[index].getFloorNumber(), style: TextStyle(color: Colors.white)),
-                      ),
-                      ListView(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
-                          children: <Widget>[
-                            Container(
-                              height: 50,
-                              color: Colors.white,
-                              child: Text('Number of rooms: ' + services.getFloors()[index].getNumRooms().toString(), style: TextStyle(color: Colors.black)),
-                            ),
-                            Container(
-                              height: 50,
-                              color: Colors.white,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  ElevatedButton(
-                                      child: Text('Edit'),
-                                      onPressed: () {
-                                        globals.currentFloorNumString = services.getFloors()[index].getFloorNumber();
-                                        Navigator.of(context).pushReplacementNamed(AdminViewRooms.routeName);
-                                      }
-                                  ),
-                                  ElevatedButton(
-                                      child: Text('Delete'),
-                                      onPressed: () {
-                                        //Temporary: remove floor and reload page
-                                        if (floorGlobals.globalNumFloors > 1) { //Only allow deletion of floors if there is more than one floor
-                                          DeleteFloorResponse response3 = services.deleteFloorMock(DeleteFloorRequest(services.getFloors()[index].getFloorNumber()));
-                                          print(response3.getResponse());
-                                          /*
+                title: Column(children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 24,
+                    color: Theme.of(context).primaryColor,
+                    child: Text(
+                        'Floor ' + services.getFloors()[index].getFloorNumber(),
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                  ListView(
+                      shrinkWrap: true,
+                      physics:
+                          NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          color: Colors.white,
+                          child: Text(
+                              'Number of rooms: ' +
+                                  services
+                                      .getFloors()[index]
+                                      .getNumRooms()
+                                      .toString(),
+                              style: TextStyle(color: Colors.black)),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                  child: Text('Edit'),
+                                  onPressed: () {
+                                    globals.currentFloorNumString = services
+                                        .getFloors()[index]
+                                        .getFloorNumber();
+                                    Navigator.of(context).pushReplacementNamed(
+                                        AdminViewRooms.routeName);
+                                  }),
+                              ElevatedButton(
+                                  child: Text('Delete'),
+                                  onPressed: () {
+                                    //Temporary: remove floor and reload page
+                                    if (floorGlobals.globalNumFloors > 1) {
+                                      //Only allow deletion of floors if there is more than one floor
+                                      DeleteFloorResponse response3 =
+                                          services.deleteFloorMock(
+                                              DeleteFloorRequest(services
+                                                  .getFloors()[index]
+                                                  .getFloorNumber()));
+                                      print(response3.getResponse());
+                                      /*
                                           floorGlobals.globalNumFloors--;
                                           floorGlobals.globalFloors.removeAt(index);
                                            */
-                                          setState(() {});
-                                        } else {
-                                          showDialog(
-                                              context: context,
-                                              builder: (ctx) => AlertDialog(
+                                      setState(() {});
+                                    } else {
+                                      showDialog(
+                                          context: context,
+                                          builder: (ctx) => AlertDialog(
                                                 title: Text('Error'),
-                                                content: Text('Floor plans must have at least one floor. To delete a whole floor plan, please use the "delete floor plan" feature on the previous page.'),
+                                                content: Text(
+                                                    'Floor plans must have at least one floor. To delete a whole floor plan, please use the "delete floor plan" feature on the previous page.'),
                                                 actions: <Widget>[
                                                   TextButton(
                                                     child: Text('Okay'),
-                                                    onPressed: (){
+                                                    onPressed: () {
                                                       Navigator.of(ctx).pop();
                                                     },
                                                   )
                                                 ],
-                                              )
-                                          );
-                                        }
-                                      }
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ]
-                      )
-                    ]
-                ),
+                                              ));
+                                    }
+                                  }),
+                            ],
+                          ),
+                        ),
+                      ])
+                ]),
                 //title: floors[index].floor()
               );
-            }
-        );
+            });
       }
     }
 
@@ -143,8 +159,9 @@ class _AdminViewFloorsState extends State<AdminViewFloors> {
         backgroundColor: Colors.transparent, //To show background image
         appBar: AppBar(
           title: Text("Manage floors"),
-          leading: BackButton( //Specify back button
-            onPressed: (){
+          leading: BackButton(
+            //Specify back button
+            onPressed: () {
               Navigator.of(context).pushReplacementNamed(FloorPlan.routeName);
             },
           ),
@@ -156,28 +173,30 @@ class _AdminViewFloorsState extends State<AdminViewFloors> {
                 children: [
                   getList(),
                   SizedBox(
-                    height: MediaQuery.of(context).size.height/18,
+                    height: MediaQuery.of(context).size.height / 18,
                     width: MediaQuery.of(context).size.width,
                   ),
                 ],
               ),
             ),
-            Container (
+            Container(
               alignment: Alignment.bottomLeft,
-              child: Container (
+              child: Container(
                   height: 50,
                   width: 130,
                   padding: EdgeInsets.all(10),
                   child: ElevatedButton(
-                    style: ElevatedButton.styleFrom (
+                    style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                     child: Text('Add floor'),
-                    onPressed: (){
+                    onPressed: () {
                       //Add new floor and reload page
-                      AddFloorResponse response2 = services.addFloorMock(AddFloorRequest(globals.loggedInUserId, ""));
+                      AddFloorResponse response2 = services.addFloorMock(
+                          AddFloorRequest(
+                              globals.floorPlanId, globals.loggedInUserId, ""));
                       print(response2.getResponse());
                       /*
                       floorGlobals.globalNumFloors++;
@@ -185,8 +204,7 @@ class _AdminViewFloorsState extends State<AdminViewFloors> {
                        */
                       setState(() {});
                     },
-                  )
-              ),
+                  )),
             )
           ],
         ),
@@ -194,6 +212,3 @@ class _AdminViewFloorsState extends State<AdminViewFloors> {
     );
   }
 }
-
-
-
