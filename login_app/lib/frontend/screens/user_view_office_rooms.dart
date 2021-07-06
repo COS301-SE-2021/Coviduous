63164d8b86637d6c0bc75424bc790615c7e1f440
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-06 09:38:52 +0200

    Create user_view_office_rooms.dart
    View office rooms screen for user.

---
+++ login_app/lib/frontend/screens/user_view_office_rooms.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_app/backend/controllers/floor_plan_controller.dart';
+import 'package:login_app/frontend/screens/home_office.dart';
+import 'package:login_app/subsystems/floorplan_subsystem/room.dart';
+
+import 'package:login_app/frontend/front_end_globals.dart' as globals;
+
+class UserViewOfficeRooms extends StatefulWidget {
+  static const routeName = "/user_office_rooms";
+  @override
+  _UserViewOfficeRoomsState createState() => _UserViewOfficeRoomsState();
+}
+
+class _UserViewOfficeRoomsState extends State<UserViewOfficeRooms> {
+  @override
+  Widget build(BuildContext context) {
+    FloorPlanController services = new FloorPlanController();
+    Widget getList() {
+      List<Room> rooms =
+      services.getRoomsForFloorNum(globals.currentFloorNumString);
+      int numOfRooms = rooms.length;
+
+      print(numOfRooms);
+
+      if (numOfRooms == 0) { //If the number of rooms = 0, don't display a list
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                height: MediaQuery.of(context).size.height/(24*globals.getWidgetScaling()),
+                color: Theme.of(context).primaryColor,
+                child: Text('No rooms found', style: TextStyle(color: Colors.white, fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+              ),
+              Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                  height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
+                  color: Colors.white,
+                  padding: EdgeInsets.all(12),
+                  child: Text('No rooms have been registered for this floor.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
+              )
+            ]
+        );
+      } else { //Else create and return a list
+        return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: const EdgeInsets.all(8),
+            itemCount: numOfRooms,
+            itemBuilder: (context, index) { //Display a list tile FOR EACH room in rooms[]
+              return ListTile(
+                title: Column(
+                    children:[
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 24,
+                        color: Theme.of(context).primaryColor,
+                        child: Text(
+                            'Room ' + rooms[index].getRoomNum(),
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      ListView(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                          children: <Widget>[
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text(
+                                  'Number of desks: ' +
+                                      services
+                                          .getRoomDetails(rooms[index].getRoomNum())
+                                          .numDesks
+                                          .toString(),
+                                  style: TextStyle(color: Colors.black)),
+                            ),
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text(
+                                  'Occupied desk percentage: ' +
+                                      services
+                                          .getRoomDetails(rooms[index].getRoomNum())
+                                          .occupiedDesks
+                                          .toString(),
+                                  style: TextStyle(color: Colors.black)),
+                            ),
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: ElevatedButton(
+                                  child: Text('View'),
+                                  onPressed: () {
+                                    globals.currentRoomNumString = rooms[index].getRoomNum();
+                                    //Navigator.of(context).pushReplacementNamed(routeName);
+                                  }),
+                            ),
+                          ]
+                      )
+                    ]
+                ),
+                //title: floors[index].floor()
+              );
+            }
+        );
+      }
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: new Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('View office spaces'),
+            leading: BackButton( //Specify back button
+              onPressed: (){
+                Navigator.of(context).pushReplacementNamed(Office.routeName);
+              },
+            ),
+          ),
+          body: Stack(
+              children: <Widget>[
+                Center(
+                    child: getList()
+                ),
+              ]
+          )
+      ),
+    );
+  }
+}
