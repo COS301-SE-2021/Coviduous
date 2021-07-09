cfae45750fecaaebe34df729e9f6f4c141fe8b9f
Peter Okumbe <u18052640@tuks.co.za>
2021-07-09 12:43:25 +0200

    Corrected description

--- login_app/lib/backend/backend_globals/office_globals.dart
+++ login_app/lib/backend/backend_globals/office_globals.dart
@@ -18,7 +18,7 @@ import 'package:login_app/subsystems/office_subsystem/booking.dart';
 //Backend global variables
 //==========================
 /**
- * List<Booking> globalBookings  acts like a database table that holds bookings made inside a room , this is to mock out functionality for testing
+ * List<Booking> globalBookings acts like a database table that holds bookings made inside a room, this is to mock out functionality for testing
  * numBookings keeps track of number of bookings in the mock bookings database table
  */
 List<Booking> globalBookings = [];
