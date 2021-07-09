9430732885e59399913548dee871a94bbd9c56f9
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-09 10:53:50 +0200

    Update admin_modify_floors.dart
    Temporary change until this functionality is implemented in the services classes.

--- login_app/lib/frontend/screens/floor_plan/admin_modify_floors.dart
+++ login_app/lib/frontend/screens/floor_plan/admin_modify_floors.dart
@@ -102,7 +102,6 @@ class _AdminModifyFloorsState extends State<AdminModifyFloors> {
                               ElevatedButton(
                                   child: Text('Delete'),
                                   onPressed: () {
-                                    //Temporary: remove floor and reload page
                                     if (floorGlobals.globalNumFloors > 1) {
                                       //Only allow deletion of floors if there is more than one floor
                                       DeleteFloorResponse response3 =
@@ -111,10 +110,10 @@ class _AdminModifyFloorsState extends State<AdminModifyFloors> {
                                                   .getFloors()[index]
                                                   .getFloorNumber()));
                                       print(response3.getResponse());
-                                      /*
-                                            floorGlobals.globalNumFloors--;
-                                            floorGlobals.globalFloors.removeAt(index);
-                                             */
+
+                                      //TEMPORARY: make sure number of floors is decreased
+                                      floorGlobals.globalNumFloors--;
+
                                       setState(() {});
                                     } else {
                                       showDialog(
