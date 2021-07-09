f8bbcd431f6c3a52690517b6e27a5f2388ec1619
Peter Okumbe <u18052640@tuks.co.za>
2021-07-09 12:42:45 +0200

    Update office_globals.dart

--- login_app/lib/backend/backend_globals/office_globals.dart
+++ login_app/lib/backend/backend_globals/office_globals.dart
@@ -1,7 +1,9 @@
 /*
-  File name: office_globals.dart
-  Purpose: Global variables used for integration with front and backend.
-  Collaborators:
+  * File name: office_globals.dart
+  
+  * Purpose: Global variables used for integration with front and backend.
+  
+  * Collaborators:
     - Njabulo Skosana
     - Peter Okumbe
     - Chaoane Malakoane
