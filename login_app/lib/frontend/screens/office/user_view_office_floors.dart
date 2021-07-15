eb779e73b99da5cdd822e02a3f29654706530086
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-15 20:09:27 +0200

    Update user_view_office_floors.dart
    Added a check to make sure an admin cannot access pages in the user side of the app while logged in as an admin.

--- login_app/lib/frontend/screens/office/user_view_office_floors.dart
+++ login_app/lib/frontend/screens/office/user_view_office_floors.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'package:login_app/backend/controllers/floor_plan_controller.dart';
 import 'package:login_app/frontend/screens/office/home_office.dart';
 import 'package:login_app/frontend/screens/office/user_view_office_rooms.dart';
+import 'package:login_app/frontend/screens/admin_homepage.dart';
+import 'package:login_app/frontend/screens/login_screen.dart';
 
 import 'package:login_app/frontend/front_end_globals.dart' as globals;
 import 'package:login_app/backend/backend_globals/floor_globals.dart' as floorGlobals;
@@ -16,6 +19,20 @@ class UserViewOfficeFloors extends StatefulWidget {
 class _UserViewOfficeFloorsState extends State<UserViewOfficeFloors> {
   @override
   Widget build(BuildContext context) {
+    //If incorrect type of user, don't allow them to view this page.
+    if (globals.type != 'User') {
+      if (globals.type == 'Admin') {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        });
+      }
+      return Container();
+    }
+
     FloorPlanController services = new FloorPlanController();
     Widget getList() {
       int numOfFloors = floorGlobals.globalNumFloors;
