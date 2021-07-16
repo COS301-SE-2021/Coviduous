36030a2312747c63caba083ef7d2a5be16832bf7
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-16 11:01:01 +0200

    Update user_view_notifications.dart
    Fixed button width.

--- login_app/lib/frontend/screens/notification/user_view_notifications.dart
+++ login_app/lib/frontend/screens/notification/user_view_notifications.dart
@@ -185,7 +185,7 @@ class _UserViewNotificationsState extends State<UserViewNotifications> {
                   alignment: Alignment.bottomRight,
                   child: Container (
                       height: 50,
-                      width: 200,
+                      width: 170,
                       padding: EdgeInsets.all(10),
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom (
