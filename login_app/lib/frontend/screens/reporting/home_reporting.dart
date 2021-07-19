abbf95659ddcd67bde9478685867571dd40388dd
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-19 16:50:37 +0200

    Create home_reporting.dart
    Reporting homepage.

---
+++ login_app/lib/frontend/screens/reporting/home_reporting.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'package:login_app/frontend/screens/admin_homepage.dart';
+import 'package:login_app/frontend/screens/user_homepage.dart';
+import 'package:login_app/frontend/screens/login_screen.dart';
+
+import 'package:login_app/frontend/front_end_globals.dart' as globals;
+
+class Reporting extends StatefulWidget {
+  static const routeName = "/reporting";
+
+  @override
+  _ReportingState createState() => _ReportingState();
+}
+//class admin
+class _ReportingState extends State<Reporting> {
+  @override
+  Widget build(BuildContext context) {
+    //If incorrect type of user, don't allow them to view this page.
+    if (globals.type != 'Admin') {
+      if (globals.type == 'User') {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        });
+      }
+      return Container();
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('Manage company reports'),
+            leading: BackButton( //Specify back button
+              onPressed: (){
+                Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+              },
+            ),
+          ),
+          body: Center(
+              child: Container (
+                  height: MediaQuery.of(context).size.height/(2*globals.getWidgetScaling()),
+                  width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                  padding: EdgeInsets.all(20),
+                  child: Column (
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        ElevatedButton (
+                            style: ElevatedButton.styleFrom (
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Row (
+                                children: <Widget>[
+                                  Expanded(child: Text('Office reports')),
+                                  Icon(Icons.add_circle_rounded)
+                                ],
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween, //Align text and icon on opposite sides
+                                crossAxisAlignment: CrossAxisAlignment.center //Center row contents vertically
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (ctx) => AlertDialog(
+                                    title: Text('Placeholder'),
+                                    content: Text('Office reports.'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text('Okay'),
+                                        onPressed: (){
+                                          Navigator.of(ctx).pop();
+                                        },
+                                      )
+                                    ],
+                                  )
+                              );
+                            }
+                        ),
+                        SizedBox (
+                          height: MediaQuery.of(context).size.height/48,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        ElevatedButton (
+                            style: ElevatedButton.styleFrom (
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Row (
+                                children: <Widget>[
+                                  Expanded(child: Text('Health reports')),
+                                  Icon(Icons.update_rounded)
+                                ],
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween, //Align text and icon on opposite sides
+                                crossAxisAlignment: CrossAxisAlignment.center //Center row contents vertically
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (ctx) => AlertDialog(
+                                    title: Text('Placeholder'),
+                                    content: Text('Health reports.'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text('Okay'),
+                                        onPressed: (){
+                                          Navigator.of(ctx).pop();
+                                        },
+                                      )
+                                    ],
+                                  )
+                              );
+                            }
+                        ),
+                      ]
+                  )
+              )
+          )
+      ),
+    );
+  }
+}
