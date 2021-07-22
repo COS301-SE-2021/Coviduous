37b9a860d8c5a2dba0f7cb24f423d82fc73bfb64
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-23 01:53:20 +0200

    admin_view_rooms.dart class definition

---
+++ login_app/lib/frontend/screens/shift/admin_view_rooms.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/scheduler.dart';
+
+//import 'package:login_app/frontend/screens/notification/user_home_notifications.dart';
+///import 'package:login_app/frontend/screens/admin_homepage.dart';
+///import 'package:login_app/frontend/screens/login_screen.dart';
+
+///import 'package:login_app/frontend/front_end_globals.dart' as globals;
+///import 'package:login_app/frontend/screens/user_homepage.dart';
+///import 'home_screen.dart';
+///import 'admin_view_shifts.dart';
+///import 'admin_shifts_page.dart';
+
+class AdminViewRooms extends StatefulWidget {
+  static const routeName = "/Admin_view_rooms";
+
+  @override
+  _AdminViewRoomsState createState() => _AdminViewRoomsState();
+}
+
+class _AdminViewRoomsState extends State<AdminViewRooms> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
