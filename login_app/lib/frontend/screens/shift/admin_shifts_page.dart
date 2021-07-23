c644db5d9226804017194b40e3b31edf76fab0d2
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-23 02:28:39 +0200

    admin_shifts_page.dart display shift details

--- login_app/lib/frontend/screens/shift/admin_shifts_page.dart
+++ login_app/lib/frontend/screens/shift/admin_shifts_page.dart
@@ -23,6 +23,53 @@ class _AdminShiftsPageState extends State<AdminShiftsPage> {
             ]
         );
       }
+      else
+        {
+          return ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: numberofshifts,
+              itemBuilder: (context, index){
+              return ListTile(
+              title: Column(
+              children: [
+                  ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [Container(
+                  height: 50,
+                  color: Colors.white,
+                  child: Text('Date: 23 November 2021', style: TextStyle(color: Colors.black)),
+                   ),
+                    Container(
+                      height: 50,
+                      color: Colors.white,
+                      child: Text('Time: Current time', style: TextStyle(color: Colors.black)),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.white,
+                      child: Text('Employee ID: INF2221', style: TextStyle(color: Colors.black)),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.white,
+                      child: Text('Employee Name: Jeff', style: TextStyle(color: Colors.black)),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.white,
+                      child: Text('Employee Surname: Masemola', style: TextStyle(color: Colors.black)),
+                    ),
+
+                    ]
+                  )
+
+                 ]
+              ),
+            );
+          }
+          );
+        }
     }
   }
 }
