e9a74563a37fee9d46254c1245f4da6eb1b4db14
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-23 02:20:11 +0200

    admin_shifts_page.dart class declaration.

---
+++ login_app/lib/frontend/screens/shift/admin_shifts_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AdminShiftsPage extends StatefulWidget {
+  static const routeName = "/Admin_shifts_pages";
+
+  @override
+  _AdminShiftsPageState createState() => _AdminShiftsPageState();
+}
+class _AdminShiftsPageState extends State<AdminShiftsPage> {
+  //String _userId = globals.loggedInUserId;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget getList() {
+
+    }
+  }
+}
