132563bacadc4a54296129c6cfd0032fe0d5945c
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-23 02:39:01 +0200

    admin_edit_shift.dart class definition

---
+++ login_app/lib/frontend/screens/shift/admin_edit_shift.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdminEditShift extends StatefulWidget {
+  static const routeName = "/Admin_edit_shifts";
+  @override
+  _AdminEditShiftState createState() => _AdminEditShiftState();
+}
+
+class _AdminEditShiftState extends State<AdminEditShift> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+  }
