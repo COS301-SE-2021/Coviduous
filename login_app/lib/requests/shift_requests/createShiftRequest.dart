ca4488b7fced14b2f737a848d4a76975cfc1c7b5
u18202374-Chaks <u18202374@tuks.co.za>
2021-07-24 17:53:49 +0200

    updated class of created shift

--- login_app/lib/requests/shift_requests/createShiftRequest.dart
+++ login_app/lib/requests/shift_requests/createShiftRequest.dart
@@ -24,6 +24,42 @@ class CreateShiftRequest {
     this.shiftNo = shiftNo;
   }
 
+  String getDate() {
+    return date;
+  }
+
+  String getStartDate() {
+    return startDate;
+  }
+
+  String getEndDate() {
+    return endDate;
+  }
+
+  String getDescription() {
+    return description;
+  }
+
+  String getFloorNo() {
+    return floorNo;
+  }
+
+  String getRoomNo() {
+    return roomNo;
+  }
+
+  int getNumEmployees() {
+    return numEmployees;
+  }
+
+  String getAdminID() {
+    return adminID;
+  }
+
+  String getCompanyID() {
+    return companyID;
+  }
+
   String getShiftNo() {
     return shiftNo;
   }
