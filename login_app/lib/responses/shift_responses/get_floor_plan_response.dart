0d295ffe58c10baefed92ff67ab282ef09d9e5b7
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-07-24 14:02:48 +0200

    floor plan update

--- login_app/lib/responses/shift_responses/get_floor_plan_response.dart
+++ login_app/lib/responses/shift_responses/get_floor_plan_response.dart
@@ -3,10 +3,12 @@ import 'package:login_app/subsystems/floorplan_subsystem/floorplan.dart';
 class GetFloorPlansResponse {
   List<FloorPlan> floorplans;
   int numFloorPlans;
+  bool success = false;
 
-  GetFloorPlansResponse(List<FloorPlan> floorPlans) {
+  GetFloorPlansResponse(List<FloorPlan> floorPlans, bool succ) {
     this.floorplans = floorPlans;
     this.numFloorPlans = floorPlans.length;
+    this.success = succ;
   }
 
   List<FloorPlan> getFloorPlans() {
