99aa91a3caeff303f2a8d17443063567e31aec1d
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-07-25 12:05:38 +0200

    controller update

--- login_app/lib/backend/controllers/shift_controller.dart
+++ login_app/lib/backend/controllers/shift_controller.dart
@@ -8,32 +8,16 @@
     - Peter Okumbe
     - Chaoane Malakoane
  */
-import 'package:login_app/backend/server_connections/floor_plan_model.dart';
+
 import 'package:login_app/backend/server_connections/shift_model.dart';
-import 'package:login_app/requests/floor_plan_requests/add_floor_request.dart';
-import 'package:login_app/requests/floor_plan_requests/add_room_request.dart';
-import 'package:login_app/requests/floor_plan_requests/create_floor_plan_request.dart';
-import 'package:login_app/requests/floor_plan_requests/delete_floor_plan_request.dart';
-import 'package:login_app/requests/floor_plan_requests/delete_floor_request.dart';
-import 'package:login_app/requests/floor_plan_requests/delete_room_request.dart';
-import 'package:login_app/requests/floor_plan_requests/edit_room_request.dart';
 import 'package:login_app/requests/shift_requests/createGroupRequest.dart';
 import 'package:login_app/requests/shift_requests/createShiftRequest.dart';
 import 'package:login_app/requests/shift_requests/get_floor_plan_request.dart';
-import 'package:login_app/responses/floor_plan_responses/add_floor_response.dart';
-import 'package:login_app/responses/floor_plan_responses/add_room_response.dart';
-import 'package:login_app/responses/floor_plan_responses/create_floor_plan_response.dart';
-import 'package:login_app/responses/floor_plan_responses/delete_floor_plan_response.dart';
-import 'package:login_app/responses/floor_plan_responses/delete_floor_response.dart';
-import 'package:login_app/responses/floor_plan_responses/delete_room_response.dart';
-import 'package:login_app/responses/floor_plan_responses/edit_room_response.dart';
 import 'package:login_app/responses/shift_responses/createGroupResponse.dart';
 import 'package:login_app/responses/shift_responses/createShiftResponse.dart';
 import 'package:login_app/responses/shift_responses/get_floor_plan_response.dart';
-import 'package:login_app/subsystems/floorplan_subsystem/floor.dart';
-import 'package:login_app/backend/backend_globals/floor_globals.dart'
-    as floorGlobals;
-import 'package:login_app/subsystems/floorplan_subsystem/room.dart';
+import 'package:login_app/backend/backend_globals/shift_globals.dart'
+    as shiftGlobal;
 
 /**
  * Class name: ShiftController
@@ -48,17 +32,18 @@ class ShiftController {
   /** 
    * Shift Queries attribute holds the class that provides access to the database , the attribute allows you to access functions that will handle database interaction.
    */
-  ShiftModel shiftQueries;
+  ShiftModel shiftQueries = new ShiftModel();
 
   FloorPlanController() {
-    this.shiftQueries = new ShiftModel();
+    //this.shiftQueries = new ShiftModel();
   }
 
 /////////////////////////////////Concrete Implementations/////////////////////////////////
 
   Future<GetFloorPlansResponse> getFloorPlans(GetFloorPlansRequest req) async {
-    if (shiftQueries.getFloorPlanUsingCompanyId(req.getCompanyId()) == true) {
-      return new GetFloorPlansResponse([], true);
+    if ((await shiftQueries.getFloorPlanUsingCompanyId(req.getCompanyId())) ==
+        true) {
+      return new GetFloorPlansResponse(shiftGlobal.globalFloorplans, true);
     } else {
       return new GetFloorPlansResponse([], false);
     }
