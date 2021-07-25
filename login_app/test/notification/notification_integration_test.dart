9ee844b2c5283b2516268865197b81f873b533a8
Peter Okumbe <u18052640@tuks.co.za>
2021-07-25 17:21:49 +0200

    Added notification integration test

---
+++ login_app/test/notification/notification_integration_test.dart
@@ -0,0 +1,88 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:login_app/backend/controllers/notification_controller.dart';
+import 'package:login_app/requests/notification_requests/create_notification_request.dart';
+import 'package:login_app/requests/notification_requests/get_notification_request.dart';
+import 'package:login_app/requests/notification_requests/get_notifications_request.dart';
+import 'package:login_app/responses/notification_responses/create_notification_response.dart';
+import 'package:login_app/responses/notification_responses/get_notifications_response.dart';
+
+void main() {
+  NotificationController notificationController = new NotificationController();
+
+  String expectedUserID;
+  String expectedUserEmail;
+  String expectedSubject;
+  String expectedMessage;
+  String expectedAdminId;
+  String expectedCompanyId;
+
+  setUp(() {
+    expectedUserID = "test";
+    expectedUserEmail = "test@gmail.com";
+    expectedSubject = "test";
+    expectedMessage = "test";
+    expectedAdminId = "test";
+    expectedCompanyId = "test";
+  });
+
+  tearDown(() {});
+
+  test('Correct create notification', () async {
+    CreateNotificationRequest req = new CreateNotificationRequest(
+        expectedUserID,
+        expectedUserEmail,
+        expectedSubject,
+        expectedMessage,
+        expectedAdminId,
+        expectedCompanyId);
+    CreateNotificationResponse resp =
+        await notificationController.createNotification(req);
+
+    GetNotificationsRequest req2 = new GetNotificationsRequest();
+    GetNotificationsResponse resp2 =
+        await notificationController.getNotifications(req2);
+
+    for (var data in resp2.getNotifications()) {
+      print(data.notificationId);
+    }
+
+    print("notificationID: " + resp.getNotificationID());
+    print("Response : " + resp.getResponseMessage());
+
+    expect(resp, isNot(null));
+    expect(true, resp.getResponse());
+  });
+
+  test('Correct view notifications', () async {
+    GetNotificationsRequest req = new GetNotificationsRequest();
+    GetNotificationsResponse resp =
+        await notificationController.getNotifications(req);
+
+    for (var data in resp.getNotifications()) {
+      print(data.notificationId);
+    }
+
+    print("Response : " + resp.getResponseMessage());
+
+    expect(resp, isNot(null));
+    expect(true, resp.getResponse());
+  });
+
+  test('Correct view notifications based on user email', () async {
+    // CREATE NOTIFICATONS FIRST with SAME user emails then view based on userEmail
+
+    GetNotificationRequest req = new GetNotificationRequest(expectedUserEmail);
+    GetNotificationsResponse resp =
+        await notificationController.getNotification(req);
+
+    for (var data in resp.getNotifications()) {
+      print(data.notificationId);
+    }
+
+    print("Response : " + resp.getResponseMessage());
+
+    expect(resp, isNot(null));
+    expect(true, resp.getResponse());
+  });
+
+}
