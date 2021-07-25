88f57c55ace33b43e06dae41d10d819708bab18e
Peter Okumbe <u18052640@tuks.co.za>
2021-07-25 16:18:48 +0200

    Create notification_globals.dart

---
+++ login_app/lib/backend/backend_globals/notification_globals.dart
@@ -0,0 +1,25 @@
+/*
+  * File name: notification_globals.dart
+  
+  * Purpose: Global variables used for integration with front and backend.
+  
+  * Collaborators:
+    - Njabulo Skosana
+    - Peter Okumbe
+    - Chaoane Malakoane
+ */
+library globals;
+
+import 'package:login_app/subsystems/notification_subsystem/notification.dart';
+
+//Global variables used throughout the program
+//=============================================
+
+//Backend global variables
+//==========================
+/**
+ * List<Shift> notificationDatabaseTable acts like a database table that holds notifications, this is to mock out functionality for testing
+ * numNotifications keeps track of number of notifications in the mock notification database table
+ */
+List<Notification> notificationDatabaseTable = [];
+int numNotifications = 0;
