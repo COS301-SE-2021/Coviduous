56488fa8a3c39475c69ef5c522fa9f3a57f266b4
Peter Okumbe <u18052640@tuks.co.za>
2021-07-25 13:10:59 +0200

    Added get shifts request classes

---
+++ login_app/lib/requests/shift_requests/get_shifts_request.dart
@@ -0,0 +1,23 @@
+/*
+  File name: get_shifts_request.dart
+  Purpose: Holds the request class of retrieving all shifts
+  Collaborators:
+    - Njabulo Skosana
+    - Peter Okumbe
+    - Chaoane Malakoane
+ */
+
+/**
+ * Purpose: This class holds the request object of retrieving shifts by an admin user
+ */
+class GetShiftsRequest {
+  // String adminID;
+
+  GetShiftsRequest() {
+    // this.adminID = adminID;
+  }
+
+  // String getAdminID() {
+  //   return this.adminID;
+  // }
+}
