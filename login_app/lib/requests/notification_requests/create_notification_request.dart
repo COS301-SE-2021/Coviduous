588d7a6f89ac79507087ee284adc0ec57fc86063
Peter Okumbe <u18052640@tuks.co.za>
2021-07-25 13:12:03 +0200

    Added notification request class

---
+++ login_app/lib/requests/notification_requests/create_notification_request.dart
@@ -0,0 +1,55 @@
+/*
+  File name: create_notification_request.dart
+  Purpose: Holds the request class of creating a notification
+  Collaborators:
+    - Njabulo Skosana
+    - Peter Okumbe
+    - Chaoane Malakoane
+ */
+
+/**
+ * Purpose: This class holds the request object of creating a notification
+ */
+class CreateNotificationRequest {
+  String userId;
+  String userEmail;
+  String subject;
+  String message;
+  String timestamp;
+  String adminId;
+  String companyId;
+
+  CreateNotificationRequest(
+    String userId,
+    String userEmail,
+    String subject,
+    String message,
+    String timestamp,
+    String adminId,
+    String companyId,
+  ) {
+    this.userId = userId;
+    this.userEmail = userEmail;
+    this.subject = subject;
+    this.message = message;
+    this.timestamp = timestamp;
+    this.adminId = adminId;
+    this.companyId = companyId;
+  }
+
+  // String getType() {
+  //   return this.type;
+  // }
+
+  // String getMessage() {
+  //   return this.message;
+  // }
+
+  // String getAdminID() {
+  //   return this.adminID;
+  // }
+
+  // String getCompanyID() {
+  //   return this.companyID;
+  // }
+}
