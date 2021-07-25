d6388e8d35505a9b308451939b540fa229b92089
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-25 16:31:22 +0200

    Update main.dart
    Moved user request access route to the correct subsystem heading for organization purposes.

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -30,6 +30,7 @@ import 'package:login_app/frontend/screens/floor_plan/admin_view_rooms.dart';
 
 //Health
 import 'package:login_app/frontend/screens/health/user_home_health.dart';
+import 'package:login_app/frontend/screens/health/user_request_access.dart';
 
 //Shift
 import 'package:login_app/frontend/screens/shift/home_shift.dart';
@@ -60,7 +61,6 @@ import 'package:login_app/frontend/screens/notification/admin_make_notification.
 import 'package:login_app/frontend/screens/notification/admin_make_notification_add_employee.dart';
 import 'package:login_app/frontend/screens/notification/admin_make_notification_assign_employees.dart';
 import 'package:login_app/frontend/screens/notification/user_view_notifications.dart';
-import 'package:login_app/frontend/screens/health/user_request_access.dart';
 
 //Reporting
 import 'package:login_app/frontend/screens/reporting/home_reporting.dart';
@@ -128,6 +128,7 @@ class MyApp extends StatelessWidget{
 
         //Health
         UserHealth.routeName: (ctx)=> UserHealth(),
+        UserRequestAccess.routeName: (ctx)=> UserRequestAccess(),
 
         //Shift
         Shift.routeName: (ctx)=> Shift(),
@@ -158,7 +159,6 @@ class MyApp extends StatelessWidget{
         MakeNotificationAssignEmployees.routeName: (ctx)=> MakeNotificationAssignEmployees(),
         MakeNotificationAddEmployee.routeName: (ctx)=> MakeNotificationAddEmployee(),
         UserViewNotifications.routeName: (ctx)=> UserViewNotifications(),
-        UserRequestAccess.routeName: (ctx)=> UserRequestAccess(),
 
         //Reporting
         Reporting.routeName: (ctx)=> Reporting(),
