f87a02c5a6218000316ee7424f0bc164766cbd6e
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:16:25 +0200

    Update admin_delete_account.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/user/admin_delete_account.dart
+++ login_app/lib/frontend/screens/user/admin_delete_account.dart
@@ -56,8 +56,8 @@ class _AdminDeleteAccountState extends State<AdminDeleteAccount>{
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
