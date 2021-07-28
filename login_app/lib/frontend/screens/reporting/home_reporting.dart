a177b3a8e731f2a2d6abe3ee72b99444afca1164
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:15:19 +0200

    Update home_reporting.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/reporting/home_reporting.dart
+++ login_app/lib/frontend/screens/reporting/home_reporting.dart
@@ -20,8 +20,8 @@ class _ReportingState extends State<Reporting> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
