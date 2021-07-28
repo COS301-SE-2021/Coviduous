af05eb0397b9ff4ef572af41b427fd211c014427
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:20:33 +0200

    Update user_home_health.dart
    Renamed user_view_covid_test to user_view_test_results to match the class name inside it.

--- login_app/lib/frontend/screens/health/user_home_health.dart
+++ login_app/lib/frontend/screens/health/user_home_health.dart
@@ -5,7 +5,7 @@ import 'package:login_app/frontend/screens/health/user_health_check.dart';
 import 'package:login_app/frontend/screens/health/user_report_infection.dart';
 import 'package:login_app/frontend/screens/health/user_upload_test_results.dart';
 import 'package:login_app/frontend/screens/health/user_upload_vaccine_confirm.dart';
-import 'package:login_app/frontend/screens/health/user_view_covid_test.dart';
+import 'package:login_app/frontend/screens/health/user_view_test_results.dart';
 import 'package:login_app/frontend/screens/health/user_view_guidelines.dart';
 import 'package:login_app/frontend/screens/health/user_view_permissions.dart';
 import 'package:login_app/frontend/screens/user_homepage.dart';
