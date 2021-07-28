c72d7755464d162c6505d0581a1ebf7944c4a6f1
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:13:24 +0200

    Update admin_delete_floor_plan.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/floor_plan/admin_delete_floor_plan.dart
+++ login_app/lib/frontend/screens/floor_plan/admin_delete_floor_plan.dart
@@ -36,8 +36,8 @@ class DeleteFloorPlanState extends State<DeleteFloorPlan> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
