14b75b18dd63db19044e662d34dad13f05bdf941
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:14:03 +0200

    Update user_upload_vaccine_confirm.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/health/user_upload_vaccine_confirm.dart
+++ login_app/lib/frontend/screens/health/user_upload_vaccine_confirm.dart
@@ -63,8 +63,8 @@ class _UserUploadVaccineConfirmState extends State<UserUploadVaccineConfirm> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'User') {
-      if (globals.type == 'Admin') {
+    if (globals.loggedInUserType != 'User') {
+      if (globals.loggedInUserType == 'Admin') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
         });
