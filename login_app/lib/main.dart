464ff79e46d21edd02886c0e5e522b3c08d241e6
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:19:45 +0200

    Update main.dart
    Renamed user_upload_covid_test to user_upload_test_results to match the class name inside it.

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -33,7 +33,7 @@ import 'package:login_app/frontend/screens/health/user_home_health.dart';
 import 'package:login_app/frontend/screens/health/user_health_check.dart';
 import 'package:login_app/frontend/screens/health/user_view_permissions.dart';
 import 'package:login_app/frontend/screens/health/user_view_guidelines.dart';
-import 'package:login_app/frontend/screens/health/user_upload_covid_test.dart';
+import 'package:login_app/frontend/screens/health/user_upload_test_results.dart';
 import 'package:login_app/frontend/screens/health/user_upload_vaccine_confirm.dart';
 import 'package:login_app/frontend/screens/health/user_view_covid_test.dart';
 import 'package:login_app/frontend/screens/health/user_report_infection.dart';
