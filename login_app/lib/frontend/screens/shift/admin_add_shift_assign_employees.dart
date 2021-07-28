6ad3c8ef5ac4bf92e61031b1d9d13b268787dde6
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:15:44 +0200

    Update admin_add_shift_assign_employees.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/shift/admin_add_shift_assign_employees.dart
+++ login_app/lib/frontend/screens/shift/admin_add_shift_assign_employees.dart
@@ -43,8 +43,8 @@ class _AddShiftAssignEmployeesState extends State<AddShiftAssignEmployees> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
