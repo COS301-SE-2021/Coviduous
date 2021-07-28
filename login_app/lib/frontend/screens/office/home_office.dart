e243e17a47568af6bb92a6123de6c3d784c296e7
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:14:51 +0200

    Update home_office.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/office/home_office.dart
+++ login_app/lib/frontend/screens/office/home_office.dart
@@ -21,8 +21,8 @@ class _OfficeState extends State<Office> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'User') {
-      if (globals.type == 'Admin') {
+    if (globals.loggedInUserType != 'User') {
+      if (globals.loggedInUserType == 'Admin') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
         });
