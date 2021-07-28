d53826b95c571d56008c74ca532f5153cede8b00
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:15:26 +0200

    Update reporting_floor_plan.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/reporting/reporting_floor_plan.dart
+++ login_app/lib/frontend/screens/reporting/reporting_floor_plan.dart
@@ -36,8 +36,8 @@ class _ReportingFloorPlanState extends State<ReportingFloorPlan> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
