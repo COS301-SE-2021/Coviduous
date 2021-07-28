28bccb847ee3e24aace2864d00b87f012b9f39f2
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 17:47:56 +0200

    Update admin_view_shifts_rooms.dart
    Integration changes.

--- login_app/lib/frontend/screens/shift/admin_view_shifts_rooms.dart
+++ login_app/lib/frontend/screens/shift/admin_view_shifts_rooms.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
+import 'package:login_app/backend/controllers/shift_controller.dart';
 import 'package:login_app/frontend/screens/shift/admin_view_shifts.dart';
 import 'package:login_app/frontend/screens/shift/admin_view_shifts_floors.dart';
 import 'package:login_app/frontend/screens/user_homepage.dart';
 import 'package:login_app/frontend/screens/login_screen.dart';
+import 'package:login_app/requests/shift_requests/get_shift_request.dart';
+import 'package:login_app/responses/shift_responses/get_shifts_response.dart';
+import 'package:login_app/subsystems/floorplan_subsystem/room.dart';
 
 import 'package:login_app/frontend/front_end_globals.dart' as globals;
 
@@ -16,6 +20,50 @@ class ViewShiftsRooms extends StatefulWidget {
 }
 
 class _ViewShiftsRoomsState extends State<ViewShiftsRooms> {
+  ShiftController services = new ShiftController();
+  GetShiftsResponse response;
+  List<Room> rooms = globals.rooms;
+  int numOfRooms = globals.rooms.length;
+
+  Future<int> getNumShifts(int index) async {
+    await Future.wait([
+      services.getShift(GetShiftRequest(rooms[index].roomNum))
+    ]).then((responses) {
+      response = responses.first;
+      print(response.getShifts().length);
+      return response.getShifts().length;
+    });
+  }
+
+  Future getShifts(int index) async {
+    await Future.wait([
+      services.getShift(GetShiftRequest(rooms[index].roomNum))
+    ]).then((responses) {
+      response = responses.first;
+      if (response.getShifts().length != 0) { //Only allow shifts to be created if rooms exist
+        globals.shifts = response.getShifts();
+        Navigator.of(context).pushReplacementNamed(ViewShifts.routeName);
+      } else {
+        showDialog(
+            context: context,
+            builder: (ctx) =>
+                AlertDialog(
+                  title: Text('No shifts found'),
+                  content: Text('No shifts have been created for this room.'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Okay'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                    )
+                  ],
+                )
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
@@ -33,43 +81,79 @@ class _ViewShiftsRoomsState extends State<ViewShiftsRooms> {
     }
 
     Widget getList() {
-      int numberOfRooms = 1;
+      print(numOfRooms);
 
-      if (numberOfRooms == 0) {
+      if (numOfRooms == 0) {
         return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //no rooms available
+              SizedBox(
+                height: MediaQuery.of(context).size.height /
+                    (5 * globals.getWidgetScaling()),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                height: MediaQuery.of(context).size.height/(24*globals.getWidgetScaling()),
+                color: Theme.of(context).primaryColor,
+                child: Text('No rooms found', style: TextStyle(color: Colors.white, fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+              ),
+              Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                  height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
+                  color: Colors.white,
+                  padding: EdgeInsets.all(12),
+                  child: Text('No rooms have been registered for this floor.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
+              )
             ]
         );
-      }
-      else {
+      } else {
         return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
             padding: const EdgeInsets.all(8),
-            itemCount: numberOfRooms,
-            itemBuilder: (context, index) {
+            itemCount: numOfRooms,
+            itemBuilder: (context, index) { //Display a list tile FOR EACH room in rooms[]
               return ListTile(
-                  title: Column(
-                      children: [
-                        ListView(
+                title: Column(
+                    children:[
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 24,
+                        color: Theme.of(context).primaryColor,
+                        child: Text(
+                            'Room ' + rooms[index].getRoomNum(),
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      ListView(
                           shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          children: [
+                          physics: NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                          children: <Widget>[
                             Container(
                               height: 50,
                               color: Colors.white,
-
-                              child: Text('Room: SDFN-1', style: TextStyle(
-                                  color: Colors.black)),
-                            ),
-                            Container(
-                              height: 50,
-                              color: Colors.white,
-
                               child: Text(
-                                  'Number of shifts: 2', style: TextStyle(
-                                  color: Colors.black)),
+                                  'Number of desks: ' + rooms[index].desks.length.toString(),
+                                  style: TextStyle(color: Colors.black)),
                             ),
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text(
+                                  'Occupied desk percentage: ' + rooms[index].getPercentage().toString(),
+                                  style: TextStyle(color: Colors.black)),
+                            ),
+                            /*
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text(
+                                  'Number of shifts: ' + getNumShifts(index).toString(),
+                                  style: TextStyle(color: Colors.black)),
+                            ),
+                            */
                             Container(
                               height: 50,
                               color: Colors.white,
@@ -77,20 +161,21 @@ class _ViewShiftsRoomsState extends State<ViewShiftsRooms> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   ElevatedButton(
-                                      child: Text('View'),
+                                      child: Text('View shifts'),
                                       onPressed: () {
-                                        Navigator.of(context).pushReplacementNamed(ViewShifts.routeName);
+                                        globals.currentRoomNum = rooms[index].getRoomNum();
+                                        getShifts(index);
                                       }),
                                 ],
                               ),
                             ),
-                          ],
-                        )
-                      ]
-                  )
+                          ]
+                      )
+                    ]
+                ),
+                //title: floors[index].floor()
               );
             }
-
         );
       }
     }
@@ -113,8 +198,10 @@ class _ViewShiftsRoomsState extends State<ViewShiftsRooms> {
           ),
           body: Stack (
               children: <Widget>[
-                Center (
-                    child: getList()
+                SingleChildScrollView(
+                  child: Center (
+                      child: getList()
+                  ),
                 ),
               ]
           )
