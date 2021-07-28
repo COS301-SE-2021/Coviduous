d5d32ca0038684e1485b5401af2dcb0ca1fdb97d
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:16:30 +0200

    Update admin_manage_account.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/user/admin_manage_account.dart
+++ login_app/lib/frontend/screens/user/admin_manage_account.dart
@@ -21,8 +21,8 @@ class _AdminManageAccountState extends State<AdminManageAccount> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
