537bfff52d558ac5392297fd486e4d872ed4a97f
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-07-28 12:13:11 +0200

    Update admin_make_announcement.dart
    Updated global variable names to make them more consistent.

--- login_app/lib/frontend/screens/announcement/admin_make_announcement.dart
+++ login_app/lib/frontend/screens/announcement/admin_make_announcement.dart
@@ -63,8 +63,8 @@ class MakeAnnouncementState extends State<MakeAnnouncement> {
   @override
   Widget build(BuildContext context) {
     //If incorrect type of user, don't allow them to view this page.
-    if (globals.type != 'Admin') {
-      if (globals.type == 'User') {
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
           Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
         });
