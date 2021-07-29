2f20f1528d8d45bb2e8bc920562c895aa1d396b3
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-07-29 22:34:39 +0200

    node package update

--- login_app/coviduous-api/functions/index.js
+++ login_app/coviduous-api/functions/index.js
@@ -1,4 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
+
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+exports.app = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
