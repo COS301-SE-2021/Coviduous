33894cbcd6423b112eca678f1333d1d343ec24ab
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-07-30 15:57:21 +0200

    coviduous api update

--- login_app/coviduous-api/functions/index.js
+++ login_app/coviduous-api/functions/index.js
@@ -4,9 +4,28 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
+var serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-api-9a206..firebaseio.com"
+});
+const db = admin.firestore();
 
 app.get('/hello-world', (req, res) => {
   return res.status(200).send('Hello World!');
+});
+
+app.post('/api/create', (req, res) => {
+  (async () => {
+      try {
+        await db.collection('announcements').doc('/' + req.body.id + '/')
+            .create({announcents: req.body});
+        return res.status(200).send();
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+    })();
 });
 
 exports.app = functions.https.onRequest(app);
