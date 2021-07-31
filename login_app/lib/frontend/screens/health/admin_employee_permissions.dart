4bd29d7379721b4a8db4d7d68a1a3fae8c63f0e2
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-31 13:18:52 +0200

    admin_employee_permissions.dart display employee permissions.

--- login_app/lib/frontend/screens/health/admin_employee_permissions.dart
+++ login_app/lib/frontend/screens/health/admin_employee_permissions.dart
@@ -28,6 +28,68 @@ class _EmployeePermissionsState extends State<EmployeePermissions> {
       }
       return Container();
     }
+    Widget getList() {
+      int numberOfRequests = 1;
+
+      if (numberOfRequests == 0) {
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                  height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
+                  color: Colors.white,
+                  padding: EdgeInsets.all(12),
+                  child: Text('Employee has no access to the building.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
+              )
+            ]
+        );
+      } else { //Else create and return a list
+        return ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: numberOfRequests,
+            itemBuilder: (context, index) { //Display a list tile FOR EACH permission in permissions[]
+              return ListTile(
+                title: Column(
+                    children:[
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height/24,
+                        color: Theme.of(context).primaryColor,
+                        child: Text('Permissions for  UserID' + (index+1).toString(), style: TextStyle(color: Colors.white)),
+                      ),
+                      ListView(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                          children: <Widget>[
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text('Employee name: Name and surname displayed here', style: TextStyle(color: Colors.black)),
+                            ),
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text('Access: True', style: TextStyle(color: Colors.black)),
+                            ),
+                            Container(
+                              height: 50,
+                              color: Colors.white,
+                              child: Text('Date: 1 August 2021', style: TextStyle(color: Colors.black)),
+                            ),
+                          ]
+                      )
+                    ]
+                ),
+                //title: floors[index].floor()
+              );
+            }
+        );
+      }
+    }
+
     return Container(
 
     );
