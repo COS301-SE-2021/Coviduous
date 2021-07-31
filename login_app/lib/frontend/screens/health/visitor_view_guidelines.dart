95d2c811a3a56b48b91d8e8f82492ffca95a753d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-31 09:04:22 +0200

    visitor_view_guidelines.dart list of guidelines validator

--- login_app/lib/frontend/screens/health/visitor_view_guidelines.dart
+++ login_app/lib/frontend/screens/health/visitor_view_guidelines.dart
@@ -33,7 +33,44 @@ class _VisitorViewGuidelinesState extends State<VisitorViewGuidelines> {
       }
       return Container();
     }
-    return Container(
+    Widget getList() {
+       if (!globals.companyGuidelinesExist) { //If company guidelines have not been uploaded yet
+         return Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Container(
+                 alignment: Alignment.center,
+                 width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                 height: MediaQuery.of(context).size.height/(24*globals.getWidgetScaling()),
+                 color: Theme.of(context).primaryColor,
+                 child: Text('No company guidelines found', style: TextStyle(color: Colors.white, fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+               ),
+               Container(
+                   alignment: Alignment.center,
+                   width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+                   height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
+                   color: Colors.white,
+                   padding: EdgeInsets.all(12),
+                   child: Text('Your admin has not uploaded company guidelines yet. Please try again later.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
+               )
+             ]
+         );
+       } else { //Else, return a container showing the company guidelines
+           return Container(
+             alignment: Alignment.center,
+             width: MediaQuery.of(context).size.width/(1.5*globals.getWidgetScaling()),
+             height: MediaQuery.of(context).size.height/(1.5*globals.getWidgetScaling()),
+             color: Colors.white,
+             child: PdfView(
+               documentLoader: Center(child: CircularProgressIndicator()),
+               pageLoader: Center(child: CircularProgressIndicator()),
+               controller: pdfController,
+             ),
+           );
+         }
+       }
+
+      return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/bg.jpg'),
@@ -53,7 +90,7 @@ class _VisitorViewGuidelinesState extends State<VisitorViewGuidelines> {
           body: Stack (
               children: <Widget>[
                 Center (
-                  //child: getList()
+                  child: getList()
                 ),
               ]
           )
