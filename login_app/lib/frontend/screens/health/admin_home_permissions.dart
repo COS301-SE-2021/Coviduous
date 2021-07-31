a2cb05309db468cd8932abe49ea7bc9166dc128d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-31 11:39:35 +0200

    admin_home_permissions.dart view permissions button

--- login_app/lib/frontend/screens/health/admin_home_permissions.dart
+++ login_app/lib/frontend/screens/health/admin_home_permissions.dart
@@ -29,7 +29,57 @@ class _AdminPermissionsState extends State<AdminPermissions> {
        return Container();
      }
     return Container(
-
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text('Health'),
+          leading: BackButton( //Specify back button
+            onPressed: (){
+              // Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
+            },
+          ),
+        ),
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height/(2*globals.getWidgetScaling()),
+            width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                ElevatedButton (
+                    style: ElevatedButton.styleFrom (
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Row (
+                        children: <Widget>[
+                          Expanded(child: Text('View permissions')),
+                          Icon(Icons.zoom_in)
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween, //Align text and icon on opposite sides
+                        crossAxisAlignment: CrossAxisAlignment.center //Center row contents vertically
+                    ),
+                    onPressed: () {
+                      // Navigator.of(context).pushReplacementNamed(UserViewPermissions.routeName);
+                    }
+                ),
+                SizedBox (
+                  height: MediaQuery.of(context).size.height/48,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
