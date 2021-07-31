5cd63287b49a947e386978af5556f394a8e26ecb
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-07-31 11:23:58 +0200

    admin_view_access_requests.dart grant access functionality

--- login_app/lib/frontend/screens/health/admin_view_access_requests.dart
+++ login_app/lib/frontend/screens/health/admin_view_access_requests.dart
@@ -148,6 +148,26 @@ class _EmployeeRequestsState extends State<EmployeeRequests> {
                         )
                     ),
                   ),
+                  Container (
+                    alignment: Alignment.centerLeft,
+                    child: Container (
+                        height: 50,
+                        width: 170,
+                        padding: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom (
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text('Grant Access'),
+                          onPressed: (){
+                            //notifications.clear();
+                            setState(() {});
+                          },
+                        )
+                    ),
+                  ),
                 ]
             )
         ),
