4fa03b619ec7bf60281c3daf1509af3d1bc9481f
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-01 10:24:50 +0200

    mockito test setup

---
+++ login_app/test/announcement/announcement_test_helpers.dart
@@ -0,0 +1,13 @@
+import 'package:mockito/mockito.dart';
+
+
+class announcementModelMock extends Mock implements AnnouncementDatabaseQueries {
+
+bool createAnnouncementModelMock(String type, String message, String adminId, String companyId){
+
+    return true;
+
+}
+
+
+}
