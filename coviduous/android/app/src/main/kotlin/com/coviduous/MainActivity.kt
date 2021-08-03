150c280f180189746ede21dc8132e7889a386ae6
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-03 15:00:23 +0200

    Revert "Merge branch 'development'"
    This reverts commit 2ec037e9016da7a85cda6171df5f7bf0fd542553, reversing
    changes made to 3ef3868e27a589ae9a6cdc1557171c2dcc792b07.

--- coviduous/android/app/src/main/kotlin/com/coviduous/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.coviduous
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ login_app/lib/frontend/screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:login_app/frontend/screens/login_screen.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+
+    Future.delayed(const Duration(seconds: 2), (){
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginScreen()), (route) => false);
+
+    });
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent, //To show background image
+        body: Center(
+          child: FlutterLogo(
+            size: 80,
+          ),
+        ),
+      ),
+    );
+  }
+}
