0808af81aa3ae07f8c6771560477ee807b3dfe81
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-05 19:08:16 +0200

    Create user_unit.test.js

---
+++ backend/functions/tests/user/user_unit.test.js
@@ -0,0 +1,12 @@
+var user = require("../../services/user/user.service");
+var firebase = require("../../services/user/firebase");
+var auth0 = require("../../services/user/auth0");
+var chai = require("chai");
+var expect = chai.expect;
+var object = new user();
+
+describe('Tests', function(){
+    it('Unit test 1', function(){
+        expect();
+    });
+});
