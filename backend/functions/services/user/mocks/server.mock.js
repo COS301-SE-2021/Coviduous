1759f35aa9bdbae7f774292898bb7876d2e18892
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-05 21:08:04 +0200

    Update server.mock.js
    Remove non-mock code from mock file.

--- backend/functions/services/user/mocks/server.mock.js
+++ backend/functions/services/user/mocks/server.mock.js
@@ -8,22 +8,4 @@ const app = express();
 app.use(cors());
 //app.use('/', express.static(path.join(__dirname, 'public')));
 
-const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-    }),
-    audience: process.env.AUTH0_API_AUDIENCE,
-    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-    algorithm: 'RS256'
-});
-
-app.get('/firebase', jwtCheck, async (req, res) => {
-    res.json("firebaseToken");
-});
-
-app.listen(3001, () => console.log('Mock server running on localhost:3001'));
-
 module.exports = app;
