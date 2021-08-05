95b7f2879ff70851460f7ed955c3a935cd6407a0
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-05 21:09:56 +0200

    Update user_unit.test.js
    The bracket placement used in the expects were not correct, causing all tests to fail.

--- backend/functions/tests/user/user_unit.test.js
+++ backend/functions/tests/user/user_unit.test.js
@@ -1,19 +1,20 @@
 var chai = require("chai");
 var expect = chai.expect;
+var should = chai.should();
 
 var service = require("../../services/user/mocks/user.service.mock");
 var serviceRef = new service();
 
 describe('Tests', function(){
     it('Sign in', function(){
-        expect(serviceRef.signIn().to.be.equal(true));
+        expect(serviceRef.signIn()).to.be.true;
     });
 
     it('View notification', function(){
-        expect(serviceRef.viewNotifications().to.be.equal(true));
+        expect(serviceRef.viewNotifications()).to.be.true;
     })
 
     it('Sign out', function(){
-        expect(serviceRef.signOut().to.be.equal(true));
+        expect(serviceRef.signOut()).to.be.true;
     })
 });
