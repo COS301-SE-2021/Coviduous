7523fc240dea2afc3243d2811439d12c04c0bac1
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-05 21:07:42 +0200

    Update user.service.mock.js
    Trying to fix npm not recognizing the paths in the requires again. Now it recognizes them.

--- backend/functions/services/user/mocks/user.service.mock.js
+++ backend/functions/services/user/mocks/user.service.mock.js
@@ -2,37 +2,37 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require('express');
 const cors = require('cors');
-const firebase = require("firebase.mock");
-const auth0 = require("auth0.mock");
-const server = require("server.mock");
+const firebase = require("../mocks/firebase.mock");
+const auth0 = require("../mocks/auth0.mock");
+const server = require("../mocks/server.mock");
 
-var serviceAccount = require("../../permissions.json");
+//var serviceAccount = require("../../permissions.json");
 
-const serverRef = new server();
+const serverRef = server;
 const firebaseClient = new firebase();
 const auth0Client = new auth0();
 
 class User {
     constructor() {
-        console.log("created user class");
+        console.log("Created user class");
     }
 
     signIn() {
         auth0Client.signIn();
-        console.log("signed in");
+        console.log("Signed in");
         return true;
     }
 
     signOut() {
         auth0Client.signOut();
         firebaseClient.signOut();
-        console.log("signed out");
+        console.log("Signed out");
         return true;
     }
 
     viewNotifications() {
         var message = firebaseClient.getMessage();
-        console.log("message received: " + message);
+        console.log("Message received: " + message);
         return true;
     }
 }
