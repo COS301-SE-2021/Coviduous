f731efd1405c34b88184b5ae8af7c0bd49c2c854
Peter Okumbe <u18052640@tuks.co.za>
2021-08-06 15:43:16 +0200

    Update announcement.controller.js

--- backend/functions/services/announcement/announcement.controller.js
+++ backend/functions/services/announcement/announcement.controller.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const admin = require('firebase-admin'); 
 //const express = require('express');
 //const cors = require('cors');
 //const app = express();
