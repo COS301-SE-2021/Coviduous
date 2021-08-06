fe52f9c93b15718ee09b2de4870ee66441e30ac8
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-06 12:15:41 +0200

    setting up config variables

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,10 +1,9 @@
+var config=require("./config/config.js");
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
- const express = require('express');
- const cors = require('cors');
- const app = express();
-
- app.use(cors({ origin: true }));
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
 
  app.get('/api', (req, res) => {
    return res.status(200).send('Connected to the coviduous api');
