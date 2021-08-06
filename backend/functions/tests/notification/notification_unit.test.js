850ac1fb2a67224ff347416bc98108ebbc04b837
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-06 12:11:53 +0200

    create notification

--- backend/functions/tests/notification/notification_unit.test.js
+++ backend/functions/tests/notification/notification_unit.test.js
@@ -7,4 +7,7 @@ describe("Test suit", function (){
     it("Test the delete notification method", function () {
         expect(myObj.deleteNotification(req,res)).to.be.equal("Notification successfully deleted");
     })
+    it("Test the create Notification method", function () {
+        expect(myObj.createNotification(req,res)).to.be.equal("Notification successfully created");
+    })
 })
