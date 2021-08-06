6ef316d2666fc2d454565ccc6aefe22bb624231a
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-06 11:11:14 +0200

    try test announcement_unit.test.js

--- backend/functions/tests/announcement/announcement_unit.test.js
+++ backend/functions/tests/announcement/announcement_unit.test.js
@@ -1,11 +1,12 @@
 var announcements=require("../tests/announcement_unit.test.js");
+var myAnnouncement = new announcements();
 var chai=require("chai");
 var expect=chai.expect;
 
 describe('Tests', function(){
     
     it('Unit test1',function(){
-        expect(announcements.add(1,2)).to.be.equal(3);
-    })
+        expect(myAnnouncement.add(1,2)).to.be.equal(3);
+    });
 });
 
