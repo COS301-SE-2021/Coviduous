4b205166a4166923c68dd999e0e1e1925c73d464
Peter Okumbe <u18052640@tuks.co.za>
2021-08-06 12:40:28 +0200

    Create announcement.service.js

---
+++ backend/functions/services/announcement/announcement.service.js
@@ -0,0 +1,23 @@
+class Announcement {
+    constructor(announcementId, type, message, timestamp, companyId, adminId)
+    {
+        this.announcementId = announcementId;
+        this.type = type;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.companyId = companyId;
+        this.adminId = adminId;
+
+        console.log("created announcement class");
+    }
+
+    createAnnouncement(announcementId, type, message, timestamp, companyId, adminId) {
+        return new Announcement(announcementId, type, message, timestamp, companyId, adminId);
+    }
+
+    getAnnouncementId(){
+        return this.announcementId;
+    }
+}
+
+module.exports=Announcement;
