962dc26d90b926666a6aaa2a5db6dda21f033373
u18202374-Chaks <u18202374@tuks.co.za>
2021-08-07 16:29:05 +0200

    shift created

---
+++ backend/functions/services/shift/shift.controller.js
@@ -0,0 +1,13 @@
+let db;
+
+exports.createShift = async (req,res) => {
+try{
+    await db.createShift(req.body.shiftID);
+    return res.status(200).send({
+        data: req.body
+    });
+} catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
