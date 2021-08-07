e2748df43b18ed421325dea4bab6bee86d5855ae
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-07 10:06:03 +0200

    index.js file update

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -17,21 +17,16 @@ app.get('/api', (req, res) => {
  return res.status(200).send('Connected to the coviduous api');
 });
 
-// Import controllers
-const floorPlanController = require("./routes/floorplan/floorplan.service.js");
-//const announcementController = require("./services/announcement/announcement.controller.js");
-//const notificationController = require("./services/notification/notification.controller.js");
-
-//subroutes
-app.post('/api/floorplan/create-floorplan', floorPlanController.createFloorPlan);
 
 // STRUCTURE: Each subsystem has it's own route.js file, respective routes are defined there
 // and each subsystem route.js file requires respective subsystem.controller.js file where functions are defined
 
 // Import routes
 const announcementRoute = require("./routes/announcement.route.js")
+const floorplanRoute = require("./routes/floorplan.route.js");
 
 // app.use('/api', subsystem_nameRoute) - use /api/ path for each subsystem route
 app.use('/api/', announcementRoute); // testing http would be '.../api/announcements' - see announcement.route.js file for routes
+app.use('/api/',floorplanRoute)
 
 exports.app = functions.https.onRequest(app);
