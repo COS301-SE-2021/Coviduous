97ef12f89d110cd9591963359967e8948fdd5ee7
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-07 13:12:31 +0200

    Update user.controller.js
    Fixed naming conventions to be more consistent.

--- backend/functions/services/user/user.controller.js
+++ backend/functions/services/user/user.controller.js
@@ -1,10 +1,10 @@
-const Service = require('../user/user.service');
+const User = require('../../models/user.model');
 
-const service = new Service();
+const userObj = new User();
 
 exports.createUser = async (req, res) => {
     try {
-        await service.createUser(req.body.email, req.body.password);
+        await userObj.createUser(req.body.email, req.body.password);
 
         console.log("User successfully created: " + req.body.email);
 
@@ -20,7 +20,7 @@ exports.createUser = async (req, res) => {
 
 exports.signUserIn = async (req, res) => {
     try {
-        await service.signUserIn(req.body.email, req.body.password);
+        await userObj.signUserIn(req.body.email, req.body.password);
 
         console.log("User successfully signed in: " + req.body.email);
 
