cfba3b3b8add48aa74cdc5ec12e47ca9ab738fc0
Peter Okumbe <u18052640@tuks.co.za>
2021-08-07 20:50:22 +0200

    Update announcement.controller.js

--- backend/functions/services/announcement/announcement.controller.js
+++ backend/functions/services/announcement/announcement.controller.js
@@ -1,72 +1,120 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin'); 
-//const express = require('express');
-//const cors = require('cors');
-//const app = express();
-//var serviceAccount = require("../../permissions.json");
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin'); 
 
-//app.use(cors({ origin: true }));
+// const db = admin.firestore();
+let database;
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://fir-api-9a206..firebaseio.com"
-// });
-
-const db = admin.firestore();
-
+exports.setDatabase = async (db) => {
+  database = db;
+}
   
+// exports.createAnnouncement = async (req, res) => {
+//   try {
+//     await db.collection('announcements').doc(req.body.announcementId)
+//         .create(req.body); // .add - auto generates document id
+    
+//     return res.status(200).send({
+//       message: 'Announcement successfully created',
+//       data: req.body
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).send(error);
+//   }
+// };
+
 exports.createAnnouncement = async (req, res) => {
   try {
-    await db.collection('announcements').doc(req.body.announcementId)
-        .create(req.body); // .add - auto generates document id
-    
-    return res.status(200).send({
-      message: 'Announcement successfully created',
-      data: req.body
-    });
+    //database.createAnnouncement(new Announcement('req.type', 'req.message', '',..,))
+    if (await database.createAnnouncement(req.body.announcementId, req.body) == true)
+    {
+      return res.status(200).send({
+        message: 'Announcement successfully created',
+        data: req.body
+      });
+    }
   } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
+      console.log(error);
+      return res.status(500).send(error);
   }
 };
+
+// exports.deleteAnnouncement = async (req, res) => {
+//   try {
+//     const document = db.collection('announcements').doc(req.body.announcementId); // delete document based on announcementID
+//     await document.delete();
+
+//     return res.status(200).send({
+//       message: 'Announcement successfully deleted'
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).send(error);
+//   }
+// };
 
 exports.deleteAnnouncement = async (req, res) => {
   try {
-    const document = db.collection('announcements').doc(req.body.announcementId); // delete document based on announcementID
-    await document.delete();
-
-    return res.status(200).send({
-      message: 'Announcement successfully deleted'
-    });
+    if (await database.deleteAnnouncement(req.body.announcementId) == true)
+    {
+      return res.status(200).send({
+        message: 'Announcement successfully deleted',
+      });
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).send(error);
   }
 };
 
+// exports.viewAnnouncements = async (req, res) => {
+//   try {
+//     const document = db.collection('announcements');
+//     const snapshot = await document.get();
+    
+//     let list = [];
+    
+//     snapshot.forEach(doc => {
+//       let data = doc.data();
+//       list.push(data);
+//     });
+
+//     let announcements = list;
+    
+//     return res.json({
+//       message: 'Successfully retrieved announcements',
+//       data: announcements 
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).send({
+//       message: err.message || "Some error occurred while fetching announcements."
+//     });
+//   }
+// };
+
 exports.viewAnnouncements = async (req, res) => {
   try {
-    const document = db.collection('announcements');
-    const snapshot = await document.get();
+      let announcements = await database.viewAnnouncements();
     
-    let list = [];
-    
-    snapshot.forEach(doc => {
-      let data = doc.data();
-      list.push(data);
-    });
+      // let list = [];
+      
+      // snapshot.forEach(doc => {
+      //   let data = doc.data();
+      //   list.push(data);
+      // });
 
-    let announcements = list;
-    
-    return res.json({
-      message: 'Successfully retrieved announcements',
-      data: announcements 
-    });
+      // let announcements = list;
+      
+      return res.json({
+        message: 'Successfully retrieved announcements',
+        data: announcements 
+      });
   } catch (error) {
-    console.log(error);
-    return res.status(500).send({
-      message: err.message || "Some error occurred while fetching announcements."
-    });
+      console.log(error);
+      return res.status(500).send({
+        message: err.message || "Some error occurred while fetching announcements."
+      });
   }
 };
 
