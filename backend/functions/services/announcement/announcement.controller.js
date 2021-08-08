337fb1384729dd49edee2c903d6aa45cfebad640
Peter Okumbe <u18052640@tuks.co.za>
2021-08-08 20:12:10 +0200

    Update announcement.controller.js

--- backend/functions/services/announcement/announcement.controller.js
+++ backend/functions/services/announcement/announcement.controller.js
@@ -5,21 +5,6 @@ let database;
 exports.setDatabase = async (db) => {
   database = db;
 }
-  
-// exports.createAnnouncement = async (req, res) => {
-//   try {
-//     await db.collection('announcements').doc(req.body.announcementId)
-//         .create(req.body); // .add - auto generates document id
-    
-//     return res.status(200).send({
-//       message: 'Announcement successfully created',
-//       data: req.body
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send(error);
-//   }
-// };
 
 exports.createAnnouncement = async (req, res) => {
   try {
@@ -52,20 +37,6 @@ exports.createAnnouncement = async (req, res) => {
   }
 };
 
-// exports.deleteAnnouncement = async (req, res) => {
-//   try {
-//     const document = db.collection('announcements').doc(req.body.announcementId); // delete document based on announcementID
-//     await document.delete();
-
-//     return res.status(200).send({
-//       message: 'Announcement successfully deleted'
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send(error);
-//   }
-// };
-
 exports.deleteAnnouncement = async (req, res) => {
   try {
     if (await database.deleteAnnouncement(req.body.announcementId) == true)
@@ -79,32 +50,6 @@ exports.deleteAnnouncement = async (req, res) => {
     return res.status(500).send(error);
   }
 };
-
-// exports.viewAnnouncements = async (req, res) => {
-//   try {
-//     const document = db.collection('announcements');
-//     const snapshot = await document.get();
-    
-//     let list = [];
-    
-//     snapshot.forEach(doc => {
-//       let data = doc.data();
-//       list.push(data);
-//     });
-
-//     let announcements = list;
-    
-//     return res.json({
-//       message: 'Successfully retrieved announcements',
-//       data: announcements 
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).send({
-//       message: err.message || "Some error occurred while fetching announcements."
-//     });
-//   }
-// };
 
 exports.viewAnnouncements = async (req, res) => {
   try {
