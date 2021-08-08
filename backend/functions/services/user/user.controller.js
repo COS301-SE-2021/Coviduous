7be083b886ce9d0d968bee339533614448760908
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-08 20:09:14 +0200

    Update user.controller.js

--- backend/functions/services/user/user.controller.js
+++ backend/functions/services/user/user.controller.js
@@ -64,8 +64,8 @@ class UserController {
 
     async updateUserDetails(req, res) {
         try {
-            await database.updateUserDetails(req.body.currentEmail, req.body.firstName, req.body.lastName,
-                req.body.companyID, req.body.companyName, req.body.companyAddress);
+            await database.updateUserDetails(req.body.currentEmail.toLowerCase(), req.body.firstName,
+                req.body.lastName, req.body.companyID, req.body.companyName, req.body.companyAddress);
 
             return res.status(200).send({
                 message: 'User updated',
@@ -79,8 +79,9 @@ class UserController {
 
     async updateUserEmail(req, res) {
         try {
-            await database.updateEmail(req.body.newEmail, req.body.currentEmail);
-            await userObj.updateUserEmail(req.body.newEmail, req.body.currentEmail, req.body.password);
+            await database.updateEmail(req.body.newEmail.toLowerCase(), req.body.currentEmail.toLowerCase()).then(() => {
+                userObj.updateUserEmail(req.body.newEmail.toLowerCase(), req.body.currentEmail.toLowerCase(), req.body.password);
+            });
 
             return res.status(200).send({
                 message: 'User updated',
