2e6f325775fa1a7f7e2b62cdcf3afa48e79ff4c2
Peter Okumbe <u18052640@tuks.co.za>
2021-08-10 13:54:56 +0200

    added permissions and permission request db functions

--- backend/functions/config/health.firestore.database.js
+++ backend/functions/config/health.firestore.database.js
@@ -40,91 +40,133 @@ exports.viewHealthChecks = async () => {
     }
 };
 
-// exports.viewHealthCheckUserEmail = async (value) => {
-//     try {
-//         const document = db.collection('health-check').where("userEmail", "==", value);
-//         const snapshot = await document.get();
+exports.viewHealthCheckUserId = async (value) => {
+    try {
+        const document = db.collection('health-check').where("userId", "==", value);
+        const snapshot = await document.get();
         
-//         let list = [];
+        let list = [];
         
-//         snapshot.forEach(doc => {
-//             let data = doc.data();
-//             list.push(data);
-//         });
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
     
-//         let healthChecks = list;
+        let healthChecks = list;
         
-//         return healthChecks;
-//     } catch (error) {
-//         console.log(error);
-//         return error;
-//     }
-// };
+        return healthChecks;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
 
 ///////////////// PERMISSIONS /////////////////
 
-// exports.createPermission = async (permissionId, data) => {
-//     try {
-//         await db.collection('permissions').doc(permissionId)
-//           .create(data); // .add - auto generates document id
-//         return true;
-//     } catch (error) {
-//         console.log(error);
-//         return false;
-//     }
-// };
+exports.createPermission = async (permissionId, data) => {
+    try {
+        await db.collection('permissions').doc(permissionId)
+          .create(data); // .add - auto generates document id
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
 
-// exports.viewPermissions = async () => {
-//     try {
-//         const document = db.collection('permissions');
-//         const snapshot = await document.get();
+exports.viewPermissions = async () => {
+    try {
+        const document = db.collection('permissions');
+        const snapshot = await document.get();
         
-//         let list = [];
+        let list = [];
         
-//         snapshot.forEach(doc => {
-//             let data = doc.data();
-//             list.push(data);
-//         });
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
     
-//         let permissions = list;
+        let permissions = list;
         
-//         return permissions;
-//     } catch (error) {
-//         console.log(error);
-//         return error;
-//     }
-// };
+        return permissions;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+exports.viewPermissionsUserId = async (value) => {
+    try {
+        const document = db.collection('permissions').where("userId", "==", value);
+        const snapshot = await document.get();
+        
+        let list = [];
+        
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
+    
+        let permissions = list;
+        
+        return permissions;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
 
 ///////////////// PERMISSION REQUEST /////////////////
 
-// exports.createPermissionRequest = async (permissionRequestId, data) => {
-//     try {
-//         await db.collection('permission-requests').doc(permissionRequestId)
-//           .create(data); // .add - auto generates document id
-//         return true;
-//     } catch (error) {
-//         console.log(error);
-//         return false;
-//     }
-// };
+exports.createPermissionRequest = async (permissionRequestId, data) => {
+    try {
+        await db.collection('permission-requests').doc(permissionRequestId)
+          .create(data); // .add - auto generates document id
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
 
-// exports.viewPermissionRequests = async () => {
-//     try {
-//         const document = db.collection('permission-requests');
-//         const snapshot = await document.get();
+exports.viewPermissionRequests = async () => {
+    try {
+        const document = db.collection('permission-requests');
+        const snapshot = await document.get();
         
-//         let list = [];
+        let list = [];
         
-//         snapshot.forEach(doc => {
-//             let data = doc.data();
-//             list.push(data);
-//         });
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
     
-//         let permissionRequests = list;
+        let permissionRequests = list;
         
-//         return permissionRequests;
-//     } catch (error) {
-//         console.log(error);
-//         return error;
-//     }
-// };
+        return permissionRequests;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+exports.viewPermissionRequestsCompanyId = async (value) => {
+    try {
+        const document = db.collection('permission-requests').where("companyId", "==", value);
+        const snapshot = await document.get();
+        
+        let list = [];
+        
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
+    
+        let permissions = list;
+        
+        return permissions;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
