5ae088dff23a4b1eaab533566a27ec4a5c734445
Peter Okumbe <u18052640@tuks.co.za>
2021-08-10 13:55:47 +0200

    added permission and permission request routes

--- backend/functions/routes/health.route.js
+++ backend/functions/routes/health.route.js
@@ -19,11 +19,14 @@ healthController.setDatabase(devDatabase);
 // Health routes
 // N.B. paths for a subsystem can all be the same
 router.post('/health/health-check', healthController.createHealthCheck);
-router.get('/health/health-check', healthController.viewHealthChecks); // + based on userId
-// router.post('/health/permission', healthController.createPermission);
-// router.get('/health/permission', healthController.viewPermissions); // + based on userId
-// router.post('/health/permission-request', healthController.createPermissionRequest);
-// router.get('/health/permission-request', healthController.viewPermissionRequests); // + based on companyId
+router.get('/health/health-check', healthController.viewHealthChecks);
+router.get('/health/health-check/userid', healthController.viewHealthCheckUserId);
+router.post('/health/permissions', healthController.createPermission);
+router.get('/health/permissions', healthController.viewPermissions);
+router.get('/health/permissions/userid', healthController.viewPermissionsUserId);
+router.post('/health/permissions/permission-request', healthController.createPermissionRequest);
+router.get('/health/permissions/permission-request', healthController.viewPermissionRequests);
+router.get('/health/permissions/permission-request/companyid', healthController.viewPermissionsRequestsCompanyId);
 
 // Export API routes
 module.exports = router;
