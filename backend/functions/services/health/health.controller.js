41a060d937ac82d60d525150613c2524c0ec84a4
Peter Okumbe <u18052640@tuks.co.za>
2021-08-10 13:56:20 +0200

    added permissions and permission request functions

--- backend/functions/services/health/health.controller.js
+++ backend/functions/services/health/health.controller.js
@@ -17,8 +17,7 @@ exports.createHealthCheck = async (req, res) => {
 
     let healthCheckObj = new HealthCheck(healthCheckId, req.body.userId, req.body.name, req.body.surname,
         req.body.email, req.body.phoneNumber, req.body.temperature, req.body.fever, req.body.cough,
-        req.body.soreThroat, req.body.chills, req.body.aches, req.body.nausea, req.body.shortnessOfBreadth, 
-        req.body.lossOfTasteSmell)
+        req.body.soreThroat, req.body.chills, req.body.aches, req.body.nausea, req.body.shortnessOfBreath, req.body.lossOfTasteSmell);
 
     let healthCheckData = {
       healthCheckId: healthCheckObj.healthCheckId,
@@ -34,7 +33,7 @@ exports.createHealthCheck = async (req, res) => {
       chills: healthCheckObj.chills,
       aches: healthCheckObj.aches,
       nausea: healthCheckObj.nausea,
-      shortnessOfBreath: healthCheckObj.shortnessOfBreadth,
+      shortnessOfBreath: healthCheckObj.shortnessOfBreath,
       lossOfTasteSmell: healthCheckObj.lossOfTasteSmell
     }
 
@@ -67,101 +66,149 @@ exports.viewHealthChecks = async (req, res) => {
     }
 };
 
+exports.viewHealthCheckUserId = async (req, res) => {
+    try {
+        let healthChecks = await database.viewHealthCheckUserId(req.body.userId);
+        
+        return res.status(200).send({
+          message: 'Successfully retrieved health checks',
+          data: healthChecks
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: err.message || "Some error occurred while fetching health checks."
+        });
+    }
+};
+
 ///////////////// PERMISSION /////////////////
 
-// exports.createPermission = async (req, res) => {
-//     try {
-//       let randInt = Math.floor(1000 + Math.random() * 9000);
-//       let permissionId = "PRMN-" + randInt.toString();
-//       let timestamp = new Date().toISOString();
+exports.createPermission = async (req, res) => {
+    try {
+      let randInt = Math.floor(1000 + Math.random() * 9000);
+      let permissionId = "PRMN-" + randInt.toString();
+      let timestamp = new Date().toISOString();
   
-//       let permissionObj = new Permission(permissionId, req.body.userId, timestamp, 
-//         req.body.officeAccess, req.body.grantedBy)
+      let permissionObj = new Permission(permissionId, req.body.userId, timestamp, 
+        req.body.officeAccess, req.body.grantedBy)
   
-//       let permissionData = {
-//         permissionId: permissionObj.permissionId,
-//         userId: permissionObj.userId,
-//         timestamp: timestamp,
-//         officeAccess: permissionObj.officeAccess,
-//         grantedBy: permissionObj.grantedBy,
-//       }
+      let permissionData = {
+        permissionId: permissionObj.permissionId,
+        userId: permissionObj.userId,
+        timestamp: timestamp,
+        officeAccess: permissionObj.officeAccess,
+        grantedBy: permissionObj.grantedBy,
+      }
   
-//       if (await database.createPermission(permissionData.healthCheckId, permissionData) == true)
-//       {
-//         return res.status(200).send({
-//           message: 'Permission successfully created',
-//           data: healthCheckData
-//         });
-//       }
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).send(error);
-//     }
-// };
+      if (await database.createPermission(permissionData.permissionId, permissionData) == true)
+      {
+        return res.status(200).send({
+          message: 'Permission successfully created',
+          data: permissionData
+        });
+      }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+};
   
-// exports.viewPermissions = async (req, res) => {
-//     try {
-//         let permissions = await database.viewPermissions();
+exports.viewPermissions = async (req, res) => {
+    try {
+        let permissions = await database.viewPermissions();
         
-//         return res.status(200).send({
-//         message: 'Successfully retrieved permissions',
-//         data: permissions
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).send({
-//         message: err.message || "Some error occurred while fetching permissions."
-//         });
-//     }
-// };
+        return res.status(200).send({
+        message: 'Successfully retrieved permissions',
+        data: permissions
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+        message: err.message || "Some error occurred while fetching permissions."
+        });
+    }
+};
+
+exports.viewPermissionsUserId = async (req, res) => {
+  try {
+      let permissions = await database.viewPermissionsUserId(req.body.userId);
+      
+      return res.status(200).send({
+        message: 'Successfully retrieved permissions',
+        data: permissions
+      });
+  } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        message: err.message || "Some error occurred while fetching permissions."
+      });
+  }
+};
 
 
 ///////////////// PERMISSION REQUEST /////////////////
 
-// exports.createPermissionRequest = async (req, res) => {
-//     try {
-//       let randInt = Math.floor(1000 + Math.random() * 9000);
-//       let permissionRequestId = "PRMN-" + randInt.toString();
-//       let timestamp = new Date().toISOString();
+exports.createPermissionRequest = async (req, res) => {
+    try {
+      let randInt = Math.floor(1000 + Math.random() * 9000);
+      let permissionRequestId = "PR-" + randInt.toString();
+      let timestamp = new Date().toISOString();
   
-//       let permissionRequestObj = new PermissionRequest(permissionRequestId, req.body.userId, req.body.shiftNumber,
-//         timestamp, req.body.reason, req.body.adminId, req.body.companyId)
+      let permissionRequestObj = new PermissionRequest(permissionRequestId, req.body.userId, req.body.shiftNumber,
+        timestamp, req.body.reason, req.body.adminId, req.body.companyId)
   
-//       let permissionRequestData = {
-//         permissionRequestId: permissionRequestObj.permissionRequestId,
-//         userId: permissionRequestObj.userId,
-//         shiftNumber: permissionRequestObj.shiftNumber,
-//         timestamp: timestamp,
-//         reason: permissionRequestObj.reason,
-//         adminId: permissionRequestObj.adminId,
-//         companyId: permissionRequestObj.companyId,
-//       }
+      let permissionRequestData = {
+        permissionRequestId: permissionRequestObj.permissionRequestId,
+        userId: permissionRequestObj.userId,
+        shiftNumber: permissionRequestObj.shiftNumber,
+        timestamp: timestamp,
+        reason: permissionRequestObj.reason,
+        adminId: permissionRequestObj.adminId,
+        companyId: permissionRequestObj.companyId,
+      }
   
-//       if (await database.createPermissionRequest(permissionRequestData.permissionRequestId, permissionRequestData) == true)
-//       {
-//         return res.status(200).send({
-//           message: 'Permission request successfully created',
-//           data: healthCheckData
-//         });
-//       }
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).send(error);
-//     }
-// };
+      if (await database.createPermissionRequest(permissionRequestData.permissionRequestId, permissionRequestData) == true)
+      {
+        return res.status(200).send({
+          message: 'Permission request successfully created',
+          data: permissionRequestData
+        });
+      }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+};
   
-// exports.viewPermissionRequests = async (req, res) => {
-//     try {
-//         let permissionRequests = await database.viewPermissionRequests();
+exports.viewPermissionRequests = async (req, res) => {
+    try {
+        let permissionRequests = await database.viewPermissionRequests();
         
-//         return res.status(200).send({
-//         message: 'Successfully retrieved permission requests',
-//         data: permissionRequests
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).send({
-//         message: err.message || "Some error occurred while fetching permission requests."
-//         });
-//     }
-// };
+        return res.status(200).send({
+        message: 'Successfully retrieved permission requests',
+        data: permissionRequests
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+        message: err.message || "Some error occurred while fetching permission requests."
+        });
+    }
+};
+
+exports.viewPermissionsRequestsCompanyId = async (req, res) => {
+  try {
+      let permissionRequests = await database.viewPermissionRequestsCompanyId(req.body.companyId);
+      
+      return res.status(200).send({
+        message: 'Successfully retrieved permission requests',
+        data: permissionRequests
+      });
+  } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        message: err.message || "Some error occurred while fetching permission requests."
+      });
+  }
+};
