05658926571a81d2fd54ae2d9b911361b659096d
Peter Okumbe <u18052640@tuks.co.za>
2021-08-10 13:55:04 +0200

    Update index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -26,18 +26,18 @@ app.get('/api', (req, res) => {
 const announcementRoute = require("./routes/announcement.route.js")
 const notificationRoute = require("./routes/notification.route.js")
 const floorplanRoute = require("./routes/floorplan.route.js");
-//const userRoute = require("./routes/user.route.js");
+const userRoute = require("./routes/user.route.js");
 const shiftRoute = require("./routes/shift.route.js");
 const officeRoute = require("./routes/office.route.js");
-// const healthRoute = require("./routes/health.route.js");
+const healthRoute = require("./routes/health.route.js");
 
 // app.use('/api', subsystem_nameRoute) - use '/api/' path for each subsystem route
 app.use('/api/', announcementRoute); // testing http would be '.../api/announcement' - see announcement.route.js file for routes
 app.use('/api/', notificationRoute);
 app.use('/api/', floorplanRoute);
-//app.use('/api/', userRoute);
+app.use('/api/', userRoute);
 app.use('/api/', shiftRoute);
 app.use('/api/', officeRoute);
-// app.use('/api/', healthRoute);
+app.use('/api/', healthRoute);
 
 exports.app = functions.https.onRequest(app);
