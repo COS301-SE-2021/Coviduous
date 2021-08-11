578f978bc9c8dcfb5f97e9b89fdf6b36772d5d52
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-11 12:10:35 +0200

    Update admin_view_notifications.dart
    Fixing an error that occurs when the notifications list is null.

--- frontend/lib/frontend/screens/notification/admin_view_notifications.dart
+++ frontend/lib/frontend/screens/notification/admin_view_notifications.dart
@@ -37,8 +37,9 @@ class _AdminViewNotificationsState extends State<AdminViewNotifications> {
     }
 
     Widget getList() {
-      List<Notification> reverseNotifications = notifications.reversed.toList(); //To display the newest notifications first
-      int numberOfNotifications = notifications.length;
+      int numberOfNotifications = 0;
+      if (notifications != null)
+        numberOfNotifications = notifications.length;
 
       if (numberOfNotifications == 0) {
         return Column(
@@ -82,6 +83,8 @@ class _AdminViewNotificationsState extends State<AdminViewNotifications> {
             ]
         );
       } else {
+        List<Notification> reverseNotifications = notifications.reversed.toList(); //To display the newest notifications first
+
         return ListView.builder(
             physics: NeverScrollableScrollPhysics(),
             shrinkWrap: true,
