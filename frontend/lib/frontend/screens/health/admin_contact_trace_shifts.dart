edd1946aed3b9a4ad04c3faf7c4e94c1222a6927
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-11 04:42:23 +0200

    creating admin_contact_trace_shifts.dart classes and widgets

---
+++ frontend/lib/frontend/screens/health/admin_contact_trace_shifts.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:frontend/frontend/screens/user_homepage.dart';
+import 'package:frontend/frontend/screens/login_screen.dart';
+import 'package:frontend/frontend/front_end_globals.dart' as globals;
+
+class ViewShifts extends StatefulWidget {
+  static const routeName = "/admin_shifts_view_shifts";
+
+  @override
+  _ViewShiftsState createState() => _ViewShiftsState();
+}
+class _ViewShiftsState extends State<ViewShifts> {
+  @override
+  Widget build(BuildContext context) {
+    //If incorrect type of user, don't allow them to view this page.
+    if (globals.loggedInUserType != 'Admin') {
+      if (globals.loggedInUserType == 'User') {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        });
+      }
+      return Container();
+    }
+    return new Scaffold();
+  }
+}
