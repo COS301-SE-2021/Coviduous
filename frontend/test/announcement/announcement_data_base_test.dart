84947f56d884f098227d042e7cfb2f6993df997d
Peter Okumbe <u18052640@tuks.co.za>
2021-08-12 17:55:44 +0200

    Update announcement_data_base_test.dart

--- frontend/test/announcement/announcement_data_base_test.dart
+++ frontend/test/announcement/announcement_data_base_test.dart
@@ -1,90 +1,50 @@
 //import '../../lib/backend/server_connections/announcement_data_base_queries.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:frontend/backend/controllers/announcements_controller.dart';
-import 'package:frontend/requests/announcements_requests/create_announcement_request.dart';
-import 'package:frontend/responses/announcement_responses/create_announcement_response.dart';
-import 'package:postgres/postgres.dart';
+import 'package:frontend/controllers/announcement_controller.dart'
+    as announcementController;
+import 'package:frontend/subsystems/announcement_subsystem/announcement2.dart';
+// import 'package:frontend/requests/announcements_requests/create_announcement_request.dart';
+// import 'package:frontend/responses/announcement_responses/create_announcement_response.dart';
 
 void main() {
-  PostgreSQLConnection connection;
-  String host = 'localhost';
-  int port = 5432;
-  String dbName = 'mock_CoviduousDB'; // an existing DB name on your localhost
-  String user = 'postgres';
-  String pass = 'postgres'; // your postgres user password
-
-  AnnouncementsController announcementController =
-      new AnnouncementsController();
+  // AnnouncementsController announcementController =
+  //     new AnnouncementsController();
 
   String expectedValue = "test";
   String expectedType;
   String expectedMessage;
-  String expectedAdminID;
-  String expectedCompanyID;
+  String expectedAdminId;
+  String expectedCompanyId;
 
   setUp(() async {
     expectedValue = "test";
-
-    // connect to db
-    connection = PostgreSQLConnection(host, port, dbName,
-        username: user, password: pass);
-
-    try {
-      await connection.open();
-      expectedMessage = "Connected to postgres database...";
-      print(expectedMessage);
-    } catch (e) {
-      print("error");
-      print(e.toString());
-    }
   });
 
-  tearDown(() async {
-    await connection.close();
-  });
+  tearDown(() async {});
 
-  // ======================DB TESTING======================== //
-
-  test('Successful database connection', () async {
-    expect(expectedMessage, "Connected to postgres database...");
-    expect(await connection.execute('select 1'), equals(1));
-  });
+  // ======================HTTP SERVER TESTING======================== //
 
   test('Create announcement', () async {
-    await connection
-        .query('''INSERT INTO announcements (announcementid, type, datecreated, message, adminid, companyid)
-                                  VALUES (@id, @type, @date, @message, @adminid, @companyid)''',
-            substitutionValues: {
-          'id': expectedValue,
-          'type': expectedValue,
-          'date': expectedValue,
-          'message': expectedValue,
-          'adminid': expectedValue,
-          'companyid': expectedValue,
-        });
+    //print(results);
 
-    var results = await connection.query("SELECT * FROM announcements");
-
-    print(results);
-
-    expect(results.length, isNot(0));
+    //expect(results.length, isNot(0));
   });
 
   test('View announcement', () async {
-    var results = await connection.query("SELECT * FROM announcements");
+    List<Announcement> list = await announcementController.getAnnouncements();
 
-    print(results);
+    for (var data in list) {
+      print(data.announcementId);
+    }
+    //print(results);
 
-    expect(results.length, isNot(0));
+    //expect(results.length, isNot(0));
   });
 
-  test('Delete announcement', () async {
-    var results = await connection.query(
-        "DELETE FROM announcements WHERE announcementid = @id",
-        substitutionValues: {'id': expectedValue});
+  // test('Delete announcement', () async {
 
-    print(results);
+  //   print(results);
 
-    expect(results.length, 0);
-  });
+  //   expect(results.length, 0);
+  // });
 }
