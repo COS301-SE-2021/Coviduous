ee6b9448c4440870ddaa4bd45bf1b9bddccf7c9e
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-13 00:31:16 +0200

    Create floor_plan.dart

---
+++ frontend/lib/subsystems/floor_plan_subsystem/floor_plan.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+FloorPlan floorPlanFromJson(String str) => FloorPlan.fromJson(json.decode(str));
+
+String floorPlanToJson(FloorPlan data) => json.encode(data.toJson());
+
+class FloorPlan {
+  String floorPlanNumber;
+  int numFloors;
+  String adminId;
+  String companyId;
+
+  FloorPlan({
+    this.floorPlanNumber,
+    this.numFloors,
+    this.adminId,
+    this.companyId,
+  });
+
+  factory FloorPlan.fromJson(Map<String, dynamic> json) => FloorPlan(
+    floorPlanNumber: json["floorPlanNumber"],
+    numFloors: json["numFloors"],
+    adminId: json["adminId"],
+    companyId: json["companyId"]
+  );
+
+  Map<String, dynamic> toJson() => {
+    "floorPlanNumber": floorPlanNumber,
+    "numFloors": numFloors,
+    "adminId" : adminId,
+    "companyId": companyId
+  };
+
+  String getFloorPlanNumber() {
+    return floorPlanNumber;
+  }
+
+  int getNumFloors() {
+    return numFloors;
+  }
+
+  String getAdminId() {
+    return adminId;
+  }
+
+  String getCompanyId() {
+    return companyId;
+  }
+}
