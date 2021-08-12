12ca24bfc4ab7b0552b0260843027f9ea0ce1fd5
Peter Okumbe <u18052640@tuks.co.za>
2021-08-12 17:05:05 +0200

    Update announcement_controller.dart

--- frontend/lib/controllers/announcement_controller.dart
+++ frontend/lib/controllers/announcement_controller.dart
@@ -23,75 +23,71 @@ int numAnnouncements = 0;
 
 ////////////////// FUNCTIONS ////////////////////
 
-String server = 'http://localhost:5001/coviduous-api/us-central1/app/api/'; //server needs to be running on firebase
-	
-	// announcementId and timestamp fields are generated in node backend
-  Future<bool> createAnnouncement(
-    String announcementId,
-    String type,
-    String message,
-    String timestamp,
-    String adminId,
-    String companyId) async {
-    String path = '/announcements';
-    String url = server + path;
+String server =
+    'http://localhost:5001/coviduous-api/us-central1/app/api/'; //server needs to be running on firebase
 
-    var request = http.Request('POST', Uri.parse(url));
-    request.body = json.encode({
-      "announcementId": announcementId,
-      "type": type,
-      "message": message,
-      "timestamp": timestamp,
-      "adminId": adminId,
-      "companyId": companyId,
-    });
+// announcementId and timestamp fields are generated in node backend
+Future<bool> createAnnouncement(String announcementId, String type,
+    String message, String timestamp, String adminId, String companyId) async {
+  String path = '/announcements';
+  String url = server + path;
 
-    var response = await request.send();
+  var request = http.Request('POST', Uri.parse(url));
+  request.body = json.encode({
+    "announcementId": announcementId,
+    "type": type,
+    "message": message,
+    "timestamp": timestamp,
+    "adminId": adminId,
+    "companyId": companyId,
+  });
 
-    if (response.statusCode == 200) {
-      print(await response.stream.bytesToString());
+  var response = await request.send();
 
-      return true;
-    }
+  if (response.statusCode == 200) {
+    print(await response.stream.bytesToString());
 
-    return false;
+    return true;
   }
 
-  /**
+  return false;
+}
+
+/**
    * getAnnouncements : Returns a list of all announcements created
    */
-  Future<bool> getAnnouncements() async {
-    String path = '/announcements';
-    String url = server + path;
+Future<bool> getAnnouncements() async {
+  String path = '/announcements';
+  String url = server + path;
 
-    var response = await http.get(Uri.parse(url));
-    // http.Response response = await http
-    //     .get(Uri.https(server, path));
+  var response = await http.get(Uri.parse(url));
+  // http.Response response = await http
+  //     .get(Uri.https(server, path));
 
-    if (response.statusCode == 200) {
-      //print(response.body);
+  if (response.statusCode == 200) {
+    //print(response.body);
 
-      var jsonString = response.body;
-      var jsonMap = jsonDecode(jsonString);
+    var jsonString = response.body;
+    var jsonMap = jsonDecode(jsonString);
 
-      //Added these lines so that it doesn't just keep adding and adding to the list indefinitely everytime this function is called
-      announcementDatabaseTable.clear();
-      numAnnouncements = 0;
+    //Added these lines so that it doesn't just keep adding and adding to the list indefinitely everytime this function is called
+    announcementDatabaseTable.clear();
+    numAnnouncements = 0;
 
-      for (var data in jsonMap["data"]) {
-        //print(data["announcementId"]);
-        var announcementData = Shift.fromJson(data);
-        announcementDatabaseTable.add(announcementData);
-        // print(
-        //     announcementDatabaseTable[numAnnouncements].announcementId);
-        numAnnouncements++;
-      }
-
-      return true;
+    for (var data in jsonMap["data"]) {
+      //print(data["announcementId"]);
+      var announcementData = Announcement.fromJson(data);
+      announcementDatabaseTable.add(announcementData);
+      // print(
+      //     announcementDatabaseTable[numAnnouncements].announcementId);
+      numAnnouncements++;
     }
 
-    return false;
+    return true;
   }
+
+  return false;
+}
 
   //   Future<bool> deleteAnnouncement(String announcementId) async {
   //   String path = '/announcements';
@@ -116,7 +112,7 @@ String server = 'http://localhost:5001/coviduous-api/us-central1/app/api/'; //se
   //   }
 
   //   //Double check to make sure it isn't still being stored internally
-  //   for (int i = 0; i < shiftGlobals.numAnnouncements; i++) {
+  //   for (int i = 0; i < numAnnouncements; i++) {
   //     if (announcementDatabaseTable[i].shiftId == announcementId) {
   //       announcementDatabaseTable.removeAt(i);
   //       numAnnouncements--;
