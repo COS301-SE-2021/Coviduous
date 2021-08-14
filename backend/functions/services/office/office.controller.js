67ccad28cefc87d633efc0d89ceb1e3a3b483ab4
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-14 15:09:29 +0200

    office.controller.js time function

--- backend/functions/services/office/office.controller.js
+++ backend/functions/services/office/office.controller.js
@@ -12,10 +12,16 @@ Date.prototype.today = function () {
       +(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
 }
 
-// For the time now
+/**
+ * This function returns the current time in a specified format.
+ * @returns {string} The current time as a string in the format HH:MM:SS.
+ */
 Date.prototype.timeNow = function () {
-   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"
+       + ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"
+       + ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 }
+
 exports.createBooking = async (req, res) => {
         try {
           let bookingNumber = "BKN-" + uuid.v4();
