a9c9fcaf09951050a9fe6e38145db50e973c34b2
Peter Okumbe <u18052640@tuks.co.za>
2021-08-14 21:20:33 +0200

    Update notification.integration.test.js

--- backend/functions/tests/notification/notification.integration.test.js
+++ backend/functions/tests/notification/notification.integration.test.js
@@ -17,7 +17,7 @@ chai.use(chaiHttp);
 describe('/POST notifications', () => {
     it('it should create an notification', () => {
         let notification = {
-            notificationId: "test-000",
+            // notificationId: "test-000",
             userId: "test-000",
             userEmail: "test-000",
             subject: "test-000",
@@ -38,19 +38,35 @@ describe('/POST notifications', () => {
         });
     });
 
-    it('it should DELETE a notification', () => {
-        let notification = {
-            notificationId: "test-000"
-        }
-
-        chai.request(server).delete('/api/notifications/')
-            .send(notification)
-            .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            expect(res.body).should.be.a('object');
-            //done();
-        })//.catch(done);
+    it('should DELETE a notification', function(done) {
+        let req = {
+            userId: "test-000",
+            userEmail: "test-000",
+            subject: "test-000",
+            message: "test-000",
+            timestamp: "test-000",
+            adminId: "test-000",
+            companyId: "test-000"
+        };
+  
+         chai.request(server)
+             .post('/api/notifications')
+             .send(req)
+             .end((err, res) => {
+                 console.log(res.body);
+                 let req2 = {
+                     notificationId: res.body.notificationId,
+                 };
+  
+                 chai.request(server)
+                     .delete('/api/notifications')
+                     .send(req2).end((err, res) => {
+                          should.exist(res);
+                          res.should.have.status(200);
+                          console.log(res.body);
+                          done();
+                     });
+             });
     });
 }); 
     
