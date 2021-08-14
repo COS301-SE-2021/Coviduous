64e3dbf7561c74daeaf6eb0b7f715bf7f065ac48
Peter Okumbe <u18052640@tuks.co.za>
2021-08-14 21:02:07 +0200

    Update announcement.unit.test.js

--- backend/functions/tests/announcement/announcement.unit.test.js
+++ backend/functions/tests/announcement/announcement.unit.test.js
@@ -3,20 +3,20 @@ var chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 var expect = chai.expect;
 var should = chai.should();
-var uuid = require("uuid"); // npm install uuid
-var triggers = require('../triggers.js');
-var firebasemock = require('firebase-mock'); // npm install firebase-mock --save-dev
-var mockauth = new firebasemock.MockFirebase();
-var mockfirestore = new firebasemock.MockFirestore();
-var announcementService= require("../../services/announcement/announcement.controller.js");
+// var uuid = require("uuid"); // npm install uuid
+// var triggers = require('../triggers.js');
+// var firebasemock = require('firebase-mock'); // npm install firebase-mock --save-dev
+// var mockauth = new firebasemock.MockFirebase();
+// var mockfirestore = new firebasemock.MockFirestore();
+// var announcementService= require("../../services/announcement/announcement.controller.js");
 
-var mocksdk = firebasemock.MockFirebaseSdk(null, function() {
-  return mockauth;
-}, function() {
-  return mockfirestore;
-});
+// var mocksdk = firebasemock.MockFirebaseSdk(null, function() {
+//   return mockauth;
+// }, function() {
+//   return mockfirestore;
+// });
 
-var mockapp = mocksdk.initializeApp();
+// var mockapp = mocksdk.initializeApp();
 let server = 'http://localhost:5001/coviduous-api/us-central1/app/';
 
 describe('Create announcement unit tests', function() {
@@ -202,78 +202,4 @@ describe('Get announcement unit tests', function() {
            });
    });
 });
-
-/*describe('Firestore Function', function () {
-  beforeEach(function() {
-    mockfirestore = new firebasemock.MockFirestore();
-    mockfirestore.autoFlush();
-    mockauth = new firebasemock.MockFirebase();
-    mockauth.autoFlush();
-  });
-
-  //var uid = '123';
-  var announcementId = "ANNOUNC-" + uuid.v4();
-
-  it('create announcement', function() {
-    var event = {
-      data: new firebasemock.DeltaDocumentSnapshot(mockapp, null, {
-        announcementId: announcementId,
-        type: 'general',
-        message: 'test message',
-        timestamp: 'test',
-        adminId: 'AID-test',
-        companyId: 'CID-test'
-      }, 'announcements/' + announcementId),
-      params: {
-        uid: announcementId
-      }
-    };
-
-    expect(event.data.get('type')).to.equal('general');
-    expect(event.params.uid).to.equal(announcementId);
-
-    triggers.create(event);
-  });
-
-  it('delete announcement', function() {
-    var event = {
-      data: new firebasemock.DeltaDocumentSnapshot(mockapp, {
-        announcementId: announcementId,
-        type: 'general',
-        message: 'test message',
-        timestamp: 'test',
-        adminId: 'AID-test',
-        companyId: 'CID-test'
-      }, null, 'announcements/' + announcementId),
-      params: {
-        uid: announcementId
-      }
-    };
-
-    expect(event.data.previous.get('type')).to.equal('general');
-    expect(event.params.uid).to.equal(announcementId);
-
-    triggers.remove(event);
-  });
-
-//   it('update', function() {
-//     var event = {
-//       data: new firebasemock.DeltaDocumentSnapshot(mockapp, {
-//         name: 'bob',
-//         createdTime: new Date()
-//       }, {
-//         name: 'bobby'
-//       }, 'users/' + uid),
-//       params: {
-//         uid: uid
-//       }
-//     };
-
-//     expect(event.data.previous.get('name')).to.equal('bob');
-//     expect(event.data.get('name')).to.equal('bobby');
-//     expect(event.params.uid).to.equal(uid);
-
-//     triggers.update(event);
-//   });
-});*/
 
