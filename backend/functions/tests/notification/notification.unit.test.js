960ee9b8f06d33c431863cb94de963e9119ba74e
Peter Okumbe <u18052640@tuks.co.za>
2021-08-14 21:29:58 +0200

    Update notification.unit.test.js

--- backend/functions/tests/notification/notification.unit.test.js
+++ backend/functions/tests/notification/notification.unit.test.js
@@ -5,49 +5,33 @@ let server = 'http://localhost:5001/coviduous-api/us-central1/app/';
 
 chai.use(chaiHttp);
 
-describe('Create notification unit test', function() {
-  it('Return 400 if request is empty', function (done) {
-      chai.request(server)
-          .post('/api/notification')
-          .send(null)
-          .end((err, res) => {
-              should.exist(res);
-              res.should.have.status(400);
-              console.log(res.body);
-              done();
-          });
-  });
-
-  it('Return 400 if empty subject', function(done) {
-    let req = {
-      subject:'', 
-      message:'Check the covid update',  
-      userEmail:'nku@gmail.com', 
-      adminId: 'ADMIN-ID',
-      companyId: 'COMPANY-ID'
-    };
-
-    chai.request(server)
-        .post('/api/notification')
-        .send(req)
-        .end((err, res) => {
-            should.exist(res);
-            res.should.have.status(400);
-            console.log(res.body);
-            done();
-        });
-
-      });
-      it('Return 400 if empty message', function(done) {
-        let req = {
-          subject:'Covid TEST', 
-          message:'',  
-          userEmail:'nku@gmail.com', 
-          adminId: 'ADMIN-ID',
-          companyId: 'COMPANY-ID'
-        };
+describe('Create notification unit tests', function() {
+    it('Return 400 if request is empty', function (done) {
         chai.request(server)
-            .post('/api/notification')
+            .post('/api/notifications')
+            .send(null)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                console.log(res.body);
+                done();
+            });
+    });
+ 
+    it('Return 400 if empty subject', function(done) {
+        let req = {
+          notificationId: "test-000",
+          userId: "test-000",
+          userEmail: "test-000",
+          subject: "",
+          message: "test-000",
+          timestamp: "test-000",
+          adminId: "test-000",
+          companyId: "test-000"
+        };
+
+        chai.request(server)
+            .post('/api/notifications')
             .send(req)
             .end((err, res) => {
                 should.exist(res);
@@ -55,17 +39,43 @@ describe('Create notification unit test', function() {
                 console.log(res.body);
                 done();
             });
-     });
-     it('Return 400 if empty userEmail', function(done) {
+    });
+ 
+    it('Return 400 if empty message', function(done) {
+        let req = {
+          userId: "test-000",
+          userEmail: "test-000",
+          subject: "test-000",
+          message: "",
+          timestamp: "test-000",
+          adminId: "test-000",
+          companyId: "test-000"
+        };
+
+        chai.request(server)
+            .post('/api/notifications')
+            .send(req)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                console.log(res.body);
+                done();
+            });
+    });
+
+    it('Return 400 if empty user ID', function(done) {
       let req = {
-        subject:'Covid TEST', 
-        message:'Check the covid update',  
-        userEmail:'', 
-        adminId: 'ADMIN-ID',
-        companyId: 'COMPANY-ID'
+        userId: "",
+        userEmail: "test-000",
+        subject: "test-000",
+        message: "test-000",
+        timestamp: "test-000",
+        adminId: "test-000",
+        companyId: "test-000"
       };
+
       chai.request(server)
-          .post('/api/notification')
+          .post('/api/notifications')
           .send(req)
           .end((err, res) => {
               should.exist(res);
@@ -73,123 +83,130 @@ describe('Create notification unit test', function() {
               console.log(res.body);
               done();
           });
-   });
-   it('Return 400 if empty adminID', function(done) {
-    let req = {
-      subject:'Covid TEST', 
-      message:'Check the covid update',  
-      userEmail:'nku@gmail.com', 
-      adminId: '',
-      companyId: 'COMPANY-ID'
-    };
-    chai.request(server)
-        .post('/api/notification')
-        .send(req)
-        .end((err, res) => {
-            should.exist(res);
-            res.should.have.status(400);
-            console.log(res.body);
-            done();
-        });
+    });
+ 
+    it('Return 400 if empty admin ID', function(done) {
+        let req = {
+          userId: "test-000",
+          userEmail: "test-000",
+          subject: "test-000",
+          message: "test-000",
+          timestamp: "test-000",
+          adminId: "",
+          companyId: "test-000"
+        };
+
+        chai.request(server)
+            .post('/api/notifications')
+            .send(req)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                console.log(res.body);
+                done();
+            });
+    });
+ 
+    it('Return 400 if empty company ID', function(done) {
+        let req = {
+          userId: "test-000",
+          userEmail: "test-000",
+          subject: "test-000",
+          message: "test-000",
+          timestamp: "test-000",
+          adminId: "test-000",
+          companyId: ""
+        };
+
+        chai.request(server)
+            .post('/api/notifications')
+            .send(req)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                console.log(res.body);
+                done();
+            });
+    });
+ 
+    it('Return 200 if creation successful', function(done) {
+        let req = {
+            userId: "test-000",
+            userEmail: "test-000",
+            subject: "test-000",
+            message: "test-000",
+            timestamp: "test-000",
+            adminId: "test-000",
+            companyId: "test-000"
+        };
+ 
+        chai.request(server)
+            .post('/api/notifications')
+            .send(req)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(200);
+                console.log(res.body);
+                done();
+            });
+    })
  });
- it('Return 400 if empty companyId', function(done) {
-  let req = {
-    subject:'Covid TEST', 
-    message:'Check the covid update',  
-    userEmail:'nku@gmail.com', 
-    adminId: 'ADMIN-ID',
-    companyId: ''
-  };
-  chai.request(server)
-      .post('/api/notification')
-      .send(req)
-      .end((err, res) => {
-          should.exist(res);
-          res.should.have.status(400);
-          console.log(res.body);
-          done();
-      });
-});
-
-it('Return 200 if creation successful', function(done) {
-  let req = {
-    subject:'Covid TEST', 
-    message:'Check the covid update',  
-    userEmail:'nku@gmail.com', 
-    adminId: 'ADMIN-ID',
-    companyId: 'COM-ID'
-  };
-
-  chai.request(server)
-      .post('/api/notification')
-      .send(req)
-      .end((err, res) => {
-          should.exist(res);
-          res.should.have.status(200);
-          console.log(res.body);
-          done();
-      });
-});
-
-
-
-
-
-
-});
-describe('Delete notification unit tests', function() {
-  it('Return 400 if request is empty', function(done) {
-      chai.request(server)
-          .delete('/api/notification')
-          .send(null)
-          .end((err, res) => {
-              should.exist(res);
-              res.should.have.status(400);
-              console.log(res.body);
-              done();
-          });
-  });
-
-  it('Return 400 if empty notificationId', function(done) {
-    let req = {
-      notificationId:''
-    };
-    chai.request(server)
-        .delete('/api/notification')
-        .send(req)
-        .end((err, res) => {
-            should.exist(res);
-            res.should.have.status(400);
-            console.log(res.body);
-            done();
-        });
-  });
-  it('Return 200 if deletion is successful', function(done) {
-    let req = {
-      subject:'Covid TEST', 
-      message:'Check the covid update',  
-      userEmail:'nku@gmail.com', 
-      adminId: 'ADMIN-ID',
-      companyId: 'COM-ID'
-    };
-
-     chai.request(server)
-         .post('/api/notification')
-         .send(req)
-         .end((err, res) => {
-             console.log(res.body);
-             let req2 = {
-              notificationId: res.body.notificationId,
-             };
-
-             chai.request(server)
-                 .delete('/api/notification')
-                 .send(req2).end((err, res) => {
-                      should.exist(res);
-                      res.should.have.status(200);
-                      console.log(res.body);
-                      done();
-                 });
-         });
+ 
+ describe('Delete notification unit tests', function() {
+    it('Return 400 if request is empty', function(done) {
+        chai.request(server)
+            .delete('/api/notifications')
+            .send(null)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                console.log(res.body);
+                done();
+            });
+    });
+ 
+    it('Return 200 if deletion is successful', function(done) {
+       let req = {
+          userId: "test-000",
+          userEmail: "test-000",
+          subject: "test-000",
+          message: "test-000",
+          timestamp: "test-000",
+          adminId: "test-000",
+          companyId: "test-000"
+       };
+ 
+        chai.request(server)
+            .post('/api/notifications')
+            .send(req)
+            .end((err, res) => {
+                console.log(res.body);
+                let req2 = {
+                    notificationId: res.body.data.notificationId
+                };
+ 
+                chai.request(server)
+                    .delete('/api/notifications')
+                    .send(req2).end((err, res) => {
+                         should.exist(res);
+                         res.should.have.status(200);
+                         console.log(res.body);
+                         done();
+                    });
+            });
+    });
  });
-});
+ 
+ describe('Get notification unit tests', function() {
+    it('Return 200 if retrieval is successful', function(done) {
+        chai.request(server)
+            .get('/api/notifications')
+            .send()
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(200);
+                console.log(res.body);
+                done();
+            });
+    });
+ });
