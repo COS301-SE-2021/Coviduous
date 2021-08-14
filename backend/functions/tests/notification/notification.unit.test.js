c31a99dadd842ee15450db42d93811339123d292
Peter Okumbe <u18052640@tuks.co.za>
2021-08-14 21:02:16 +0200

    Update notification.unit.test.js

--- backend/functions/tests/notification/notification.unit.test.js
+++ backend/functions/tests/notification/notification.unit.test.js
@@ -1,21 +1,9 @@
 var chai = require("chai");
 var expect = chai.expect;
-var uuid = require("uuid"); // npm install uuid
-var triggers = require('../triggers.js');
-var firebasemock = require('firebase-mock'); // npm install firebase-mock --save-dev
-const { viewNotificationsUserEmail } = require("../../services/notification/notification.controller.js");
-var mockauth = new firebasemock.MockFirebase();
-var mockfirestore = new firebasemock.MockFirestore();
-
-var mocksdk = firebasemock.MockFirebaseSdk(null, function() {
-  return mockauth;
-}, function() {
-  return mockfirestore;
-});
-
-var mockapp = mocksdk.initializeApp();
+//var uuid = require("uuid"); // npm install uuid
 let server = 'http://localhost:5001/coviduous-api/us-central1/app/';
 
+chai.use(chaiHttp);
 
 describe('Create notification unit test', function() {
   it('Return 400 if request is empty', function (done) {
@@ -29,6 +17,7 @@ describe('Create notification unit test', function() {
               done();
           });
   });
+
   it('Return 400 if empty subject', function(done) {
     let req = {
       subject:'', 
@@ -204,100 +193,3 @@ describe('Delete notification unit tests', function() {
          });
  });
 });
-
-  
-
-
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-  /*it('create notification', function() {
-    var notificationId = "NTFN-" + uuid.v4();
-
-    var event = {
-      data: new firebasemock.DeltaDocumentSnapshot(mockapp, null, {
-        notificationId: notificationId,
-        userId: 'UID-test',
-        userEmail: 'test@gmail.com',
-        subject: 'test subject',
-        message: 'test message',
-        timestamp: 'test',
-        adminId: 'AID-test',
-        companyId: 'CID-test'
-      }, 'notifications/' + notificationId),
-      params: {
-        uid: notificationId
-      }
-    };
-
-    expect(event.data.get('subject')).to.equal('test subject');
-    expect(event.params.uid).to.equal(notificationId);
-
-    triggers.create(event);
-  });
-
-  it('view notifications', function() {
-    var notificationId = "NTFN-" + uuid.v4();
-    var notificationId2 = "NTFN-" + uuid.v4();
-    
-    var event = {
-      data: new firebasemock.DeltaDocumentSnapshot(mockapp, {
-        notificationId: notificationId,
-        userId: 'test',
-        userEmail: 'test',
-        subject: 'test',
-        message: 'test',
-        timestamp: 'test',
-        adminId: 'test',
-        companyId: 'test'
-      }, 
-      {
-        notificationId: notificationId2,
-        userId: 'test2',
-        userEmail: 'test2',
-        subject: 'test2',
-        message: 'test2',
-        timestamp: 'test2',
-        adminId: 'test2',
-        companyId: 'test2'
-      }, 'notifications/' + notificationId),
-      params: {
-        uid: notificationId
-      }
-    };
-
-    //expect(event.data.previous.get('subject')).to.equal('test');
-    expect(event.data.get('subject')).to.equal('test2');
-    expect(event.params.uid).to.equal(notificationId);
-
-    triggers.read(event);
-  });
-});*/
