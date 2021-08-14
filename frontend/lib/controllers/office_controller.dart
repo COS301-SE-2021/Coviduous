0545672d5a1e101b4960db4a4fcdfaf89ff3ab4d
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-14 21:53:49 +0200

    office_controller.dart createBooking

--- frontend/lib/controllers/office_controller.dart
+++ frontend/lib/controllers/office_controller.dart
@@ -4,8 +4,42 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:frontend/models/notification/notification.dart';
+import 'package:frontend/models/office/booking.dart';
 import 'package:frontend/controllers/server_info.dart' as serverInfo;
 
-////////////////// FUNCTIONS ////////////////////
+List<Booking> bookingDatabaseTable = [];
+int numBookings = 0;
 
+
+String server = serverInfo.getServer();
+
+Future<bool> createBooking(String bookingNumber, String deskNumber, String floorNumber,
+    String roomNumber, String timestamp, String userId) async {
+  String path = '/office';
+  String url = server + path;
+  var request;
+
+  try {
+    request = http.Request('POST', Uri.parse(url));
+    request.body = json.encode({
+      "bookingNumber": bookingNumber,
+      "userId": userId,
+      "deskNumber": deskNumber,
+      "floorNumber": floorNumber,
+      "roomNumber": roomNumber,
+      "timestamp": timestamp,
+    });
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+
+      return true;
+    }
+  } catch (error) {
+    print(error);
+  }
+
+  return false;
+}
