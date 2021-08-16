39b8d5ea2b6cec62012bab6dceff259d2740221a
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-16 14:58:37 +0200

    trying to resolve overflow error.

--- frontend/lib/views/health/user_home_health.dart
+++ frontend/lib/views/health/user_home_health.dart
@@ -60,7 +60,7 @@ class _UserHealthState extends State<UserHealth> {
               child: Container (
                   height: MediaQuery.of(context).size.height/(2*globals.getWidgetScaling()),
                   width: MediaQuery.of(context).size.width/(2*globals.getWidgetWidthScaling()),
-                  padding: EdgeInsets.all(16),
+                  padding: EdgeInsets.all(20),
                   child: SingleChildScrollView(
                     child: Column (
                         mainAxisAlignment: MainAxisAlignment.start,
