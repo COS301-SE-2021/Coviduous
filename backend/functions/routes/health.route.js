b49ea3274fa47f37762ae463d7d7ef40eb6f110f
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-16 15:02:43 +0200

    Update health.route.js
    Changed a few get routes to post to avoid errors on Flutter web.

--- backend/functions/routes/health.route.js
+++ backend/functions/routes/health.route.js
@@ -1,5 +1,6 @@
 // Import health controller
 const healthController = require("../services/health/health.controller.js");
+let router = require('express').Router();
 // Import database
 const devDatabase = require("../config/health.firestore.database.js");
 const devDatabase2 = require("../config/notification.firestore.database.js");
@@ -12,15 +13,15 @@ healthController.setNotificationDatabase(devDatabase2);
 // N.B. paths for a subsystem can all be the same
 router.post('/health/health-check', healthController.createHealthCheck);
 router.post('/health/report-infection', healthController.reportInfection);
-router.get('/health/permissions', healthController.viewPermissions);
+router.post('/health/permissions/view', healthController.viewPermissions);
 router.delete('/health/permissions',healthController.deletePermissionRequest);
 
 router.post('/health/permissions/permission-request', healthController.createPermissionRequest);
 router.post('/health/permissions/permission-request/grant', healthController.grantPermission);
-router.get('/health/permissions/permission-request', healthController.viewPermissionsRequestsCompanyId);
-router.get('/health/contact-trace/group', healthController.viewGroup);
-router.get('/health/contact-trace/shifts', healthController.viewShifts);
-router.get('/health/contact-trace/notify-group', healthController.notifyGroup);
+router.post('/health/permissions/permission-request/view', healthController.viewPermissionsRequestsCompanyId);
+router.post('/health/contact-trace/group', healthController.viewGroup);
+router.post('/health/contact-trace/shifts', healthController.viewShifts);
+router.post('/health/contact-trace/notify-group', healthController.notifyGroup);
 
 // Export API routes
 module.exports = router;
