9fb297f2ea6631804a0211b25f3fbb09d6d15743
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-16 14:51:07 +0200

    controller updates

--- backend/functions/routes/health.route.js
+++ backend/functions/routes/health.route.js
@@ -1,13 +1,3 @@
-let router = require('express').Router();
-
-// Set default API response
-router.get('/', function (req, res) {
-    res.json({
-        status: 200,
-        message: 'This is the default health API route'
-    });
-});
-
 // Import health controller
 const healthController = require("../services/health/health.controller.js");
 // Import database
@@ -22,16 +12,15 @@ healthController.setNotificationDatabase(devDatabase2);
 // N.B. paths for a subsystem can all be the same
 router.post('/health/health-check', healthController.createHealthCheck);
 router.post('/health/report-infection', healthController.reportInfection);
-//router.get('/health/health-check', healthController.viewHealthChecks);
-//router.get('/health/health-check/userid', healthController.viewHealthCheckUserId);
-//router.post('/health/permissions', healthController.createPermission);
 router.get('/health/permissions', healthController.viewPermissions);
-//router.get('/health/permissions/userid', healthController.viewPermissionsUserId);
+router.delete('/health/permissions',healthController.deletePermissionRequest);
+
 router.post('/health/permissions/permission-request', healthController.createPermissionRequest);
 router.post('/health/permissions/permission-request/grant', healthController.grantPermission);
 router.get('/health/permissions/permission-request', healthController.viewPermissionsRequestsCompanyId);
 router.get('/health/contact-trace/group', healthController.viewGroup);
 router.get('/health/contact-trace/shifts', healthController.viewShifts);
-//router.delete('/health/permissions',healthController.deletePermissionsPermissionId);
+router.get('/health/contact-trace/notify-group', healthController.notifyGroup);
+
 // Export API routes
 module.exports = router;
