d303f054164a2f19d2a18e06cd37a0768b2b80ef
Peter Okumbe <u18052640@tuks.co.za>
2021-08-16 23:24:55 +0200

    Update shift.unit.test.js

--- backend/functions/tests/shift/shift.unit.test.js
+++ backend/functions/tests/shift/shift.unit.test.js
@@ -171,54 +171,55 @@ describe('Create shift unit tests', function() {
     })
 });
  
-// describe('Delete shift unit tests', function() {
-//     it('Return 400 if request is empty', function(done) {
-//         chai.request(server)
-//             .delete('/api/shift')
-//             .send(null)
-//             .end((err, res) => {
-//                 should.exist(res);
-//                 res.should.have.status(400);
-//                 //console.log(res.body);
-//                 done();
-//             });
-//     });
+describe('Delete shift unit tests', function() {
+    it('Return 400 if request is empty', function(done) {
+        chai.request(server)
+            .delete('/api/shift')
+            .send(null)
+            .end((err, res) => {
+                should.exist(res);
+                res.should.have.status(400);
+                //console.log(res.body);
+                done();
+            });
+    });
  
-//     it('Return 200 if deletion is successful', function() {
-//         let req = {
-//             date: "test-000",
-//             startTime: "test-000",
-//             endTime: "test-000",
-//             description: "test-000",
-//             floorNumber: "test-000",
-//             roomNumber: "test-000",
-//             groupNumber: "test-000",
-//             floorPlanNumber: "test-000",
-//             adminId: "AID-test",
-//             companyId: "CID-test"
-//         };
+    it('Return 200 if deletion is successful', function() {
+        let req = {
+            date: "test-000",
+            startTime: "test-000",
+            endTime: "test-000",
+            description: "test-000",
+            floorNumber: "test-000",
+            roomNumber: "test-000",
+            groupNumber: "test-000",
+            floorPlanNumber: "test-000",
+            adminId: "AID-test",
+            companyId: "CID-test"
+        };
  
-//         chai.request(server)
-//             .post('/api/shift')
-//             .send(req)
-//             .end((err, res) => {
-//                 console.log(res.body);
-//                 let req2 = {
-//                     shiftID: res.body.data.shiftID
-//                 };
+        chai.request(server)
+            .post('/api/shift')
+            .send(req)
+            .end((err, res) => {
+                console.log(res.body);
+                let req2 = {
+                    shiftId: res.body.data.shiftID
+                };
+                console.log(req2);
  
-//                 chai.request(server)
-//                     .delete('/api/shift')
-//                     .send(req2)
-//                     .end((err, res) => {
-//                          should.exist(res);
-//                          res.should.have.status(200);
-//                          //console.log(res.body);
-//                          //done();
-//                     });
-//             });
-//     });
-// });
+                chai.request(server)
+                    .delete('/api/shift')
+                    .send(req2)
+                    .end((err, res) => {
+                         should.exist(res);
+                         res.should.have.status(200);
+                         //console.log(res.body);
+                         //done();
+                    });
+            });
+    });
+});
  
 describe('Get shifts unit tests', function() {
     it('Return 200 if retrieval is successful', function() {
