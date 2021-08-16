f0b5dc006fef40e551f8ae32e179765713933aac
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-16 10:52:00 +0200

    permission request model update

--- backend/functions/models/permission.request.model.js
+++ backend/functions/models/permission.request.model.js
@@ -1,9 +1,10 @@
 class PermissionRequest {
-    constructor(permissionRequestId, permissionId,userId, shiftNumber, timestamp, reason, adminId, companyId)
+    constructor(permissionRequestId, permissionId,userId, userEmail,shiftNumber, timestamp, reason, adminId, companyId)
     {
         this.permissionRequestId = permissionRequestId;
         this.permissionId=permissionId;
         this.userId = userId;
+        this.userEmail = userEmail;
         this.shiftNumber = shiftNumber;
         this.timestamp = timestamp;
         this.reason = reason;
