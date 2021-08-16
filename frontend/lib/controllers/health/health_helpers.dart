3c14024632d3d70c8b05ef84e68197034b518ced
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-16 16:22:07 +0200

    Create health_helpers.dart
    Helper class for the health controller.

---
+++ frontend/lib/controllers/health/health_helpers.dart
@@ -0,0 +1,151 @@
+import 'package:frontend/controllers/health/health_controller.dart' as healthController;
+import 'package:frontend/globals.dart' as globals;
+
+Future<bool> createHealthCheckUser(String temperature, bool fever, bool cough, bool soreThroat,
+    bool chills, bool aches, bool nausea, bool shortnessOfBreath, bool lossOfTasteSmell,
+    bool sixFeetContact, bool testedPositive, bool travelled, bool headache) async {
+  bool result = false;
+  Future.wait([
+    healthController.createHealthCheck(globals.loggedInUserId, globals.loggedInUser.getFirstName(), globals.loggedInUser.getLastName(),
+        globals.loggedInUserEmail, "", temperature, fever, cough, soreThroat, chills, aches, nausea,
+        shortnessOfBreath, lossOfTasteSmell, sixFeetContact, testedPositive, travelled, headache)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentHealthCheck = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> createHealthCheckVisitor(String firstName, String lastName, String email, String phoneNumber,
+    String temperature, bool fever, bool cough, bool soreThroat, bool chills, bool aches, bool nausea,
+    bool shortnessOfBreath, bool lossOfTasteSmell, bool sixFeetContact, bool testedPositive, bool travelled, bool headache) async {
+  bool result = false;
+  Future.wait([
+    healthController.createHealthCheck("VISITOR", firstName, lastName, email, phoneNumber, temperature, fever, cough,
+        soreThroat, chills, aches, nausea, shortnessOfBreath, lossOfTasteSmell, sixFeetContact, testedPositive, travelled, headache)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentHealthCheck = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> getPermissionsUser() async {
+  bool result = false;
+  Future.wait([
+    healthController.getPermissions(globals.loggedInUserEmail)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentPermissions = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> getPermissionsVisitor(String email) async {
+  bool result = false;
+  Future.wait([
+    healthController.getPermissions(email)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentPermissions = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> reportInfection() async {
+  bool result = false;
+  Future.wait([
+    healthController.reportInfection(globals.loggedInUserId, globals.loggedInUserEmail, "adminId", globals.loggedInCompanyId)
+  ]).then((results) {
+    result = results.first;
+  });
+  return result;
+}
+
+Future<bool> createPermissionRequest(String reason) async {
+  bool result = false;
+  Future.wait([
+    healthController.createPermissionRequest(globals.currentPermissionId, globals.loggedInUserId,
+        globals.loggedInUserEmail, globals.currentShiftNum, reason, "adminId", globals.loggedInCompanyId)
+  ]).then((results) {
+    result = results.first;
+  });
+  return result;
+}
+
+Future<bool> getPermissionRequests() async {
+  bool result = false;
+  Future.wait([
+    healthController.getPermissionRequests(globals.loggedInCompanyId)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentPermissionRequests = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> deletePermissionRequest() async {
+  bool result = false;
+  Future.wait([
+    healthController.deletePermissionRequest(globals.currentPermissionRequestId)
+  ]).then((results) {
+    result = results.first;
+  });
+  return result;
+}
+
+Future<bool> grantPermission(String userId, String userEmail) async {
+  bool result = false;
+  Future.wait([
+    healthController.grantPermission(userId, userEmail, globals.loggedInUserId, globals.loggedInCompanyId)
+  ]).then((results) {
+    result = results.first;
+  });
+  return result;
+}
+
+Future<bool> viewGroup() async {
+  bool result = false;
+  Future.wait([
+    healthController.viewGroup(globals.currentShiftNum)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentGroup = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> viewShifts(String userEmail) async {
+  bool result = false;
+  Future.wait([
+    healthController.viewShifts(userEmail)
+  ]).then((results) {
+    if (results != null) {
+      globals.currentShifts = results.first;
+      result = true;
+    }
+  });
+  return result;
+}
+
+Future<bool> notifyGroup() async {
+  bool result = false;
+  Future.wait([
+    healthController.notifyGroup(globals.currentShiftNum)
+  ]).then((results) {
+    result = results.first;
+  });
+  return result;
+}
