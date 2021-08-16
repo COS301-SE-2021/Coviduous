dbb72dafd274cd55098dcff63b79063e6e4f3f69
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-16 18:33:43 +0200

    visitor_view_permissions.dart

--- frontend/lib/views/health/visitor_view_permissions.dart
+++ frontend/lib/views/health/visitor_view_permissions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-
 import 'package:frontend/views/admin_homepage.dart';
 import 'package:frontend/views/user_homepage.dart';
 import 'package:frontend/views/health/visitor_home_health.dart';
