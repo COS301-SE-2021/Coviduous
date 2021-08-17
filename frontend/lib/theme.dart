9e5e63f9daec42b0b9ceb490c6e4b61d2a41523d
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-17 13:41:36 +0200

    Changing color scheme again

--- frontend/lib/theme.dart
+++ frontend/lib/theme.dart
@@ -2,30 +2,30 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 Color appBarColor = Color(0xff3B324E);
-Color primaryColor = Color(0xff4CF9FF);
+Color primaryColor = Color(0xff006266);
 Color secondaryColor = Color(0xff251F34);
 Color lineColor = Color(0xffBBACE5);
 Color focusColor = Color(0xff0D9999);
 
 Map<int, Color> textFieldSelectedSwatch = {
-  50: Color.fromRGBO(76, 249, 255, .1),
-  100: Color.fromRGBO(76, 249, 255, .2),
-  200: Color.fromRGBO(76, 249, 255, .3),
-  300: Color.fromRGBO(76, 249, 255, .4),
-  400: Color.fromRGBO(76, 249, 255, .5),
-  500: Color.fromRGBO(76, 249, 255, .6),
-  600: Color.fromRGBO(76, 249, 255, .7),
-  700: Color.fromRGBO(76, 249, 255, .8),
-  800: Color.fromRGBO(76, 249, 255, .9),
-  900: Color.fromRGBO(76, 249, 255, 1),
+  50: Color.fromRGBO(0, 98, 102, .1),
+  100: Color.fromRGBO(0, 98, 102, .2),
+  200: Color.fromRGBO(0, 98, 102, .3),
+  300: Color.fromRGBO(0, 98, 102, .4),
+  400: Color.fromRGBO(0, 98, 102, .5),
+  500: Color.fromRGBO(0, 98, 102, .6),
+  600: Color.fromRGBO(0, 98, 102, .7),
+  700: Color.fromRGBO(0, 98, 102, .8),
+  800: Color.fromRGBO(0, 98, 102, .9),
+  900: Color.fromRGBO(0, 98, 102, 1),
 };
 
-MaterialColor textFieldSelectedColor = MaterialColor(0xff4CF9FF, textFieldSelectedSwatch);
+MaterialColor textFieldSelectedColor = MaterialColor(0xff006266, textFieldSelectedSwatch);
 
 TextTheme textTheme = TextTheme(
   bodyText1: TextStyle(color: Colors.black),
   bodyText2: TextStyle(color: Colors.black),
-  button: TextStyle(color: Colors.black),
+  button: TextStyle(color: Colors.white),
   caption: TextStyle(color: Colors.black),
   headline1: TextStyle(color: Colors.black),
   headline2: TextStyle(color: Colors.black),
