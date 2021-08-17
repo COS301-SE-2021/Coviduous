b42a73fd09e6482735067f405406347bab6cef4f
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-08-17 12:14:27 +0200

    mock database routes updated

--- backend/functions/tests/triggers.js
+++ backend/functions/tests/triggers.js
@@ -3,18 +3,22 @@
 
 function create(event) {
   console.log(event.params.uid);
+  console.log("Stored Notification Object Into The Mock Database Successfully");
 }
 
 function read(event) {
     console.log(event.params.uid);
+    console.log("Read Notification Object In The Mock Database Successfully");
   }
 
 function update(event) {
   console.log(event.params.uid);
+  console.log("Updated Notification Object In The Mock Database Successfully");
 }
 
 function remove(event) {
   console.log(event.params.uid);
+  console.log("Removed Notification Object In The Mock Database Successfully");
 }
 
 module.exports = {
