2ed7decbcc3820684655eae1a65234622f0f1e17
u18202374-Chaks <u18202374@tuks.co.za>
2021-08-18 14:04:13 +0200

    SHift unit test

--- backend/functions/routes/shift.route.js
+++ backend/functions/routes/shift.route.js
@@ -1,6 +1,6 @@
 let router = require('express').Router();
 let db = require("../config/shift.firestore.database.js");
-
+let database = require("../config/mock_database/shift.firestore.mock.database");
 /**
  * Default a Get router 
  */
@@ -12,8 +12,11 @@ let db = require("../config/shift.firestore.database.js");
 });
 
 const Shift = require("../services/shift/shift.controller.js");
+const ShiftMock = require("../services/shift/shift.controller.js");
+
 
 Shift.setDatabase(db);
+ShiftMock.setDatabase(database);
 
 /**
  * Shift Routes
@@ -28,6 +31,23 @@ router.put('/shift',Shift.updateShift);
 router.get('/group',Shift.getGroup);
 router.post('/group/shift-id',Shift.getGroupForShift);
 router.post('/group',Shift.createGroup);
+
+
+/**
+ * shift mock routes 
+ */
+ router.post('/mock/shift',ShiftMock.createShift);
+ router.delete('/mock/shift',ShiftMock.deleteShift);
+ router.get('/mock/shift',ShiftMock.viewShifts);
+ router.put('/mock/shift',ShiftMock.updateShift);
+
+/**
+ * group mock routes 
+ */
+ router.get('/mock/group',ShiftMock.getGroup);
+ router.post('/mock/group/shift-id',ShiftMock.getGroupForShift);
+ router.post('/mock/group',ShiftMock.createGroup);
+ 
 
 // Export API routes
 module.exports = router
