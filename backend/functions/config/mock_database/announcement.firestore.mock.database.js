78a1f027372b75df4d66bb5acff2d4d0184d35c2
u18202374-Chaks <u18202374@tuks.co.za>
2021-08-18 13:32:15 +0200

    Updated firestore mock database

--- backend/functions/config/mock_database/announcement.firestore.mock.database.js
+++ backend/functions/config/mock_database/announcement.firestore.mock.database.js
@@ -19,3 +19,59 @@ exports.getDataBucket = async () => {
     return databucket;
 }
 
+
+exports.createAnnouncement = async (announcementId, testdata) => {
+
+    databucket = {
+        data: new firebasemock.DeltaDocumentSnapshot(mockapp, null, {
+            announcementId: announcementId,
+            type: testdata.type,
+            message: testdata.message,
+            timestamp: testdata.timestamp,
+            adminId: testdata.adminId,
+            companyId: testdata.companyId
+        }, 'announcements/' + announcementId),
+        params: {
+          uid: announcementId
+        }
+      };
+
+      triggers.create(databucket);
+      return true;
+};
+exports.deleteAnnouncement = async (announcementId) => {
+    databucket = {
+        data: new firebasemock.DeltaDocumentSnapshot(mockapp, null, {
+            announcementId: announcementId,
+            type: "",
+            message: "",
+            timestamp: "",
+            adminId: "",
+            companyId: "" 
+          
+        }, 'announcements/' + announcementId),
+        params: {
+          uid: announcementId
+        }
+      };
+      triggers.remove(databucket);
+      return true;
+};
+exports.viewAnnouncements = async () => {
+    databucket = {
+        data: new firebasemock.DeltaDocumentSnapshot(mockapp, null, {
+            announcementId: announcementId,
+            type: "General",
+            message: "Alert",
+            timestamp: "12 Aug ",
+            adminId: "ADM-110",
+            companyId: "CID-120" 
+          
+        }, 'announcements/' + "mock object"),
+        params: {
+          uid: "mock object"
+        }
+      };
+      triggers.read(databucket);
+      return true;
+};
