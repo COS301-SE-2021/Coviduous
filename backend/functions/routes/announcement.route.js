44b577540e9e37ea90cc3a922209c4cb11e5a456
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-18 12:57:04 +0200

    Fixing error where the development database is being overwritten by the mock database

--- backend/functions/routes/announcement.route.js
+++ backend/functions/routes/announcement.route.js
@@ -10,15 +10,15 @@ router.get('/', function (req, res) {
 
 // Import announcement controller
 const announcementController = require("../services/announcement/announcement.controller.js");
-const announcementControllerMock = require("../services/announcement/announcement.controller.js");
+//const announcementControllerMock = require("../services/announcement/announcement.controller.js");
 
 // Import database
 const devDatabase = require("../config/announcement.firestore.database.js");
-const mockDatabase = require("../config/mock_database/announcement.firestore.mock.database");
+//const mockDatabase = require("../config/mock_database/announcement.firestore.mock.database");
 
 // set database to use
 announcementController.setDatabase(devDatabase);
-announcementControllerMock.setDatabase(mockDatabase);
+//announcementControllerMock.setDatabase(mockDatabase);
 
 // Announcement routes
 // N.B. paths for a subsystem can all be the same
@@ -26,12 +26,10 @@ router.get('/announcements', announcementController.viewAnnouncements);
 router.post('/announcements', announcementController.createAnnouncement);
 router.delete('/announcements', announcementController.deleteAnnouncement);
 
-
 //Announcement routes mock
-router.get('/mock/announcements', announcementControllerMock.viewAnnouncements);
-router.post('/mock/announcements', announcementControllerMock.createAnnouncement);
-router.delete('/mock/announcements', announcementControllerMock.deleteAnnouncement);
-
+//router.get('/mock/announcements', announcementControllerMock.viewAnnouncements);
+//router.post('/mock/announcements', announcementControllerMock.createAnnouncement);
+//router.delete('/mock/announcements', announcementControllerMock.deleteAnnouncement);
 
 // Export API routes
 module.exports = router;
