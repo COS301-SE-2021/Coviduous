209a0e2ec68f01361bc1c4901975830e652b56b6
Peter Okumbe <u18052640@tuks.co.za>
2021-08-23 09:53:29 +0200

    Update notification.controller.js

--- backend/functions/services/notification/notification.controller.js
+++ backend/functions/services/notification/notification.controller.js
@@ -29,86 +29,86 @@ exports.verifyCredentials = async (adminId, companyId) => {
  * @returns res An HTTP status indicating whether the request was successful or not.
  */
 exports.createNotification = async (req, res) => {
-        let fieldErrors = [];
-        
-        let reqJson;
-        try {
-            reqJson = JSON.parse(req.body);
-        } catch (e) {
-            reqJson = req.body;
-        }
-        console.log(reqJson);
-
-
-        if(req.body == null) {
-            fieldErrors.push({field: null, message: 'Request object may not be null'});
-        }
-
-        if (reqJson.userId == null || reqJson.userId === '') {
-            fieldErrors.push({field: 'userId', message: 'User ID may not be empty'});
-        }
-
-        if (reqJson.userEmail == null || reqJson.userEmail === "") {
-            fieldErrors.push({field: 'userEmail', message: 'User email may not be empty'});
-        }
-
-        if (reqJson.subject == null || reqJson.subject === "") {
-            fieldErrors.push({field: 'subject', message: 'Subject may not be empty'});
-        }
-
-        if (reqJson.message == null || reqJson.message === "") {
-            fieldErrors.push({field: 'message', message: 'Message may not be empty'});
-        }
-
-        if (reqJson.adminId == null || reqJson.adminId === "") {
-            fieldErrors.push({field: 'adminId', message: 'Admin ID may not be empty'});
-        }
-
-        if (reqJson.companyId == null || reqJson.companyId === "") {
-            fieldErrors.push({field: 'companyId', message: 'Company ID may not be empty'});
-        }
-
-        if (fieldErrors.length > 0) {
-            return res.status(400).send({
-                message: '400 Bad Request: Incorrect fields',
-                errors: fieldErrors
-            });
-        }
-
-        if (await this.verifyCredentials(reqJson.adminId, reqJson.companyId) === false) {
-            return res.status(403).send({
-                message: '403 Forbidden: Access denied',
-            });
-        }
-
-        let notificationId = "NTFN-" + uuid.v4();
-        let timestamp = new Date().toISOString();
+    let fieldErrors = [];
     
-        let notificationObj = new Notification(notificationId, reqJson.userId, reqJson.userEmail,
-            reqJson.subject, reqJson.message, timestamp, reqJson.adminId, reqJson.companyId);
-    
-        let notificationData = {
-          notificationId: notificationId,
-          userId: reqJson.userId,
-          userEmail: reqJson.userEmail,
-          subject: reqJson.subject,
-          message: reqJson.message,
-          timestamp: timestamp,
-          adminId: reqJson.adminId,
-          companyId: reqJson.companyId
-        }
-    
-        if (await database.createNotification(notificationData.notificationId, notificationData) == true)
-        {
-          return res.status(200).send({
-            message: 'Notification successfully created',
-            data: notificationData
-          });
-        }
-        else
-        {
-            return res.status(500).send({message: "500 server error."});
-        }
+    let reqJson;
+    try {
+        reqJson = JSON.parse(req.body);
+    } catch (e) {
+        reqJson = req.body;
+    }
+    console.log(reqJson);
+
+
+    if(req.body == null) {
+        fieldErrors.push({field: null, message: 'Request object may not be null'});
+    }
+
+    if (reqJson.userId == null || reqJson.userId === '') {
+        fieldErrors.push({field: 'userId', message: 'User ID may not be empty'});
+    }
+
+    if (reqJson.userEmail == null || reqJson.userEmail === "") {
+        fieldErrors.push({field: 'userEmail', message: 'User email may not be empty'});
+    }
+
+    if (reqJson.subject == null || reqJson.subject === "") {
+        fieldErrors.push({field: 'subject', message: 'Subject may not be empty'});
+    }
+
+    if (reqJson.message == null || reqJson.message === "") {
+        fieldErrors.push({field: 'message', message: 'Message may not be empty'});
+    }
+
+    if (reqJson.adminId == null || reqJson.adminId === "") {
+        fieldErrors.push({field: 'adminId', message: 'Admin ID may not be empty'});
+    }
+
+    if (reqJson.companyId == null || reqJson.companyId === "") {
+        fieldErrors.push({field: 'companyId', message: 'Company ID may not be empty'});
+    }
+
+    if (fieldErrors.length > 0) {
+        return res.status(400).send({
+            message: '400 Bad Request: Incorrect fields',
+            errors: fieldErrors
+        });
+    }
+
+    if (await this.verifyCredentials(reqJson.adminId, reqJson.companyId) === false) {
+        return res.status(403).send({
+            message: '403 Forbidden: Access denied',
+        });
+    }
+
+    let notificationId = "NTFN-" + uuid.v4();
+    let timestamp = new Date().toISOString();
+
+    let notificationObj = new Notification(notificationId, reqJson.userId, reqJson.userEmail,
+        reqJson.subject, reqJson.message, timestamp, reqJson.adminId, reqJson.companyId);
+
+    let notificationData = {
+        notificationId: notificationId,
+        userId: reqJson.userId,
+        userEmail: reqJson.userEmail,
+        subject: reqJson.subject,
+        message: reqJson.message,
+        timestamp: timestamp,
+        adminId: reqJson.adminId,
+        companyId: reqJson.companyId
+    }
+
+    if (await database.createNotification(notificationData.notificationId, notificationData) == true)
+    {
+        return res.status(200).send({
+        message: 'Notification successfully created',
+        data: notificationData
+        });
+    }
+    else
+    {
+        return res.status(500).send({message: "500 server error."});
+    }
 };
 
 /**
@@ -120,47 +120,41 @@ exports.createNotification = async (req, res) => {
  * @returns res - HTTP status indicating whether the request was successful or not.
  */
 exports.deleteNotification = async (req, res) => {
-    // try {
-        // data validation
-        let fieldErrors = [];
+    let fieldErrors = [];
 
-        let reqJson;
-        try {
-            reqJson = JSON.parse(req.body);
-        } catch (e) {
-            reqJson = req.body;
-        }
-        console.log(reqJson);
+    let reqJson;
+    try {
+        reqJson = JSON.parse(req.body);
+    } catch (e) {
+        reqJson = req.body;
+    }
+    console.log(reqJson);
 
-        if(req.body == null) {
-            fieldErrors.push({field: null, message: 'Request object may not be null'});
-        }
+    if(req.body == null) {
+        fieldErrors.push({field: null, message: 'Request object may not be null'});
+    }
 
-        if (reqJson.notificationId == null || reqJson.notificationId === '') {
-            fieldErrors.push({field: 'notificationId', message: 'Notification ID may not be empty'});
-        }
+    if (reqJson.notificationId == null || reqJson.notificationId === '') {
+        fieldErrors.push({field: 'notificationId', message: 'Notification ID may not be empty'});
+    }
 
-        if (fieldErrors.length > 0) {
-            return res.status(400).send({
-                message: '400 Bad Request: Incorrect fields',
-                errors: fieldErrors
-            });
-        }
+    if (fieldErrors.length > 0) {
+        return res.status(400).send({
+            message: '400 Bad Request: Incorrect fields',
+            errors: fieldErrors
+        });
+    }
 
-        if (await database.deleteNotification(req.body.notificationId) == true)
-        {
-            return res.status(200).send({
-                message: 'Notification successfully deleted',
-            });
-        }
-        else
-        {
-            return res.status(500).send({message: "500 server error."});
-        }
-    // } catch (error) {
-    //     console.log(error);
-    //     return res.status(500).send(error);
-    // }
+    if (await database.deleteNotification(req.body.notificationId) == true)
+    {
+        return res.status(200).send({
+            message: 'Notification successfully deleted',
+        });
+    }
+    else
+    {
+        return res.status(500).send({message: "500 server error."});
+    }
 };
 
 /**
