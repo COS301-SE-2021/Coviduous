d19a697cbd10fd3d8d2ae06e2159d65d1a0f1086
Peter Okumbe <u18052640@tuks.co.za>
2021-08-23 20:59:46 +0200

    Update reporting.firestore.database.js

--- backend/functions/config/reporting.firestore.database.js
+++ backend/functions/config/reporting.firestore.database.js
@@ -2,32 +2,51 @@ let admin = require('firebase-admin');
 let db = admin.firestore();
 
 // functions
-// exports.addSickEmployee = async (sickEmployeeId, data) => {
-//     try {
-//         await db.collection('sick-employees').doc(sickEmployeeId)
-//           .create(data); // .add - auto generates document id
-//         return true;
-//     } catch (error) {
-//         console.log(error);
-//         return false;
-//     }
-// };
+exports.addSickEmployee = async (sickEmployeeId, data) => {
+    try {
+        await db.collection('sick-employees').doc(sickEmployeeId)
+          .create(data); // .add - auto generates document id
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
 
-// exports.viewSickEmployees = async () => {
+exports.viewSickEmployees = async () => {
+    try {
+        const document = db.collection('sick-employees');
+        const snapshot = await document.get();
+        
+        let list = [];
+        
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
+    
+        let sickEmployees = list;
+        
+        return sickEmployees;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+// exports.viewSickEmployeesCompanyId = async (companyId) => {
 //     try {
-//         const document = db.collection('sick-employees');
+//         const document = db.collection('sick-employees').where("companyId", "==", companyId);
 //         const snapshot = await document.get();
-        
+
 //         let list = [];
-        
+
 //         snapshot.forEach(doc => {
 //             let data = doc.data();
 //             list.push(data);
 //         });
-    
-//         let sickEmployees = list;
-        
-//         return sickEmployees;
+
+//         return list;
 //     } catch (error) {
 //         console.log(error);
 //         return error;
