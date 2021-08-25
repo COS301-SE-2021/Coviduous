77093a2a4acd94ea526d48a2c2c5988dff445693
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-25 16:23:58 +0200

    reporting_health_overall.dart

---
+++ frontend/lib/views/reporting/reporting_health.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'package:frontend/views/admin_homepage.dart';
+import 'package:frontend/views/reporting/reporting_floor_plans.dart';
+import 'package:frontend/views/user_homepage.dart';
+import 'package:frontend/views/login_screen.dart';
+
+import 'package:frontend/controllers/floor_plan/floor_plan_helpers.dart' as floorPlanHelpers;
+import 'package:frontend/globals.dart' as globals;
+
+class ReportingHealth extends StatefulWidget {
+  static const routeName = "/reporting";
+
+  @override
+  _ReportingHealthState createState() => _ReportingHealthState();
+}
+//class admin
+class _ReportingHealthState extends State<ReportingHealth> {
+  Future<bool> _onWillPop() async {
+    Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+    return (await true);
+  }
+  @override
+  Widget build(BuildContext context) {
+    //If incorrect type of user, don't allow them to view this page.
+    if (globals.loggedInUserType != 'ADMIN') {
+      if (globals.loggedInUserType == 'USER') {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        });
+      }
+      return Container();
+    }
+  }
+}
