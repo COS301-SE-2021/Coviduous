f9ec6fcf9294c3a4c0c2e7c7a4ec2e7d1d75c060
Peter Okumbe <u18052640@tuks.co.za>
2021-08-25 20:50:53 +0200

    Update reporting.firestore.database.js

--- backend/functions/config/reporting.firestore.database.js
+++ backend/functions/config/reporting.firestore.database.js
@@ -1,7 +1,7 @@
 let admin = require('firebase-admin');
 let db = admin.firestore();
 
-// functions
+////////// HEALTH REPORTING ////////////
 exports.addSickEmployee = async (sickEmployeeId, data) => {
     try {
         await db.collection('sick-employees').doc(sickEmployeeId)
@@ -75,6 +75,39 @@ exports.viewSickEmployees = async (companyId) => {
         });
 
         return list;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+////////// COMPANY REPORTING ////////////
+exports.addCompanyData = async (companyId, data) => {
+    try {
+        await db.collection('company-data').doc(companyId)
+          .create(data);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+
+exports.viewCompanyData = async () => {
+    try {
+        const document = db.collection('company-data');
+        const snapshot = await document.get();
+        
+        let list = [];
+        
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
+    
+        let companyData = list;
+        
+        return companyData;
     } catch (error) {
         console.log(error);
         return error;
