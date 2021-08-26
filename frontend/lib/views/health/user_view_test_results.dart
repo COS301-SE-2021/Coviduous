84f905f358a75d7c68d403e785ff84f3bc0ea0eb
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-26 22:25:26 +0200

    Update user_view_test_results.dart
    This screen now properly displays a list of test results.

--- frontend/lib/views/health/user_view_test_results.dart
+++ frontend/lib/views/health/user_view_test_results.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
-import 'package:frontend/views/admin_homepage.dart';
 import 'package:frontend/views/health/user_home_health.dart';
 import 'package:frontend/views/health/user_upload_test_results.dart';
+import 'package:frontend/views/health/user_view_single_test_result.dart';
+import 'package:frontend/views/admin_homepage.dart';
 import 'package:frontend/views/login_screen.dart';
-import 'package:native_pdf_view/native_pdf_view.dart';
 
 import 'package:frontend/globals.dart' as globals;
 
@@ -17,18 +17,12 @@ class UserViewTestResults extends StatefulWidget {
 }
 
 class _UserViewTestResultsState extends State<UserViewTestResults> {
-  final pdfController = PdfController(
-    document: PdfDocument.openAsset('assets/sample.pdf'),
-  );
-
   Future<bool> _onWillPop() async {
     Navigator.of(context).pushReplacementNamed(UserHealth.routeName);
     return (await true);
   }
-
   @override
   Widget build(BuildContext context) {
-    //If incorrect type of user, don't allow them to view this page.
     if (globals.loggedInUserType != 'USER') {
       if (globals.loggedInUserType == 'ADMIN') {
         SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
@@ -41,82 +35,129 @@ class _UserViewTestResultsState extends State<UserViewTestResults> {
       }
       return Container();
     }
-
     Widget getList() {
-      if (!globals.testResultsExist) { //If COVID test results have not been uploaded yet
-        return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
-                height: MediaQuery.of(context).size.height/(24*globals.getWidgetScaling()),
-                color: Theme.of(context).primaryColor,
-                child: Text('No test results found', style: TextStyle(color: Colors.white,
-                    fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5)),
-              ),
-              Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width/(2*globals.getWidgetScaling()),
-                  height: MediaQuery.of(context).size.height/(12*globals.getWidgetScaling()),
-                  color: Colors.white,
-                  padding: EdgeInsets.all(12),
-                  child: Text('Please upload your COVID-19 test results.', style: TextStyle(fontSize: (MediaQuery.of(context).size.height * 0.01) * 2.5))
-              )
-            ]
-        );
-      } else { //Else, return a container showing the company guidelines
-        return Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width/(1.5*globals.getWidgetScaling()),
-            height: MediaQuery.of(context).size.height/(1.5*globals.getWidgetScaling()),
-            color: Colors.white,
-            child: PdfView(
-              documentLoader: Center(child: CircularProgressIndicator()),
-              pageLoader: Center(child: CircularProgressIndicator()),
-              controller: pdfController,
-            ),
-        );
-      }
-    }
+      int numberOfResults = globals.currentTestResults.length;
 
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: new Scaffold(
-          appBar: AppBar(
-            title: Text('View COVID-19 test results'),
-            leading: BackButton( //Specify back button
-              onPressed: (){
-                Navigator.of(context).pushReplacementNamed(UserHealth.routeName);
-              },
-            ),
+      print(numberOfResults);
+
+      if (numberOfResults == 0) {
+        return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height /
+                (5 * globals.getWidgetScaling()),
           ),
-          body: Stack (
-              children: <Widget>[
-                Center (
-                    child: getList()
-                ),
-                Container (
-                  alignment: Alignment.bottomRight,
-                  child: Container (
-                      height: 50,
-                      width: 200,
-                      padding: EdgeInsets.all(10),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom (
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width /
+                (2 * globals.getWidgetScaling()),
+            height: MediaQuery.of(context).size.height /
+                (24 * globals.getWidgetScaling()),
+            color: Theme.of(context).primaryColor,
+            child: Text('No test results found',
+                style: TextStyle(color: Colors.white,
+                    fontSize:
+                    (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width /
+                  (2 * globals.getWidgetScaling()),
+              height: MediaQuery.of(context).size.height /
+                  (12 * globals.getWidgetScaling()),
+              color: Colors.white,
+              padding: EdgeInsets.all(12),
+              child: Text('You have not uploaded any test results.',
+                  style: TextStyle(
+                      fontSize:
+                      (MediaQuery.of(context).size.height * 0.01) * 2.5)))
+        ]);
+      } else {
+        //Else create and return a list
+        return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: EdgeInsets.all(16),
+            itemCount: numberOfResults,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Column(children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    color: Theme.of(context).primaryColor,
+                    child: Text('Employee ' + globals.currentTestResults[index].getUserId()),
+                  ),
+                  ListView(
+                      shrinkWrap: true,
+                      physics:
+                      NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                      children: <Widget>[
+                        Container(
+                          color: Colors.white,
+                          child: Text('Date: ' + globals.currentTestResults[index].getTimestamp()),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  child: Text('View'),
+                                  onPressed: () {
+                                    globals.currentTestResult = globals.currentTestResults[index];
+                                    Navigator.of(context).pushReplacementNamed(UserViewSingleTestResult.routeName);
+                                  }),
+                            ],
                           ),
                         ),
-                        child: Text('Upload new PDF'),
-                        onPressed: (){
-                          Navigator.of(context).pushReplacementNamed(UserUploadTestResults.routeName);
-                        },
-                      )
-                  ),
-                ),
-              ]
-          )
+                      ])
+                ]),
+              );
+            });
+      }
+    }
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title:
+          Text("View COVID-19 test results"),
+          leading: BackButton(
+            //Specify back button
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed(UserHealth.routeName);
+            },
+          ),
+        ),
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Center(
+                child: getList(),
+              ),
+            ),
+            Container (
+              alignment: Alignment.bottomRight,
+              child: Container (
+                  height: 50,
+                  width: 200,
+                  padding: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom (
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text('Upload new PDF'),
+                    onPressed: (){
+                      Navigator.of(context).pushReplacementNamed(UserUploadTestResults.routeName);
+                    },
+                  )
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
