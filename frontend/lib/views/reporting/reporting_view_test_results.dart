26ee1903a8142cd34ba2c87179e1ebe408ebb34b
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-26 02:08:19 +0200

    reporting_view_test_results.dart

---
+++ frontend/lib/views/reporting/reporting_view_test_results.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+import 'package:frontend/views/admin_homepage.dart';
+///import 'package:frontend/views/reporting/reporting_floor_plans.dart';
+import 'package:frontend/views/user_homepage.dart';
+import 'package:frontend/views/login_screen.dart';
+
+///import 'package:frontend/controllers/floor_plan/floor_plan_helpers.dart' as floorPlanHelpers;
+import 'package:frontend/globals.dart' as globals;
+
+class ViewTestResults extends StatefulWidget {
+  static const routeName = "/reporting";
+
+  @override
+  _ViewTestResultsState createState() => _ViewTestResultsState();
+}
+//class admin
+class _ViewTestResultsState extends State<ViewTestResults> {
+  int numberOfEmployees = 1;
+
+  Future<bool> _onWillPop() async {
+    Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+    return (await true);
+  }
+  @override
+  Widget build(BuildContext context) {
+    if (globals.loggedInUserType != 'ADMIN') {
+      if (globals.loggedInUserType == 'USER') {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(UserHomePage.routeName);
+        });
+      } else {
+        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        });
+      }
+      return Container();
+    }
+  }
+}
