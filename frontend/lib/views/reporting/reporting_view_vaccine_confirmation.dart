b910cfda8f93429ee4441cc9c474511da9f747ae
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-26 11:24:22 +0200

    Update reporting_view_vaccine_confirmation.dart
    Fixed up this page so that it doesn't conflict with other pages in the app.

--- frontend/lib/views/reporting/reporting_view_vaccine_confirmation.dart
+++ frontend/lib/views/reporting/reporting_view_vaccine_confirmation.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
-import 'package:frontend/views/admin_homepage.dart';
-///import 'package:frontend/views/reporting/reporting_floor_plans.dart';
 import 'package:frontend/views/user_homepage.dart';
 import 'package:frontend/views/login_screen.dart';
 import 'package:frontend/views/reporting/reporting_view_recovered_employees.dart';
-///import 'package:frontend/controllers/floor_plan/floor_plan_helpers.dart' as floorPlanHelpers;
+
 import 'package:frontend/globals.dart' as globals;
 
-class ViewVaccineConfirmation extends StatefulWidget {
+class ReportingViewVaccineConfirmation extends StatefulWidget {
   static const routeName = "/reporting_view_vaccine_confirmation";
 
   @override
-  _ViewVaccineConfirmationState createState() => _ViewVaccineConfirmationState();
+  _ReportingViewVaccineConfirmationState createState() => _ReportingViewVaccineConfirmationState();
 }
-//class admin
-class _ViewVaccineConfirmationState extends State<ViewVaccineConfirmation> {
+
+class _ReportingViewVaccineConfirmationState extends State<ReportingViewVaccineConfirmation> {
   int numberOfEmployees = 1;
 
   Future<bool> _onWillPop() async {
-    Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+    Navigator.of(context).pushReplacementNamed(ReportingViewRecoveredEmployees.routeName);
     return (await true);
   }
   @override
@@ -133,7 +131,7 @@ class _ViewVaccineConfirmationState extends State<ViewVaccineConfirmation> {
           leading: BackButton(
             //Specify back button
             onPressed: () {
-              Navigator.of(context).pushReplacementNamed(ReportingHealthViewEmployees.routeName);
+              Navigator.of(context).pushReplacementNamed(ReportingViewRecoveredEmployees.routeName);
             },
           ),
         ),
