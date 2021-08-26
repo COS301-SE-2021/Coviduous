d3086f5de4ef1e9eca461f185f636e28a269e222
clemy402 <70370515+clemy402@users.noreply.github.com>
2021-08-26 02:20:56 +0200

    reporting_view_vaccine_confirmation.dart

--- frontend/lib/views/reporting/reporting_view_vaccine_confirmation.dart
+++ frontend/lib/views/reporting/reporting_view_vaccine_confirmation.dart
@@ -37,5 +37,116 @@ class _ViewVaccineConfirmationState extends State<ViewVaccineConfirmation> {
       }
       return Container();
     }
+    Widget getList() {
+      int numberOfResults = 1;
+
+      print(numberOfResults);
+
+      if (numberOfResults == 0) {
+        return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height /
+                (5 * globals.getWidgetScaling()),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width /
+                (2 * globals.getWidgetScaling()),
+            height: MediaQuery.of(context).size.height /
+                (24 * globals.getWidgetScaling()),
+            color: Theme.of(context).primaryColor,
+            child: Text('No vaccine confirmations found',
+                style: TextStyle(color: Colors.white,
+                    fontSize:
+                    (MediaQuery.of(context).size.height * 0.01) * 2.5)),
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width /
+                  (2 * globals.getWidgetScaling()),
+              height: MediaQuery.of(context).size.height /
+                  (12 * globals.getWidgetScaling()),
+              color: Colors.white,
+              padding: EdgeInsets.all(12),
+              child: Text('No vaccine confirmations have been uploaded by this user.',
+                  style: TextStyle(
+                      fontSize:
+                      (MediaQuery.of(context).size.height * 0.01) * 2.5)))
+        ]);
+      } else {
+        //Else create and return a list
+        return ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: EdgeInsets.all(16),
+            itemCount: numberOfResults,
+            itemBuilder: (context, index) {
+              //Display a list tile FOR EACH room in rooms[]
+              return ListTile(
+                title: Column(children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    color: Theme.of(context).primaryColor,
+                    child: Text('Employee ID '),
+                  ),
+                  ListView(
+                      shrinkWrap: true,
+                      physics:
+                      NeverScrollableScrollPhysics(), //The lists within the list should not be scrollable
+                      children: <Widget>[
+                        Container(
+                          color: Colors.white,
+                          child: Text('Vaccine Confirmation: 1 '),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.white,
+                          child: Text('DATE: '),
+                        ),
+                        Container(
+                          height: 50,
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  child: Text('View'),
+                                  onPressed: () {
+                                    ///will show pdf of vaccine confirmations.
+                                  }),
+                            ],
+                          ),
+                        ),
+                      ])
+                ]),
+              );
+            });
+      }
+    }
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title:
+          Text("View vaccine confirmation"),
+          leading: BackButton(
+            //Specify back button
+            onPressed: () {
+              ///Navigator.of(context).pushReplacementNamed(ReportingFloors.routeName);
+            },
+          ),
+        ),
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Center(
+                child: getList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
