7164d165bf3c1a0260b50d0b67a728532d5652b5
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-08-28 12:05:17 +0200

    Update reporting_controller.dart
    Added functions to get the booking and shift summaries of a company.

--- frontend/lib/controllers/reporting/reporting_controller.dart
+++ frontend/lib/controllers/reporting/reporting_controller.dart
@@ -5,7 +5,9 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:frontend/models/reporting/booking_summary.dart';
 import 'package:frontend/models/reporting/health_summary.dart';
+import 'package:frontend/models/reporting/shift_summary.dart';
 import 'package:frontend/models/user/recovered_user.dart';
 import 'package:frontend/models/user/sick_user.dart';
 import 'package:frontend/controllers/server_info.dart' as serverInfo;
@@ -17,12 +19,59 @@ int numSickEmployees = 0;
 List<RecoveredUser> recoveredEmployeesDatabaseTable = [];
 int numRecoveredEmployees = 0;
 
+List<BookingSummary> bookingSummaryDatabaseTable = [];
+int numBookingSummaries;
+
 List<HealthSummary> healthSummaryDatabaseTable = [];
 int numHealthSummaries = 0;
+
+List<ShiftSummary> shiftSummaryDatabaseTable = [];
+int numShiftSummaries = 0;
 
 String server = serverInfo.getServer(); //server needs to be running on Firebase
 
 //Company reporting
+
+//Get bookings overview
+Future<List<BookingSummary>> getBookingSummary(String companyId, String year, String month) async {
+  String path = "/reporting/booking-summary";
+  String url = server + path;
+
+  var request;
+
+  try {
+    request = http.Request("POST", Uri.parse(url));
+    request.body = json.encode({
+      "companyId": companyId,
+      "year": year,
+      "month": month,
+    });
+    request.headers.addAll(globals.requestHeaders);
+
+    var response = await request.send();
+    print(await response.statusCode);
+
+    if (response.statusCode == 200) {
+      var jsonString = (await response.stream.bytesToString());
+      var jsonMap = jsonDecode(jsonString);
+
+      bookingSummaryDatabaseTable.clear();
+      numBookingSummaries = 0;
+
+      for (var data in jsonMap["data"]) {
+        var summaryData = BookingSummary.fromJson(data);
+        bookingSummaryDatabaseTable.add(summaryData);
+        numBookingSummaries++;
+      }
+
+      return bookingSummaryDatabaseTable;
+    }
+  } catch (error) {
+    print(error);
+  }
+
+  return null;
+}
 
 //Get health overview
 Future<List<HealthSummary>> getHealthSummary(String companyId, String year, String month) async {
@@ -57,6 +106,47 @@ Future<List<HealthSummary>> getHealthSummary(String companyId, String year, Stri
       }
 
       return healthSummaryDatabaseTable;
+    }
+  } catch (error) {
+    print(error);
+  }
+
+  return null;
+}
+
+//Get shift overview
+Future<List<ShiftSummary>> getShiftSummary(String companyId, String year, String month) async {
+  String path = "/reporting/shift-summary";
+  String url = server + path;
+
+  var request;
+
+  try {
+    request = http.Request("POST", Uri.parse(url));
+    request.body = json.encode({
+      "companyId": companyId,
+      "year": year,
+      "month": month,
+    });
+    request.headers.addAll(globals.requestHeaders);
+
+    var response = await request.send();
+    print(await response.statusCode);
+
+    if (response.statusCode == 200) {
+      var jsonString = (await response.stream.bytesToString());
+      var jsonMap = jsonDecode(jsonString);
+
+      shiftSummaryDatabaseTable.clear();
+      numShiftSummaries = 0;
+
+      for (var data in jsonMap["data"]) {
+        var summaryData = ShiftSummary.fromJson(data);
+        shiftSummaryDatabaseTable.add(summaryData);
+        numShiftSummaries++;
+      }
+
+      return shiftSummaryDatabaseTable;
     }
   } catch (error) {
     print(error);
