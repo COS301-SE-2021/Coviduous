ca2be7e79aa28e35033cd84f8127258966d4d531
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-02 22:22:10 +0200

    Update admin_add_floor_plan.dart
    Made the container have rounded edges.

--- frontend/lib/views/floor_plan/admin_add_floor_plan.dart
+++ frontend/lib/views/floor_plan/admin_add_floor_plan.dart
@@ -122,122 +122,124 @@ class _AddFloorPlanState extends State<AddFloorPlan> {
         body: Center(
           child: SingleChildScrollView(
             //So the element doesn't overflow when you open the keyboard
-            child: Container(
-              width: MediaQuery.of(context).size.width /
-                  (2 * globals.getWidgetScaling()),
-              color: Colors.white,
-              margin: EdgeInsets.all(16),
-              child: Form(
-                key: _formKey,
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.all(20.0),
-                        child: (globals.floorPlanImageExists) ? Image(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                width: MediaQuery.of(context).size.width / (2 * globals.getWidgetScaling()),
+                color: Colors.white,
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
                           alignment: Alignment.center,
-                          image: MemoryImage(fileBytes),
-                          width: double.maxFinite,
-                          height: MediaQuery.of(context).size.height/6,
-                        ) : Image(
-                          alignment: Alignment.center,
-                          image: AssetImage('assets/images/placeholder-office-building.png'),
-                          width: double.maxFinite,
-                          height: MediaQuery.of(context).size.height/6,
-                        ),
-                      ),
-                      Text('Selected file: ' + pickerFileName),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height/48,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                      ElevatedButton(
-                        child: Text('Select an image'),
-                        onPressed: () {
-                          getImage();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                          margin: EdgeInsets.all(20.0),
+                          child: (globals.floorPlanImageExists) ? Image(
+                            alignment: Alignment.center,
+                            image: MemoryImage(fileBytes),
+                            width: double.maxFinite,
+                            height: MediaQuery.of(context).size.height/6,
+                          ) : Image(
+                            alignment: Alignment.center,
+                            image: AssetImage('assets/images/placeholder-office-building.png'),
+                            width: double.maxFinite,
+                            height: MediaQuery.of(context).size.height/6,
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height/48,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                      Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                          child: TextFormField(
-                            textInputAction: TextInputAction.done, //The "return" button becomes a "done" button when typing
-                            decoration: InputDecoration(labelText: 'Enter number of floors'),
-                            keyboardType: TextInputType.number,
-                            validator: (String value) {
-                              int num = int.tryParse(value);
-                              if (num == null || num <= 0) {
-                                return 'Number of floors must be greater than zero';
-                              }
-                              return null;
-                            },
-                            onSaved: (String value) {
-                              _numFloor = value;
-                            },
-                          )
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height/48,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                      ElevatedButton(
+                        Text('Selected file: ' + pickerFileName),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height/48,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        ElevatedButton(
+                          child: Text('Select an image'),
+                          onPressed: () {
+                            getImage();
+                          },
                           style: ElevatedButton.styleFrom(
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
                           ),
-                          child: Text('Proceed'),
-                          onPressed: () {
-                            if (!_formKey.currentState.validate()) {
-                              return;
-                            }
-                            _formKey.currentState.save();
-
-                            floorPlanHelpers.createFloorPlan(num.parse(_numFloor)).then((result) {
-                              if (result == true) {
-                                saveImage(fileBytes, fileName);
-                                floorPlanHelpers.getFloorPlans().then((result) {
-                                  if (result == true) {
-                                    Navigator.of(context).pushReplacementNamed(AdminModifyFloorPlans.routeName);
-                                  } else {
-                                    Navigator.of(context).pushReplacementNamed(FloorPlanScreen.routeName);
-                                  }
-                                });
-                              } else {
-                                showDialog(
-                                    context: context,
-                                    builder: (ctx) => AlertDialog(
-                                      title: Text('Creation unsuccessful'),
-                                      content: Text('Floor plans must have at least one floor.'),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          child: Text('Okay'),
-                                          onPressed: () {
-                                            Navigator.of(ctx).pop();
-                                          },
-                                        )
-                                      ],
-                                    )
-                                );
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height/48,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        Padding(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: TextFormField(
+                              textInputAction: TextInputAction.done, //The "return" button becomes a "done" button when typing
+                              decoration: InputDecoration(labelText: 'Enter number of floors'),
+                              keyboardType: TextInputType.number,
+                              validator: (String value) {
+                                int num = int.tryParse(value);
+                                if (num == null || num <= 0) {
+                                  return 'Number of floors must be greater than zero';
+                                }
+                                return null;
+                              },
+                              onSaved: (String value) {
+                                _numFloor = value;
+                              },
+                            )
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height/48,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text('Proceed'),
+                            onPressed: () {
+                              if (!_formKey.currentState.validate()) {
+                                return;
                               }
-                            });
-                          }),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 48,
-                        width: MediaQuery.of(context).size.width,
-                      ),
-                    ],
+                              _formKey.currentState.save();
+
+                              floorPlanHelpers.createFloorPlan(num.parse(_numFloor)).then((result) {
+                                if (result == true) {
+                                  saveImage(fileBytes, fileName);
+                                  floorPlanHelpers.getFloorPlans().then((result) {
+                                    if (result == true) {
+                                      Navigator.of(context).pushReplacementNamed(AdminModifyFloorPlans.routeName);
+                                    } else {
+                                      Navigator.of(context).pushReplacementNamed(FloorPlanScreen.routeName);
+                                    }
+                                  });
+                                } else {
+                                  showDialog(
+                                      context: context,
+                                      builder: (ctx) => AlertDialog(
+                                        title: Text('Creation unsuccessful'),
+                                        content: Text('Floor plans must have at least one floor.'),
+                                        actions: <Widget>[
+                                          TextButton(
+                                            child: Text('Okay'),
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                          )
+                                        ],
+                                      )
+                                  );
+                                }
+                              });
+                            }),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 48,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
