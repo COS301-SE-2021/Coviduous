8f28315ba794e06dac9c1259f38d0a7bd6943242
Peter Okumbe <u18052640@tuks.co.za>
2021-09-02 18:39:33 +0200

    Update reporting.unit.test.js

---
+++ backend/functions/tests/reporting/reporting.unit.test.js
@@ -0,0 +1,156 @@
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var expect = chai.expect;
+var should = chai.should();
+const uuid = require("uuid");
+
+let server = 'http://localhost:5002/coviduous-api/us-central1/app/';
+
+chai.use(chaiHttp);
+
+// describe('Create user unit tests', function() {
+//     it('Return 400 if request is empty', function(done) {
+//         chai.request(server)
+//             .post('/api/users')
+//             .send(null)
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(400);
+//                 //console.log(res.body);
+//                 done();
+//             });
+//     });
+ 
+//     it('Return 400 if empty userId', function(done) {
+//         let req = {
+//             userId: "",
+//             type: "USER",
+//             firstName: "test-000",
+//             lastName: "test-000",
+//             email: "test-000",
+//             userName: "test-000",
+//             companyId: "test-000",
+//             companyName: "test-000",
+//             companyAddress: "test-000"
+//         };
+
+//         chai.request(server)
+//             .post('/api/users')
+//             .send(req)
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(400);
+//                 console.log(res.body);
+//                 done();
+//             });
+//     });
+ 
+//     it('Return 400 if empty type', function(done) {
+//         let req = {
+//             userId: "test-000",
+//             type: "",
+//             firstName: "test-000",
+//             lastName: "test-000",
+//             email: "test-000",
+//             userName: "test-000",
+//             companyId: "test-000",
+//             companyName: "test-000",
+//             companyAddress: "test-000"
+//         };
+
+//         chai.request(server)
+//             .post('/api/users')
+//             .send(req)
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(400);
+//                 console.log(res.body);
+//                 done();
+//             });
+//     });
+
+//     it('Return 400 if incorrect type', function(done) {
+//         let req = {
+//             userId: "test-000",
+//             type: "INCORRECT TYPE",
+//             firstName: "test-000",
+//             lastName: "test-000",
+//             email: "test-000",
+//             userName: "test-000",
+//             companyId: "test-000",
+//             companyName: "test-000",
+//             companyAddress: "test-000"
+//         };
+
+//       chai.request(server)
+//           .post('/api/users')
+//           .send(req)
+//           .end((err, res) => {
+//               should.exist(res);
+//               res.should.have.status(400);
+//               console.log(res.body);
+//               done();
+//           });
+//     });
+ 
+//     it('Return 400 if empty company name for admin user', function(done) {
+//         let req = {
+//             userId: "test-000",
+//             type: "ADMIN",
+//             firstName: "test-000",
+//             lastName: "test-000",
+//             email: "test-000",
+//             userName: "test-000",
+//             companyId: "test-000",
+//             companyName: "",
+//             companyAddress: "test-000"
+//         };
+
+//         chai.request(server)
+//             .post('/api/users')
+//             .send(req)
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(400);
+//                 console.log(res.body);
+//                 done();
+//             });
+//     });
+ 
+//     it('Return 400 if empty company address for admin user', function(done) {
+//         let req = {
+//             userId: "test-000",
+//             type: "ADMIN",
+//             firstName: "test-000",
+//             lastName: "test-000",
+//             email: "test-000",
+//             userName: "test-000",
+//             companyId: "test-000",
+//             companyName: "test-000",
+//             companyAddress: ""
+//         };
+
+//         chai.request(server)
+//             .post('/api/users')
+//             .send(req)
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(400);
+//                 console.log(res.body);
+//                 done();
+//             });
+//     });
+//  });
+
+//  describe('Get users unit tests', function() {
+//     it('Return 200 if retrieval is successful', function() {
+//         chai.request(server)
+//             .get('/api/users')
+//             .end((err, res) => {
+//                 should.exist(res);
+//                 res.should.have.status(200);
+//                 //console.log(res.body);
+//                 //done();
+//             });
+//     });
+// });
