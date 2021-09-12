714a2d77446a9bf730eda47262a9dc96f617e15a
Peter Okumbe <u18052640@tuks.co.za>
2021-09-12 13:39:34 +0200

    Create user.controller.js

---
+++ backend/deployable_functions/functions/controllers/user.controller.js
@@ -0,0 +1,42 @@
+let functions = require("firebase-functions");
+let admin = require('firebase-admin');
+let express = require('express');
+let cors = require('cors');
+let userApp = express();
+//var serviceAccount = require("./permissions.json");
+const authMiddleware = require('../authMiddleWare.js');
+
+userApp.use(cors({ origin: true }));
+userApp.use(express.urlencoded({ extended: true }));
+userApp.use(express.json());
+//admin.initializeApp(); 
+
+
+let database = admin.firestore();
+let uuid = require("uuid");
+
+ /**
+ * This function returns the current date in a specified format.
+ * @returns {string} The current date as a string in the format DD/MM/YYYY
+ */
+  Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() + "/" +
+        (((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+  }
+  
+  /**
+  * This function returns the current time in a specified format.
+  * @returns {string} The current time as a string in the format HH:MM:SS.
+  */
+  Date.prototype.timeNow = function () {
+    return ((this.getHours() < 10)?"0":"") + this.getHours() + ":" +
+        ((this.getMinutes() < 10)?"0":"") + this.getMinutes() + ":" +
+        ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+  }
+
+///////////////// functions /////////////////
+
+
+
+
+exports.user = functions.https.onRequest(userApp);
