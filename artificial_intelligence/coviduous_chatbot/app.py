81422951a55781b6e7b05933348c59e73eb3cb19
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-09-12 13:58:02 +0200

    updating chatbot

--- artificial_intelligence/coviduous_chatbot/app.py
+++ artificial_intelligence/coviduous_chatbot/app.py
@@ -1,6 +1,6 @@
 from coviduous_bot import Bot
 
-from flask import Flask, render_template, request
+from flask import Flask,jsonify,render_template, request
 chatbot= Bot()
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -14,6 +14,44 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.allow_question(userText))
 
-
+@app.route('/api/message', methods=['POST'])
+def prognosis():
+    data = request.get_json()
+    message = data.get('question', '')
+    tutorial = "tutorial"
+    shortcut="shortcut"
+    if message != None and tutorial in message:
+        reply = str(chatbot.allow_question(message))
+        responseData={
+        "recieved_message":message,
+        "is_tutorial":"true",
+        "is_shortcut":"false",
+        "is_normal_question":"false",
+        "answer":reply
+        }
+        return jsonify(responseData)
+    elif message != None and shortcut in message:
+        reply = str(chatbot.allow_question(message))
+        responseData={
+        "recieved_message":message,
+        "is_tutorial":"false",
+        "is_shortcut":"true",
+        "is_normal_question":"false",
+        "answer":reply
+        }
+        return jsonify(responseData)
+    else:
+        reply = str(chatbot.allow_question(message))
+        responseData={
+        "recieved_message":message,
+        "is_tutorial":"false",
+        "is_shortcut":"false",
+        "is_normal_question":"true",
+        "answer":reply
+        }
+        return jsonify(responseData)
+    
 if __name__ == "__main__":
     app.run()
+
+
