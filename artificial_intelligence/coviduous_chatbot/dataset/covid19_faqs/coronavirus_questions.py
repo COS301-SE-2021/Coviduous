a89947788554fff99f1d2fa7f0c4180b28f9b9a0
Peter Okumbe <u18052640@tuks.co.za>
2021-09-13 00:30:09 +0200

    Update coronavirus_questions.py

--- artificial_intelligence/coviduous_chatbot/dataset/covid19_faqs/coronavirus_questions.py
+++ artificial_intelligence/coviduous_chatbot/dataset/covid19_faqs/coronavirus_questions.py
@@ -1,4 +1,8 @@
 COVID19_QUES = [
+	{
+        "Question": "What is COVID-19?",
+        "Answer": "COVID-19 is a disease caused by a virus called SARS-CoV-2. Most people with COVID-19 have mild symptoms, but some people can become severely ill. Although most people with COVID-19 get better within weeks of illness, some people experience post-COVID conditions."
+    },
     {
         "Question": "What are the symptoms of COVID-19?",
         "Answer": "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment. Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention."
@@ -6,7 +10,43 @@ COVID19_QUES = [
     {
         "Question": "What is self-isolation?",
         "Answer": "Self-isolation is a way to keep yourself from possibly infecting others if you think you might be infected. It involves limiting contact with public places, relatives, friends, colleagues, and public transport."
-    }
+    },
+    {
+        "Question": "How does the virus spread?",
+        "Answer": "COVID-19 spreads when an infected person breathes out droplets and very small particles that contain the virus. These droplets and particles can be breathed in by other people or land on their eyes, noses, or mouth. In some circumstances, they may contaminate surfaces they touch. People who are closer than 6 feet from the infected person are most likely to get infected."
+    },
+    {
+        "Question": "Should I use soap and water or hand sanitizer to protect against COVID-19?",
+        "Answer": "Handwashing is one of the best ways to protect yourself and your family from getting sick. Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food. If soap and water are not readily available, use an alcohol-based hand sanitizer with at least 60% alcohol."
+    },
+    {
+        "Question": "What are the symptoms and complications that COVID-19 can cause?",
+        "Answer": "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually."
+    },
+    {
+        "Question": "What is contact tracing?",
+        "Answer": "Contact tracing has been used for decades by state and local health departments to slow or stop the spread of infectious diseases. Contact tracing slows the spread of COVID-19 by letting people know they may have been exposed to COVID-19 and should monitor their health for signs and symptoms of COVID-19."
+    },
+    {
+        "Question": "What will happen with my personal information during contact tracing?",
+        "Answer": "If you have been diagnosed with COVID-19, your name will not be shared with those you came in contact with. The health department will only notify people you were in close contact with that they might have been exposed to COVID-19."
+    },
+    {
+        "Question": "Who is considered a close contact of someone with COVID-19?",
+        "Answer": "For COVID-19, a close contact is anyone who was within 6 feet of an infected person for a total of 15 minutes or more over a 24-hour period (for example, three individual 5-minute exposures for a total of 15 minutes)."
+    },
+    {
+        "Question": "If I am a close contact, will I be tested for COVID-19?",
+        "Answer": "If you have been in close contact with someone who has COVID-19, you should be tested, even if you do not have symptoms of COVID-19. The health department may be able to provide resources for testing in your area."
+    },
+    {
+        "Question": "Can I go to work having been in close contact with someone with COVID-19?",
+        "Answer": "Anyone who is sick or displaying symptoms should not go to school or work. If you have been in close contact with a confirmed COVID-19 case, you should self-isolate for 14 days from the date of close contact. If you are concerned, contact your medical practitioner for further advice."
+    },
+    {
+        "Question": "What is the risk of my child becoming sick with COVID-19?",
+        "Answer": "Children can be infected with the virus that causes COVID-19 and can get sick with COVID-19. Most children with COVID-19 have mild symptoms or they may have no symptoms at all (“asymptomatic”). Fewer children have been sick with COVID-19 compared to adults."
+    },
 ]
 
 
