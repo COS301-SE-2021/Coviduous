bcddf111ea7491eddaa0282ac1c93f5d3caa4ff1
u18202374-Chaks <u18202374@tuks.co.za>
2021-09-13 23:47:55 +0200

    reporting u[pdated

--- backend/deployable_functions/functions/controllers/reporting.controller.js
+++ backend/deployable_functions/functions/controllers/reporting.controller.js
@@ -651,7 +651,445 @@ reportingApp.put('/api/reporting/company/company-data/floors/dec', async (req, r
         return res.status(500).send({message: "Some error occurred while updating number of floors."});
     }
 });
+reportingApp.put('/api/reporting/company/company-data/rooms/inc', async (req, res) =>  {    
+    // data validation
+    let fieldErrors = [];
 
+    //Look into express.js middleware so that these lines are not necessary
+    let reqJson;
+    try {
+        reqJson = JSON.parse(req.body);
+    } catch (e) {
+        reqJson = req.body;
+    }
+    console.log(reqJson);
+    //////////////////////////////////////////////////////////////////////
+       
+    if(req.body == null) {
+        fieldErrors.push({field: null, message: 'Request object may not be null'});
+    }
+
+    if (reqJson.companyId == null || reqJson.companyId === '') {
+        fieldErrors.push({field: 'companyId', message: 'Company ID may not be empty'});
+    }
+
+    if (fieldErrors.length > 0) {
+        console.log(fieldErrors);
+        return res.status(400).send({
+            message: '400 Bad Request: Incorrect fields',
+            errors: fieldErrors
+        });
+    }
+    let response = database.collection('company-data').doc(reqJson.companyId);
+    let doc = await response.get();
+    let companyData = doc.data();
+    let currentNumRoomsInCompanyData = companyData.numberOfRooms;
+
+
+    try {
+        if (parseInt(currentNumRoomsInCompanyData) >= 0)
+        {
+            let newNumRooms = parseInt(currentNumRoomsInCompanyData) + 1;
+            newNumRooms = newNumRooms.toString();
+
+            response = await database.collection('company-data').doc(reqJson.companyId).update({
+                "numberOfRooms": newNumRooms
+            });
+            return res.status(200).send({
+                message: "Successfully added number of rooms",
+                //data: req.body
+            });
+            
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: "Some error occurred while updating number of floors."});
+    }    
+});
+
+reportingApp.put('/api/reporting/company/company-data/rooms/dec', async (req, res) =>  {    
+    
+    // data validation
+    let fieldErrors = [];
+
+    //Look into express.js middleware so that these lines are not necessary
+    let reqJson;
+    try {
+        reqJson = JSON.parse(req.body);
+    } catch (e) {
+        reqJson = req.body;
+    }
+    console.log(reqJson);
+    //////////////////////////////////////////////////////////////////////
+       
+    if(req.body == null) {
+        fieldErrors.push({field: null, message: 'Request object may not be null'});
+    }
+
+    if (reqJson.companyId == null || reqJson.companyId === '') {
+        fieldErrors.push({field: 'companyId', message: 'Company ID may not be empty'});
+    }
+
+    if (fieldErrors.length > 0) {
+        console.log(fieldErrors);
+        return res.status(400).send({
+            message: '400 Bad Request: Incorrect fields',
+            errors: fieldErrors
+        });
+    }
+    
+    
+    let response = database.collection('company-data').doc(reqJson.companyId);
+    let doc = await response.get();
+    let companyData = doc.data();
+    let currentNumRoomsInCompanyData = companyData.numberOfRooms;
+
+
+    try {
+        if (parseInt(currentNumRoomsInCompanyData) >= 0)
+        {
+            let newNumRooms = parseInt(currentNumRoomsInCompanyData) - 1;
+            newNumRooms = newNumRooms.toString();
+
+            response = await database.collection('company-data').doc(reqJson.companyId).update({
+                "numberOfRooms": newNumRooms
+            });
+            return res.status(200).send({
+                message: "Successfully deleted number of rooms",
+                //data: req.body
+            });
+            
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: "Some error occurred while updating number of floors."});
+    }
+});
+reportingApp.post('/api/reporting/health-summary/setup', async (req, res) =>  {    
+    
+    let reqJson;
+          try {
+              reqJson = JSON.parse(req.body);
+          } catch (e) {
+              reqJson = req.body;
+          }
+        
+        // First we get all the health summaries in our database and check if there is an exisiting health summary with
+        // our companyId
+
+        if (reqJson.companyId == null || reqJson.companyId === '') {
+            fieldErrors.push({field: 'companyId', message: 'Company ID may not be empty'});
+        }
+        
+            const document = database.collection('health-summary');
+            const snapshot = await document.get();
+            
+            let list = [];
+            
+            snapshot.forEach(doc => {
+                let data = doc.data();
+                list.push(data);
+            });
+        
+        let healthSummaries =list;
+    
+        let filteredList=[];   
+        healthSummaries.forEach(obj => {
+        if(obj.companyId===reqJson.companyId)
+              {
+                filteredList.push(obj);
+              }
+              else
+              {
+        
+              }
+            });
+    
+        if(filteredList.length>0)
+        {
+            // company was previously initialized no need to re-initilize
+            return res.status(200).send({
+                message: 'Company Health Summary Already Has An Initial Instance',
+            });
+    
+        }
+        else
+        {
+            
+          //company was never registered before so we setup their health summary table
+          let healthSummaryId = "HSID-" + uuid.v4();
+          let timestamp = new Date().today() + " @ " + new Date().timeNow();
+          let month= timestamp.charAt(3)+timestamp.charAt(4);
+          let year= timestamp.charAt(6)+timestamp.charAt(7)+timestamp.charAt(8)+timestamp.charAt(9);
+    
+          let healthSummary = {
+            healthSummaryId: healthSummaryId,
+            month: month,
+            year:year,
+            timestamp: timestamp,
+            companyId: reqJson.companyId,
+            numHealthChecksUsers: 0,
+            numHealthChecksVisitors: 0,
+            numReportedInfections: 0,
+            numReportedRecoveries:0
+              
+            }
+            try{
+                await database.collection('health-summary').doc(healthSummaryId)
+                .create(healthSummary);
+                 return res.status(200).send({
+                message: 'Company Health Summary Successfuly Set',
+                data:healthSummary
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({message: "Some error occurred while setting up the heatlh check."});
+        }
+     }
+       });
+       reportingApp.post('/api/reporting/permission-summary/setup', async (req, res) =>  {    
+        let reqJson;
+          try {
+              reqJson = JSON.parse(req.body);
+          } catch (e) {
+              reqJson = req.body;
+          }
+        
+        // First we get all the permission summaries in our database and check if there is an exisiting permission summary with
+        // our companyId
+        const document = database.collection('permission-summary');
+        const snapshot = await document.get();
+        
+        let list = [];
+        
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            list.push(data);
+        });
+    
+        let permissionSummaries =list;
+        
+        let filteredList=[];   
+        permissionSummaries.forEach(obj => {
+        if(obj.companyId===reqJson.companyId)
+              {
+                filteredList.push(obj);
+              }
+              else
+              {
+        
+              }
+            });
+    
+        if(filteredList.length>0)
+        {
+            // company was previously initialized no need to re-initilize
+            return res.status(200).send({
+                message: 'Company Permission Summary Already Has An Initial Instance',
+            });
+    
+        }
+        else
+        {
+            
+          //company was never registered before so we setup their permission summary table
+          let permissionSummaryId = "PMSN-" + uuid.v4();
+          let timestamp = new Date().today() + " @ " + new Date().timeNow();
+          let month= timestamp.charAt(3)+timestamp.charAt(4);
+          let year= timestamp.charAt(6)+timestamp.charAt(7)+timestamp.charAt(8)+timestamp.charAt(9);
+    
+          let permissionSummary = {
+            permissionSummaryId: permissionSummaryId,
+            month: month,
+            year:year,
+            timestamp: timestamp,
+            companyId: reqJson.companyId,
+            numPermissionDeniedUsers: 0,
+            numPermissionDeniedVisitors: 0,
+            numPermissionGrantedUsers: 0,
+            numPermissionGrantedVisitors: 0,
+            totalPermissions:0
+              
+            }
+            try{
+            await database.collection('permission-summary').doc(permissionSummaryId)
+              .create(permissionSummary);
+            
+            return res.status(200).send({
+                message: 'Company Permission Summary Successfuly Set',
+                data:permissionSummary
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({message: "Some error occurred while setting up the heatlh check."});
+        }
+        
+        }
+    });
+
+    reportingApp.post('/api/reporting/summary-shifts', async (req, res) =>  {    
+        let reqJson;
+        try {
+            reqJson = JSON.parse(req.body);
+        } catch (e) {
+            reqJson = req.body;
+        }
+            const document = database.collection('summary-shifts').where("companyId", "==", companyId);
+            const snapshot = await document.get();
+      
+            let list = [];
+            snapshot.forEach(doc => {
+                let data = doc.data();
+                list.push(data);
+            });
+            
+    
+        let getNumberShift = list;
+          
+        if (getNumberShift != null) {
+          return res.status(200).send({
+            message: 'Successfully retrieved number of shifts',
+            data: getNumberShift
+          });
+        } else {
+          return res.status(500).send({message: "Some error occurred while fetching number of shifts."});
+        }
+    });
+    reportingApp.post('/api/reporting/summary-bookings', async (req, res) =>  {    
+
+        let reqJson;
+        try {
+            reqJson = JSON.parse(req.body);
+        } catch (e) {
+            reqJson = req.body;
+        }
+            const document = database.collection('summary-bookings').where("companyId", "==", companyId);
+            const snapshot = await document.get();
+      
+            let list = [];
+            snapshot.forEach(doc => {
+                let data = doc.data();
+                list.push(data);
+            });
+               
+        let getNumberBooking = list;
+          
+        if (getNumberBooking != null) {
+          return res.status(200).send({
+            message: 'Successfully retrieved number of bookings',
+            data: getNumberBooking
+          });
+        } else {
+          return res.status(500).send({message: "Some error occurred while fetching number of bookings."});
+        }
+    });
+    reportingApp.post('/api/reporting/summary-summary', async (req, res) =>  {    
+        let reqJson;
+          try {
+              reqJson = JSON.parse(req.body);
+          } catch (e) {
+              reqJson = req.body;
+          }
+    
+          const document = database.collection('health-summary');
+          const snapshot = await document.get();
+          
+          let list = [];
+          
+          snapshot.forEach(doc => {
+              let data = doc.data();
+              list.push(data);
+          });
+      
+    
+        let healthSummaries = list;
+        
+        let filteredList=[];   
+        healthSummaries.forEach(obj => {
+        if(obj.companyId===reqJson.companyId && reqJson.month===obj.month && reqJson.year==obj.year)
+              {
+                filteredList.push(obj);
+              }
+              else
+              {
+        
+              }
+            });
+        
+            if(filteredList.length>0)
+            {
+                return res.status(200).send({
+                    message: 'Successfully retrieved health summary',
+                    data: filteredList
+                });
+           
+             }
+            else{
+                return res.status(500).send({
+                    message: 'Problem with either the companId,month or year you requesting for',
+                });
+            }
+    
+    });
+    reportingApp.post('/api/reporting/permission-summary', async (req, res) =>  {    
+
+        let reqJson;
+          try {
+              reqJson = JSON.parse(req.body);
+          } catch (e) {
+              reqJson = req.body;
+          }
+          
+            const document = database.collection('permission-summary');
+            const snapshot = await document.get();
+            
+            let list = [];
+            
+            snapshot.forEach(doc => {
+                let data = doc.data();
+                list.push(data);
+            });
+    
+        let permissionSummaries = list;
+        
+        let filteredList=[];   
+        permissionSummaries.forEach(obj => {
+        if(obj.companyId===reqJson.companyId && reqJson.month===obj.month && reqJson.year==obj.year)
+              {
+                filteredList.push(obj);
+              }
+              else
+              {
+        
+              }
+            });
+        
+            if(filteredList.length>0)
+            {
+                return res.status(200).send({
+                    message: 'Successfully retrieved permission summary',
+                    data: filteredList
+                });
+           
+             }
+            else{
+                return res.status(500).send({
+                    message: 'Problem with either the companId,month or year you requesting for',
+                });
+            }
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+
+            
+
+    
      
 
 
