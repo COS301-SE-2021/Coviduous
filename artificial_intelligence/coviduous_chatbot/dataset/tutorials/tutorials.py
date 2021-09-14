03220e8d76dd37e049c3cf77f0a250af7695d7b2
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-09-14 14:21:10 +0200

    setting up tutorial ques

---
+++ artificial_intelligence/coviduous_chatbot/dataset/tutorials/tutorials.py
@@ -0,0 +1,92 @@
+TUTORIALS = [
+    {
+        "Question": "tutorial complete health check, tutorial on how to complete a health check, how to complete a health check",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "tutorial on how to book an office space,tutorial book office space",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "tutorial How to view bookings, How to view my bookings, tutorial view bookings",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to view permissions, tutorial view permissions, how to request access, tutorial request access",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to upload test results and vaccine confirmation, how to upload test results, how to upload vaccine confirmation, tutorial upload test results, tutorial upload vaccine confirmation",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to report your infection, How to report an infection , tutorial report infection",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to reset your password, tutorial reset password , how to reset my password",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to delete your account, tutorial delete account , how to delete my account",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to create a floor plan, tutorial create floor plan",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to delete your account, tutorial delete account , how to delete my account",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to modify a floor plan, tutorial modify floor plan ",
+        "Answer": "base64String"
+    },
+     {
+        "Question": "How to create a shift, tutorial create shift",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to modify a floor plan, tutorial modify floor plan ",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to create a shift, tutorial create shift",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to modify a shift, tutorial modify shift",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to grant permissions,tutorial grant permission",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to modify a shift, tutorial modify shift",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to contact trace an infected employee, tutorial contact trace",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to generate a company overview, tutorial company overview",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to generate an office report, How to generate a health report,How to generate a health report, tutorial reports",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to create an announcement, tutorial create announcement",
+        "Answer": "base64String"
+    },
+    {
+        "Question": "How to create a notification, tutorial create notification",
+        "Answer": "base64String"
+    }
+]
+
+
