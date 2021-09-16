25a5e836aa250d6f7c767d961d578f102ad28b60
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-16 10:14:45 +0200

    Update admin_view_notifications.dart
    Fixed overflow error.

--- frontend/lib/views/notification/admin_view_notifications.dart
+++ frontend/lib/views/notification/admin_view_notifications.dart
@@ -70,7 +70,7 @@ class _AdminViewNotificationsState extends State<AdminViewNotifications> {
                   child: Container(
                     color: Colors.white,
                     height: (!globals.getIfOnPC())
-                        ? MediaQuery.of(context).size.height/5.5
+                        ? MediaQuery.of(context).size.height/5
                         : MediaQuery.of(context).size.height/7,
                     margin: EdgeInsets.all(5),
                     child: Row(
