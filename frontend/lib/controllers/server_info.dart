80fb99a5e305fcdbf8f17b94168cbf080ba5a99b
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-16 20:56:58 +0200

    Update server_info.dart
    Temporary change until the chatbot is deployed.

--- frontend/lib/controllers/server_info.dart
+++ frontend/lib/controllers/server_info.dart
@@ -27,7 +27,11 @@ String getAIserver() {
 
 String getChatbotServer() {
   if (kReleaseMode) { //If release version of the app
-    return '';
+    if (globals.getIfOnPC()) { //If on PC
+      return 'http://localhost:5000/api/message';
+    } else { //Else, on mobile
+      return 'http://10.0.2.2:5000/api/message';
+    }
   } else { //Else, testing version of the app
     if (globals.getIfOnPC()) { //If on PC
       return 'http://localhost:5000/api/message';
