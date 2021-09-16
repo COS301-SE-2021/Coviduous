a4d7397cfedbee5610c2e9d76f2069e7fc31a814
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-09-16 09:07:14 +0200

    shortcut setup

---
+++ artificial_intelligence/coviduous_chatbot/dataset/shortcuts/shortcuts.py
@@ -0,0 +1,100 @@
+#Greetings first
+SHORTCUTS = [
+    {
+        "Question": "shortcut-view-announcements-admin",
+        "Answer": "/admin_announcements"
+    },
+    {
+        "Question": "shortcut-view-announcements-user",
+        "Answer": "/user_announcements"
+    },
+    {
+        "Question": "shortcut-create-announcements-admin",
+        "Answer": "/admin_make_announcement"
+    },
+    {
+        "Question": "shortcut-add-floor-plan",
+        "Answer": "/admin_add_floor_plan"
+    },
+    {
+        "Question": "shortcut-modify-floor-plans",
+        "Answer": "/admin_modify_floor_plans"
+    },
+    {
+        "Question": "shortcut-contact-trace",
+        "Answer": "/admin_contact_trace_shifts"
+    },
+    {
+        "Question": "shortcut-health-home-user",
+        "Answer": "/user_health"
+    },
+    {
+        "Question": "shortcut-health-check-user",
+        "Answer": "/user_health_check"
+    },
+    {
+        "Question": "shortcut-report-infection-user",
+        "Answer": "/user_report_infection"
+    },
+    {
+        "Question": "shortcut-view-permission-user",
+        "Answer": "/user_view_permissions"
+    },
+    {
+        "Question": "shortcut-upload-test-result-user",
+        "Answer": "/user_upload_test_result"
+    },
+    {
+        "Question": "shortcut-upload-vaccine-confirmation-user",
+        "Answer": "/user_upload_vaccine_confirm"
+    },
+    {
+        "Question": "shortcut-health-check-visitor",
+        "Answer": "/visitor_health_check"
+    },
+    {
+        "Question": "shortcut-view-notifications-user",
+        "Answer": "/user_view_notifications"
+    },
+    {
+        "Question": "shortcut-view-notifications-admin",
+        "Answer": "/admin_view_notifications"
+    },
+    {
+        "Question": "shortcut-create-notification-admin",
+        "Answer": "/admin_make_notification"
+    },
+    {
+        "Question": "shortcut-create-booking-user",
+        "Answer": "/user_office_floor_plans"
+    },
+    {
+        "Question": "shortcut-reports-admin",
+        "Answer": "/reporting"
+    },
+    {
+        "Question": "shortcut-create-shift-admin",
+        "Answer": "/admin_add_shift_floor_plans"
+    },
+    {
+        "Question": "shortcut-create-booking-user",
+        "Answer": "/user_office_floor_plans"
+    },
+    {
+        "Question": "shortcut-view-shifts-admin",
+        "Answer": "/admin_view_shifts_floor_plans"
+    },
+    {
+        "Question": "shortcut-admin",
+        "Answer": "View announcements:shortcut-view-announcements-admin,Make announcement:shortcut-create-announcements-admin,Add floor plan:shortcut-add-floor-plan,Modify floor plans:shortcut-modify-floor-plans,Admin contact trace:shortcut-contact-trace,Reporting homepage:shortcut-reports-admin,Create shift:shortcut-create-shift-admin,View shifts:shortcut-view-shifts-admin"
+    },
+    {
+        "Question": "shortcut-visitor",
+        "Answer": "health-check:shortcut-health-check-visitor"
+    },
+    {
+        "Question": "shortcut-user",
+        "Answer": ""
+    }
+]
+
