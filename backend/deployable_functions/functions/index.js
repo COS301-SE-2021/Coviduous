964d1eba82bad80e85bedbfb4c80337b45e1f728
njabuloskosana <49289663+njabuloskosana@users.noreply.github.com>
2021-09-16 14:26:15 +0200

    setting up deployment of backend functions

--- backend/deployable_functions/functions/index.js
+++ backend/deployable_functions/functions/index.js
@@ -2,15 +2,13 @@ let admin = require('firebase-admin');
 admin.initializeApp(); 
 
 module.exports = {
-    ...require("./swagger.js"),
+    //...require("./swagger.js"),
+    //...require('./controllers/health.controller.js'),
     ...require("./controllers/office.controller.js"),
     ...require("./controllers/user.controller.js"),
     ...require("./controllers/shift.controller.js"),
-    ...require('./controllers/announcement.controller.js'),
-    ...require('./controllers/floorplan.controller.js'),
     ...require('./controllers/notification.controller.js'),
     ...require('./controllers/reporting.controller.js'),
-    ...require('./controllers/health.controller.js')
+    ...require('./controllers/announcement.controller.js'),
+    ...require('./controllers/floorplan.controller.js')
 }
-
-
