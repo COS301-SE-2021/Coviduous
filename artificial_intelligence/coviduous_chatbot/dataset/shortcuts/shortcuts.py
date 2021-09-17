fc34600d235e27966e3b86913eaddc8edec3f5bb
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-17 18:30:09 +0200

    Fixed the chatbot's answers a bit more

--- artificial_intelligence/coviduous_chatbot/dataset/shortcuts/shortcuts.py
+++ artificial_intelligence/coviduous_chatbot/dataset/shortcuts/shortcuts.py
@@ -85,15 +85,15 @@ SHORTCUTS = [
     },
     {
         "Question": "shortcut-admin",
-        "Answer": "View announcements: shortcut-view-announcements-admin\nMake announcement: shortcut-create-announcements-admin\n\nAdd floor plan: shortcut-add-floor-plan\nModify floor plans: shortcut-modify-floor-plans\n\nAdmin contact trace: shortcut-contact-trace\n\nReporting homepage: shortcut-reports-admin\n\nCreate shift: shortcut-create-shift-admin\nView shifts: shortcut-view-shifts-admin"
+        "Answer": "Type any of these shortcuts you would like to access.\n\nAnnouncements:\n* View announcements: shortcut-view-announcements-admin\n* Make announcement: shortcut-create-announcements-admin\n\nFloor plans:\n* Add floor plan: shortcut-add-floor-plan\n* Modify floor plans: shortcut-modify-floor-plans\n\nPermissions:\n* Contact trace an employee: shortcut-contact-trace\n\nReporting:\n* Reporting homepage: shortcut-reports-admin\n\nShifts:\n* Create shift: shortcut-create-shift-admin\n* View shifts: shortcut-view-shifts-admin"
     },
     {
         "Question": "shortcut-visitor",
-        "Answer": "Complete a health-check: shortcut-health-check-visitor"
+        "Answer": "Type any of these shortcuts you would like to access.\n\nHealth:\n* Complete a health-check: shortcut-health-check-visitor"
     },
     {
         "Question": "shortcut-user",
-        "Answer": "View announcements: shortcut-view-announcements-user\n\nHealth homepage: shortcut-health-home-user\nComplete a health-check: shortcut-health-check-user\nUpload COVID-19 test results: shortcut-upload-test-result-user\nUpload vaccine confirmation: shortcut-upload-vaccine-confirmation-user\n\nView notifications: shortcut-view-notifications-user\n\nMake a booking: shortcut-create-booking-user\nView bookings: shortcut-view-bookings-user"
+        "Answer": "Type any of these shortcuts you would like to access.\n\nAnnouncements:\n* View announcements: shortcut-view-announcements-user\n\nHealth:\n* Health homepage: shortcut-health-home-user\n* Complete a health-check: shortcut-health-check-user\n* Upload COVID-19 test results: shortcut-upload-test-result-user\n* Upload vaccine confirmation: shortcut-upload-vaccine-confirmation-user\n\nNotifications:\n* View notifications: shortcut-view-notifications-user\n\nBookings:\n* Make a booking: shortcut-create-booking-user\n* View bookings: shortcut-view-bookings-user"
     }
 ]
 
