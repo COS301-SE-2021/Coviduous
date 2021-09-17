3e699642beb705041e9349b1dc15c59df472aa48
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-17 17:32:00 +0200

    Fixed a few of the chatbot's responses

--- artificial_intelligence/coviduous_chatbot/dataset/shortcuts/shortcuts.py
+++ artificial_intelligence/coviduous_chatbot/dataset/shortcuts/shortcuts.py
@@ -80,20 +80,20 @@ SHORTCUTS = [
         "Answer": "/user_office_floor_plans"
     },
     {
-        "Question": "shortcut-view-shifts-admin",
-        "Answer": "/admin_view_shifts_floor_plans"
+        "Question": "shortcut-view-bookings-user",
+        "Answer": "/user_view_bookings"
     },
     {
         "Question": "shortcut-admin",
-        "Answer": "View announcements:shortcut-view-announcements-admin,Make announcement:shortcut-create-announcements-admin,Add floor plan:shortcut-add-floor-plan,Modify floor plans:shortcut-modify-floor-plans,Admin contact trace:shortcut-contact-trace,Reporting homepage:shortcut-reports-admin,Create shift:shortcut-create-shift-admin,View shifts:shortcut-view-shifts-admin"
+        "Answer": "View announcements: shortcut-view-announcements-admin\nMake announcement: shortcut-create-announcements-admin\n\nAdd floor plan: shortcut-add-floor-plan\nModify floor plans: shortcut-modify-floor-plans\n\nAdmin contact trace: shortcut-contact-trace\n\nReporting homepage: shortcut-reports-admin\n\nCreate shift: shortcut-create-shift-admin\nView shifts: shortcut-view-shifts-admin"
     },
     {
         "Question": "shortcut-visitor",
-        "Answer": "health-check:shortcut-health-check-visitor"
+        "Answer": "Complete a health-check: shortcut-health-check-visitor"
     },
     {
         "Question": "shortcut-user",
-        "Answer": ""
+        "Answer": "View announcements: shortcut-view-announcements-user\n\nHealth homepage: shortcut-health-home-user\nComplete a health-check: shortcut-health-check-user\nUpload COVID-19 test results: shortcut-upload-test-result-user\nUpload vaccine confirmation: shortcut-upload-vaccine-confirmation-user\n\nView notifications: shortcut-view-notifications-user\n\nMake a booking: shortcut-create-booking-user\nView bookings: shortcut-view-bookings-user"
     }
 ]
 
