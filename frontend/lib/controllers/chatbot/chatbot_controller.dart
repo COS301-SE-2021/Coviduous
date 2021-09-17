a0e4df49632914aac9e385413bcd0f349a969ab0
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-17 17:50:35 +0200

    Updated how chatbot displays shortcuts and tutorials

--- frontend/lib/controllers/chatbot/chatbot_controller.dart
+++ frontend/lib/controllers/chatbot/chatbot_controller.dart
@@ -1,13 +1,19 @@
 // Chatbot controller
 library controllers;
 
-import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:frontend/models/chatbot/chatbot_response.dart';
 
 import 'package:frontend/globals.dart' as globals;
+import 'package:frontend/controllers/announcement/announcement_helpers.dart' as announcementHelpers;
+import 'package:frontend/controllers/floor_plan/floor_plan_helpers.dart' as floorPlanHelpers;
+import 'package:frontend/controllers/health/health_helpers.dart' as healthHelpers;
+import 'package:frontend/controllers/notification/notification_helpers.dart' as notificationHelpers;
+import 'package:frontend/controllers/office/office_helpers.dart' as officeHelpers;
 import 'package:frontend/controllers/server_info.dart' as serverInfo;
 
 String chatbotServer = serverInfo.getChatbotServer();
@@ -40,4 +46,129 @@ Future<ChatbotResponse> sendAndReceive(String message) async {
   }
 
   return null;
+}
+
+void navigateShortcut(BuildContext context, String shortcutRoute) {
+  if (globals.loggedInUserType == 'ADMIN') {
+    switch (shortcutRoute) {
+      case '/admin_announcements': {
+        announcementHelpers.getAnnouncements().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/admin_make_announcement':
+      case '/admin_add_floor_plan':
+      case '/admin_make_notification':
+      case '/reporting': {
+        Navigator.of(context).pushReplacementNamed(shortcutRoute);
+      }
+      break;
+
+      case '/admin_view_notifications': {
+        notificationHelpers.getNotifications().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/admin_modify_floor_plans':
+      case '/admin_add_shift_floor_plans':
+      case '/admin_view_shifts_floor_plans': {
+        floorPlanHelpers.getFloorPlans().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      default: {
+        _showErrorMessage(context, 'You do not have access to this page.');
+      }
+      break;
+    }
+  } else if (globals.loggedInUserType == "USER") {
+    switch (shortcutRoute) {
+      case '/user_announcements': {
+        announcementHelpers.getAnnouncements().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/user_upload_test_result':
+      case '/user_upload_vaccine_confirm':
+      case '/user_health':
+      case '/user_health_check':
+      case '/user_report_infection': {
+        Navigator.of(context).pushReplacementNamed(shortcutRoute);
+      }
+      break;
+
+      case '/user_view_permissions': {
+        healthHelpers.getPermissionsUser().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/user_view_notifications': {
+        notificationHelpers.getNotifications().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/user_office_floor_plans': {
+        floorPlanHelpers.getFloorPlans().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      case '/user_view_bookings': {
+        officeHelpers.getBookings().then((result) {
+          _checkResult(context, shortcutRoute, result);
+        });
+      }
+      break;
+
+      default: {
+        _showErrorMessage(context, 'You do not have access to this page.');
+      }
+      break;
+    }
+  } else {
+    if (shortcutRoute == '/visitor_health_check') {
+      Navigator.of(context).pushReplacementNamed(shortcutRoute);
+    } else {
+      _showErrorMessage(context, 'You do not have access to this page.');
+    }
+  }
+}
+
+void _checkResult(BuildContext context, String shortcutRoute, bool result) {
+  if (result == true) {
+    Navigator.of(context).pushReplacementNamed(shortcutRoute);
+  } else {
+    _showErrorMessage(context, 'An error occurred while trying to access this page. Please try again later.');
+  }
+}
+
+void _showErrorMessage(BuildContext context, String message) {
+  showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Okay'),
+            onPressed: (){
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      )
+  );
 }
