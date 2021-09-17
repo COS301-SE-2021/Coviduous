979c79686ebabb8e808e183dc1dac9670c391062
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-17 13:23:47 +0200

    Update admin_make_announcement_test.dart
    Fixed the widgets this test looks for.

--- frontend/test/screens/announcement/admin_make_announcement_test.dart
+++ frontend/test/screens/announcement/admin_make_announcement_test.dart
@@ -23,8 +23,8 @@ void main() {
     await tester.pumpWidget(createWidgetForTesting(child: new MakeAnnouncement()));
 
     //Verify that the correct widgets appear in the correct order
-    expect(find.text('Select announcement type'), findsOneWidget); //Find one widget containing 'Select announcement type'
-    expect(find.text('Description'), findsOneWidget); //Find one widget containing 'Description'
+    expect(find.text('Select type'), findsOneWidget); //Find one widget containing 'Select announcement type'
+    expect(find.text('Write your announcement'), findsOneWidget); //Find one widget containing 'Description'
     expect(find.text('Post'), findsOneWidget); //Find one widget containing 'Post'
   });
 }
