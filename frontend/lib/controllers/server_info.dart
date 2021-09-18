6377d19432145975895adacbe958386d23fd6c73
u16040865 <61485475+u16040865@users.noreply.github.com>
2021-09-18 12:09:46 +0200

    Trying to deploy frontend

--- frontend/lib/controllers/server_info.dart
+++ frontend/lib/controllers/server_info.dart
@@ -27,11 +27,7 @@ String getAIserver() {
 
 String getChatbotServer() {
   if (kReleaseMode) { //If release version of the app
-    if (globals.getIfOnPC()) { //If on PC
-      return 'http://localhost:5000/api/message';
-    } else { //Else, on mobile
-      return 'http://10.0.2.2:5000/api/message';
-    }
+    return 'https://coviduous-chatbot.herokuapp.com/api/message';
   } else { //Else, testing version of the app
     if (globals.getIfOnPC()) { //If on PC
       return 'http://localhost:5000/api/message';
